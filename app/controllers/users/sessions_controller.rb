0576ca43321331af8c91796d8d239382ed461adc
Arnaud Levy <contact@arnaudlevy.com>
2021-08-11 18:09:20 +0200

    lightweight

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,15 +1,10 @@
 class Users::SessionsController < Devise::SessionsController
   prepend_before_action :set_university, only: :create
-  before_action :configure_sign_in_params, only: :create
 
   protected
 
   def set_university
     return if request.params[:user].nil?
     request.params[:user][:university_id] = current_university.id
-  end
-
-  def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:university_id])
   end
 end
