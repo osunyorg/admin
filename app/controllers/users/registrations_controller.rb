0bde9d2812d54a222bb9f3196ac95087d04cd44f
Sébastien Gaya <sebastien.gaya@gmail.com>
2021-10-11 01:52:16 +0200

    account update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   prepend_before_action :set_university, only: :create
   before_action :configure_sign_up_params, only: :create
+  before_action :configure_account_update_params, only: :update
 
   protected
 
@@ -9,8 +10,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
     request.params[:user][:university_id] = current_university.id
   end
 
-  # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:language_id])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:mobile_phone, :language_id])
   end
 end
