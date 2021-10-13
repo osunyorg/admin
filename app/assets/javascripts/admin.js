ee3485a478a71063388b56f0994beec41c16e77d
pabois <pierreandre.boissinot@noesya.coop>
2021-10-13 17:34:15 +0200

    rename assets

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,5 +2,6 @@
 //= require jquery_ujs
 //= require notyf/notyf.min
 //= require simple_form_password_with_hints
+//= require simple_form_image_field
 //= require appstack/app
 //= require_tree ./admin
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,4 @@
 //= require jquery3
 //= require jquery_ujs
 //= require simple_form_password_with_hints
-//= require simple_form_image_fields
+//= require simple_form_image_field
