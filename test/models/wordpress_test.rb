57b72744769759a753304c0a211b28c2a76edda5
Arnaud Levy <contact@arnaudlevy.com>
2021-10-19 09:55:23 +0200

    wp

--- test/models/wordpress_test.rb
+++ test/models/wordpress_test.rb
@@ -35,4 +35,20 @@ class WordpressTest < ActiveSupport::TestCase
     assert_equal  Wordpress.clean('TRAVAILLER DEMAIN, Débat &#8211; le 10 mai à 18h30'),
                   'TRAVAILLER DEMAIN, Débat – le 10 mai à 18h30'
   end
+
+  test "remove classes" do
+    assert_equal  Wordpress.clean('<h2 class="titre-diplome">→ Qu’est-ce que le B.U.T.&nbsp;?</h2>'),
+                  '<h2>→ Qu’est-ce que le B.U.T.&nbsp;?</h2>'
+  end
+
+  test "remove divs" do
+    # Quid des images ? Comment gérer le transfert vers scaleway + active storage dans le code ?
+    assert_equal  Wordpress.clean('<div class="wp-block-group"><div class="wp-block-group__inner-container"><div class="wp-block-columns"><div class="wp-block-column"><div class="wp-block-image"><figure class="alignright size-medium is-resized"><a href="https://www.iut.u-bordeaux-montaigne.fr/wp-content/uploads/2021/01/visuel_1.png" rel="lightbox[14475]"><img src="https://www.iut.u-bordeaux-montaigne.fr/wp-content/uploads/2021/01/visuel_1-240x300.png" alt="Le BUT, qu\'est-ce que c\'est ?" class="wp-image-14821" width="173" height="216" srcset="https://www.iut.u-bordeaux-montaigne.fr/wp-content/uploads/2021/01/visuel_1-240x300.png 240w, https://www.iut.u-bordeaux-montaigne.fr/wp-content/uploads/2021/01/visuel_1.png 730w"></a></figure></div></div>'),
+                  '<figure><a href="https://www.iut.u-bordeaux-montaigne.fr/wp-content/uploads/2021/01/visuel_1.png"><img src="https://www.iut.u-bordeaux-montaigne.fr/wp-content/uploads/2021/01/visuel_1-240x300.png" alt="Le BUT, qu\'est-ce que c\'est ?" width="173" height="216" srcset="https://www.iut.u-bordeaux-montaigne.fr/wp-content/uploads/2021/01/visuel_1-240x300.png 240w, https://www.iut.u-bordeaux-montaigne.fr/wp-content/uploads/2021/01/visuel_1.png 730w"></a></figure>'
+
+  end
+
+  test "authorize iframes" do
+
+  end
 end
