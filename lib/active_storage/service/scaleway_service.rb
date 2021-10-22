6155c52462482a64ac27c1021bf4d496121f01cc
pabois <pierreandre.boissinot@noesya.coop>
2021-10-22 16:05:05 +0200

    creating a new scaleway service extending s3 to resolve the direct upload acl problem

---
+++ lib/active_storage/service/scaleway_service.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'active_storage/service/s3_service.rb'
+
+module ActiveStorage
+  class Service::ScalewayService < Service::S3Service
+
+    def headers_for_direct_upload(key, content_type:, checksum:, filename: nil, disposition: nil, **)
+      content_disposition = content_disposition_with(type: disposition, filename: filename) if filename
+
+      headers = public? ? { "x-amz-acl" => "public-read" } : {}
+
+      headers.merge({ "Content-Type" => content_type, "Content-MD5" => checksum, "Content-Disposition" => content_disposition })
+    end
+  end
+end
