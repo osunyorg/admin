9eeed489eb7cc272978decd1c837779ceff45772
Arnaud Levy <contact@arnaudlevy.com>
2021-10-23 17:27:09 +0200

    better layout

--- app/controllers/admin/communication/application_controller.rb
+++ app/controllers/admin/communication/application_controller.rb
@@ -1,6 +1,11 @@
 class Admin::Communication::ApplicationController < Admin::ApplicationController
   def breadcrumb
-    super
-    add_breadcrumb Communication.model_name.human
+    if @website
+      short_breadcrumb
+      breadcrumb_for @website
+    else
+      super
+      add_breadcrumb Communication.model_name.human
+    end
   end
 end
--- app/controllers/admin/communication/website/application_controller.rb
+++ app/controllers/admin/communication/website/application_controller.rb
@@ -3,11 +3,6 @@ class Admin::Communication::Website::ApplicationController < Admin::Communicatio
 
   protected
 
-  def breadcrumb
-    short_breadcrumb
-    breadcrumb_for @website, website_id: nil
-  end
-
   def default_url_options
     return {} unless params.has_key? :website_id
     {
