f849cc3f091870712673a84554f171323ad05de1
Sébastien Gaya <sebastien.gaya@gmail.com>
2021-11-05 11:50:10 +0100

    wip variant service test

---
+++ test/models/variant_service_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class VariantServiceTest < ActiveSupport::TestCase
+  # test "convert apostroph" do
+  #   assert_equal  'Ouverture du CRM pendant les vacances d’Avril',
+  #                 Wordpress.clean_html('Ouverture du CRM pendant les vacances d&#8217;Avril')
+  # end
+end
