57dedb8fdd4593fb64c94310a356579a50394aaa
Sébastien Gaya <sebastien.gaya@gmail.com>
2021-11-16 12:48:28 +0100

    media ctrl

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -3,8 +3,11 @@ class MediaController < ApplicationController
     @blob = ActiveStorage::Blob.find_signed! params[:signed_id]
     variant_service = VariantService.compute(@blob, params[:filename_with_transformations], params[:format])
     transformations = variant_service.transformations
-    blob_or_variant_url = transformations.empty?  ? url_for(@blob)
-                                                  : url_for(@blob.variant(transformations))
+    if !@blob.variable? || transformations.empty?
+      blob_or_variant_url = url_for(@blob)
+    else
+      blob_or_variant_url = url_for(@blob.variant(transformations))
+    end
 
     redirect_to blob_or_variant_url
   end
