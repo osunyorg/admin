3670ff855e7cd582637cf7bb669246a7d13128ed
Arnaud Levy <contact@arnaudlevy.com>
2022-01-06 10:01:31 +0100

    wip

--- app/models/education/program.rb
+++ app/models/education/program.rb
@@ -109,6 +109,10 @@ class Education::Program < ApplicationRecord
     active_storage_blobs
   end
 
+  def git_destroy_dependencies_static
+    explicit_active_storage_blobs
+  end
+
   def update_children_paths
     children.each do |child|
       child.update_column :path, child.generated_path
