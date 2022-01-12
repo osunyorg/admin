79baa2bb1110823354895215986e7a4285df9859
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-01-12 17:30:21 +0100

    fix #131

--- app/models/university/with_users.rb
+++ app/models/university/with_users.rb
@@ -2,6 +2,8 @@ module University::WithUsers
   extend ActiveSupport::Concern
 
   included do
+    attr_accessor :source_university_id
+
     has_many :users, dependent: :destroy
 
     after_commit :synchronize_server_admin_users, on: :create
@@ -9,9 +11,8 @@ module University::WithUsers
     private
 
     def synchronize_server_admin_users
-      User.synchronize_server_admin_users(id)
+      return unless source_university_id.present?
+      SyncServerAdminUsersJob.perform_later(source_university_id, id)
     end
-    handle_asynchronously :synchronize_server_admin_users, queue: 'default'
-
   end
 end
