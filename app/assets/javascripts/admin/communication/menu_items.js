a12d363a78e19fcb33ce9c6a05320cd6568f7599
Arnaud Levy <contact@arnaudlevy.com>
2022-01-21 22:19:28 +0100

    working with shame

--- app/assets/javascripts/admin/communication/menu_items.js
+++ app/assets/javascripts/admin/communication/menu_items.js
@@ -2,20 +2,46 @@
 window.osuny.communication.menuItems = {
     init: function () {
         'use strict';
-        this.kindInput = document.querySelector('form .js-kind-input');
-        if (this.kindInput === null) {
-            return;
-        }
-
+        var i,
+            kind;
+        this.kindInput = document.getElementById('communication_website_menu_item_kind');
+        this.kinds = document.querySelectorAll('[data-kind]');
+        this.url = document.querySelector('.communication_website_menu_item_url');
         this.switchUrl = this.kindInput.dataset.url;
-
-        this.kindInput.addEventListener('change', this.onKindChange.bind(this));
+        for (i = 0; i < this.kinds.length; i += 1) {
+            kind = this.kinds[i];
+            kind.addEventListener('click', this.onKindChange.bind(this));
+        }
+        this.chooseKind(this.kindInput.value);
     },
 
-    onKindChange: function () {
+    onKindChange: function (event) {
         'use strict';
-        var kind = this.kindInput.value;
+        var target = event.target,
+            // TODO get value from parent class="kind"
+            kind = target.dataset.tmp;
+        this.chooseKind(kind);
+        this.loadData(kind);
+    },
 
+    chooseKind: function (kind) {
+        'use strict';
+        var active = document.querySelector('[data-kind="' + kind + '"]'),
+            i;
+        this.kindInput.value = kind;
+        for (i = 0; i < this.kinds.length; i += 1) {
+            this.kinds[i].classList.remove('kind--selected');
+        }
+        active.classList.add('kind--selected');
+        if (kind === 'url') {
+            this.url.classList.remove('d-none');
+        } else {
+            this.url.classList.add('d-none');
+        }
+    },
+
+    loadData: function (kind) {
+        'use strict';
         $.ajax(this.switchUrl, {
             method: 'GET',
             data: 'kind=' + kind,
