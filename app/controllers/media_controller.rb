713fe9f49e1d5cd346c6d21ec90ac4a3146af047
Arnaud Levy <contact@arnaudlevy.com>
2022-01-25 11:47:33 +0100

    content-length

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,6 +1,7 @@
 class MediaController < ApplicationController
   def show
     @blob = ActiveStorage::Blob.find_signed! params[:signed_id]
+    @size = @blob.byte_size
     if @blob.variable?
       variant_service = VariantService.compute(@blob, params[:filename_with_transformations], params[:format])
       transformations = variant_service.transformations
@@ -8,7 +9,7 @@ class MediaController < ApplicationController
     else
       blob_or_variant_url = url_for(@blob)
     end
-
+    response.headers["Content-Length"] = "#{@size}"
     redirect_to blob_or_variant_url
   end
 end
