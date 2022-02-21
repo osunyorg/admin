a82baeab7953f3da54a56d3186440b1bd4277012
Arnaud Levy <contact@arnaudlevy.com>
2022-02-21 13:31:47 +0100

    fix

--- app/controllers/admin/education/programs_controller.rb
+++ app/controllers/admin/education/programs_controller.rb
@@ -85,7 +85,7 @@ class Admin::Education::ProgramsController < Admin::Education::ApplicationContro
       :name, :slug, :level, :capacity, :ects, :continuing, :description, :published,
       :featured_image, :featured_image_delete, :featured_image_infos, :featured_image_alt,
       :prerequisites, :objectives, :duration, :registration, :pedagogy, :content,
-      :evaluation, :accessibility, :pricing, :contacts, :opportunities, :other,
+      :evaluation, :accessibility, :pricing, :contacts, :opportunities, :results, :other,
       :parent_id, school_ids: [], university_person_involvements_attributes: [:id, :person_id, :description, :position, :_destroy]
     )
   end
