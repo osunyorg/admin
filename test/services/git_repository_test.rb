1070a07a1af7d1e6909329bd09472840fa07d5df
Arnaud Levy <contact@arnaudlevy.com>
2022-02-21 18:12:40 +0100

    github tests

--- test/services/git_repository_test.rb
+++ test/services/git_repository_test.rb
@@ -2,29 +2,53 @@ require "test_helper"
 
 class GitRepositoryTest < ActiveSupport::TestCase
   test "incorrect credentials for github" do
-    exception = assert_raises(Exception) do
-      provider = Git::Providers::Github.new 'wrong token', 'username/repository'
-      provider.create_file '/path.txt', 'content'
-      provider.push 'this is a commit'
+    VCR.use_cassette(location) do
+      exception = assert_raises(Exception) do
+        provider = Git::Providers::Github.new 'wrong token', 'username/repository'
+        provider.create_file '/path.txt', 'content'
+        provider.push 'this is a commit'
+      end
+      assert_equal exception.class, Octokit::Unauthorized
     end
-    assert_equal exception.class, Octokit::Unauthorized
   end
 
   test "file creation on github" do
-    provider = Git::Providers::Github.new 'correct token', 'username/repository'
-    provider.create_file '/path.txt', 'content'
-    result = provider.push 'this is a commit'
+    VCR.use_cassette(location) do
+      assert_nothing_raised do
+        provider = Git::Providers::Github.new ENV['TEST_GITHUB_TOKEN'], ENV['TEST_GITHUB_REPOSITORY']
+        provider.create_file 'test.txt', 'content'
+        result = provider.push 'Creating test.txt file'
+      end
+    end
   end
 
   test "file update on github" do
-
+    VCR.use_cassette(location) do
+      assert_nothing_raised do
+        provider = Git::Providers::Github.new ENV['TEST_GITHUB_TOKEN'], ENV['TEST_GITHUB_REPOSITORY']
+        provider.update_file 'test.txt', 'test.txt', 'new content'
+        result = provider.push 'Updating test.txt file'
+      end
+    end
   end
 
   test "file move on github" do
-
+    VCR.use_cassette(location) do
+      assert_nothing_raised do
+        provider = Git::Providers::Github.new ENV['TEST_GITHUB_TOKEN'], ENV['TEST_GITHUB_REPOSITORY']
+        provider.update_file 'new_test.txt', 'test.txt', 'new content'
+        result = provider.push 'Moving test.txt file'
+      end
+    end
   end
 
   test "file destroy on github" do
-
+    VCR.use_cassette(location) do
+      assert_nothing_raised do
+        provider = Git::Providers::Github.new ENV['TEST_GITHUB_TOKEN'], ENV['TEST_GITHUB_REPOSITORY']
+        provider.destroy_file 'new_test.txt'
+        result = provider.push 'Destroying new_test.txt file'
+      end
+    end
   end
 end
--- test/support/vcr.rb
+++ test/support/vcr.rb
@@ -4,4 +4,6 @@ VCR.configure do |c|
   c.cassette_library_dir = 'test/cassettes'
   c.hook_into :webmock
   c.allow_http_connections_when_no_cassette = false
+  c.filter_sensitive_data('<TEST_GITHUB_TOKEN>') { ENV['TEST_GITHUB_TOKEN'] }
+  c.filter_sensitive_data('<TEST_GITHUB_REPOSITORY>') { ENV['TEST_GITHUB_REPOSITORY'] }
 end
