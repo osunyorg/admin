cf46a09235a8c56b23d9012decf353fd19d62cec
pabois <pierreandre.boissinot@noesya.coop>
2022-02-21 17:46:29 +0100

    tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 
+Dir["./test/support/**/*.rb"].each { |f| require f }
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
