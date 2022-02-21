2ea275661a5a99b1a6c78ad565bd1dd53502e90a
Arnaud Levy <contact@arnaudlevy.com>
2022-02-21 17:47:53 +0100

    test wip

--- test/services/git_repository_test.rb
+++ test/services/git_repository_test.rb
@@ -1,8 +1,30 @@
 require "test_helper"
 
 class GitRepositoryTest < ActiveSupport::TestCase
+  test "incorrect credentials for github" do
+    exception = assert_raises(Exception) do
+      provider = Git::Providers::Github.new 'wrong token', 'username/repository'
+      provider.create_file '/path.txt', 'content'
+      provider.push 'this is a commit'
+    end
+    assert_equal exception.class, Octokit::Unauthorized
+  end
 
-  test "params for dan-gold.jpeg" do
+  test "file creation on github" do
+    provider = Git::Providers::Github.new 'correct token', 'username/repository'
+    provider.create_file '/path.txt', 'content'
+    result = provider.push 'this is a commit'
+  end
+
+  test "file update on github" do
+
+  end
+
+  test "file move on github" do
+
+  end
+
+  test "file destroy on github" do
 
   end
 end
