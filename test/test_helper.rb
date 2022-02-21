8aba2cd44ece313a6466d0b02f6264b079053b6b
Arnaud Levy <contact@arnaudlevy.com>
2022-02-21 17:48:38 +0100

    clean

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,11 +5,6 @@ require "rails/test_help"
 Dir["./test/support/**/*.rb"].each { |f| require f }
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
 end
