503544061aaae8ff9977fbd6e9148d3eb5558b4f
pabois <pierreandre.boissinot@noesya.coop>
2022-02-24 14:13:10 +0100

    close #215

--- app/assets/javascripts/admin/plugins/summernote.js
+++ app/assets/javascripts/admin/plugins/summernote.js
@@ -2,50 +2,63 @@
 $(function () {
     'use strict';
 
+    var configs = [];
+    configs['mini'] = {
+        toolbar: [
+            ['font', ['bold', 'italic']],
+            ['insert', ['link']],
+            ['view', ['codeview']]
+        ]
+    };
+
+    configs['default'] = {
+        popover: {
+            image: [
+                ['remove', ['removeMedia']]
+            ]
+        },
+        toolbar: [
+            ['style', ['style']],
+            ['font', ['bold', 'italic']],
+            ['para', ['ul', 'ol']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture', 'video']],
+            ['view', ['codeview']]
+        ],
+        styleTags: [
+            'p',
+            'blockquote',
+            'pre',
+            'h2',
+            'h3',
+            'h4'
+        ],
+        followingToolbar: true,
+        callbacks: {
+            onImageUpload: function (files) {
+                var attachmentUpload = new SummernoteAttachmentUpload(this, files[0]);
+                attachmentUpload.start();
+            },
+            onMediaDelete: function (_, $editable) {
+                $.summernote.rails.cleanEmptyAttachments($editable);
+            },
+            onKeyup: function (e) {
+                var $editable = $(e.currentTarget);
+                if (e.keyCode === 8) {
+                    $.summernote.rails.cleanEmptyAttachments($editable);
+                }
+            }
+        }
+    };
+
     $.extend($.summernote.lang['en-US'].image, {
         dragImageHere: 'Drag file here',
         dropImage: 'Drop file'
     });
 
     $('[data-provider="summernote"]').each(function () {
-        $(this).summernote({
-            popover: {
-                image: [
-                    ['remove', ['removeMedia']]
-                ]
-            },
-            toolbar: [
-                ['style', ['style']],
-                ['font', ['bold', 'italic']],
-                ['para', ['ul', 'ol']],
-                ['table', ['table']],
-                ['insert', ['link', 'picture', 'video']],
-                ['view', ['codeview']]
-            ],
-            styleTags: [
-                'p',
-                'blockquote',
-                'pre',
-                'h2',
-                'h3',
-                'h4'
-            ],
-            followingToolbar: true,
-            callbacks: {
-                onImageUpload: function (files) {
-                    var attachmentUpload = new SummernoteAttachmentUpload(this, files[0]);
-                    attachmentUpload.start();
-                },
-                onMediaDelete: function (_, $editable) {
-                    $.summernote.rails.cleanEmptyAttachments($editable);
-                },
-                onKeyup: function (e) {
-                    var $editable = $(e.currentTarget);
-                    if (e.keyCode === 8) {
-                        $.summernote.rails.cleanEmptyAttachments($editable);
-                    }
-                }
-            }
-        });
+        var config = $(this).attr('data-summernote-config');
+        config = config || 'default';
+        $(this).summernote(configs[config]);
     });
 });
