0c4129ab336f017d640e2d60b87ac5aaca448225
Arnaud Levy <contact@arnaudlevy.com>
2022-02-27 19:01:16 +0100

    fix dependencies

--- app/models/communication/block/organization_chart.rb
+++ app/models/communication/block/organization_chart.rb
@@ -1,15 +1,13 @@
 class Communication::Block::OrganizationChart < Communication::Block::Template
   def git_dependencies
     dependencies = []
-    data['elements'].each do |element|
-      element['persons'].each do |person|
-        id = person['id']
-        next if id.blank?
-        person = university.people.find id
-        next if person.nil?
-        dependencies += [person]
-        dependencies += person.active_storage_blobs
-      end
+    data['elements'].each do |person|
+      id = person['id']
+      next if id.blank?
+      person = university.people.find id
+      next if person.nil?
+      dependencies += [person]
+      dependencies += person.active_storage_blobs
     end
     dependencies.uniq
   end
--- app/models/communication/block/partner.rb
+++ app/models/communication/block/partner.rb
@@ -1,14 +1,12 @@
 class Communication::Block::Partner < Communication::Block::Template
   def git_dependencies
     dependencies = []
-    data['elements'].each do |element|
-      element['partners'].each do |partner|
-        id = partner.dig('logo', 'id')
-        next if id.blank?
-        blob = university.active_storage_blobs.find id
-        next if blob.nil?
-        dependencies += [blob]
-      end
+    data['elements'].each do |partner|
+      id = partner.dig('logo', 'id')
+      next if id.blank?
+      blob = university.active_storage_blobs.find id
+      next if blob.nil?
+      dependencies += [blob]
     end
     dependencies.uniq
   end
