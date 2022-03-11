68f0e814a7d3589c3d708339870f44cef83eb4a2
alexisben <alexiben7@gmail.com>
2022-03-11 17:02:39 +0100

    Cleanup static

--- app/models/communication/block/partner.rb
+++ app/models/communication/block/partner.rb
@@ -3,6 +3,14 @@ class Communication::Block::Partner < Communication::Block::Abstract
     elements.each do |partner|
       blob = find_blob partner, 'logo'
       add_dependency blob unless blob.nil?
+
+      # TODO : add organization dependency
+      # id = partner['id']
+      # next if id.blank?
+      # organization = university.organizations.find id
+      # next if organization.nil?
+      # add_dependency organization
+      # add_dependency organization.active_storage_blobs
     end
   end
 end
