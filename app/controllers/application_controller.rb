2fb26b9de53e9ad276e0178345f34513cac31824
Arnaud Levy <contact@arnaudlevy.com>
2022-03-16 16:16:37 +0100

    authenticate extranets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include WithErrors
   include WithLocale
   include WithDomain
+  before_action :authenticate_user!
 
   def breadcrumb
     add_breadcrumb t('home'), root_path
