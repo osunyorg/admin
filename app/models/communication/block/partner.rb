c30a517d975430c7f8328a179120d5521e70234a
Arnaud Levy <contact@arnaudlevy.com>
2022-03-23 23:52:25 +0100

    fix

--- app/models/communication/block/partner.rb
+++ app/models/communication/block/partner.rb
@@ -3,12 +3,17 @@ class Communication::Block::Partner < Communication::Block::Abstract
     elements.each do |partner|
       blob = find_blob partner, 'logo'
       add_dependency blob unless blob.nil?
-      id = partner['id']
-      next if id.blank?
-      organization = university.organizations.find id
-      next if organization.nil?
-      add_dependency organization
-      add_dependency organization.active_storage_blobs
+      add_partner partner['id']
     end
+  end
+
+  protected
+
+  def add_partner(id)
+    next if id.blank?
+    organization = university.organizations.find id
+    next if organization.nil?
+    add_dependency organization
+    add_dependency organization.active_storage_blobs
   end
 end
