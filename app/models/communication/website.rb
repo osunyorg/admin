c5204964fe969e8dd13118ce324b5821d28b0815
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-04-04 17:30:46 +0200

    split website git dependencies

--- app/models/communication/website.rb
+++ app/models/communication/website.rb
@@ -58,21 +58,24 @@ class Communication::Website < ApplicationRecord
     dependencies += people_with_facets + people.map(&:active_storage_blobs).flatten if has_people?
     dependencies += [categories] if has_communication_categories?
     dependencies += about.git_dependencies(website)
-
-    # TMP: add index_pages
-    dependencies += [index_for(:home)] + index_for(:home).active_storage_blobs
-    dependencies += [index_for(:communication_posts)] + index_for(:communication_posts).active_storage_blobs if has_communication_posts?
-    dependencies += [index_for(:education_programs)] + index_for(:education_programs).active_storage_blobs if has_education_programs?
-    dependencies += [index_for(:persons)] + index_for(:persons).active_storage_blobs if has_people?
-    dependencies += [index_for(:authors)] + index_for(:authors).active_storage_blobs if has_authors?
-    dependencies += [index_for(:administrators)] + index_for(:administrators).active_storage_blobs if has_administrators?
-    dependencies += [index_for(:teachers)] + index_for(:teachers).active_storage_blobs if has_teachers?
-    dependencies += [index_for(:research_volumes)] + index_for(:research_volumes).active_storage_blobs if has_research_volumes?
-    dependencies += [index_for(:research_articles)] + index_for(:research_articles).active_storage_blobs if has_research_articles?
-    dependencies += [index_for(:researchers)] + index_for(:researchers).active_storage_blobs if has_researchers?
-    # END TMP
-
+    dependencies += git_index_pages_dependencies(website)
     dependencies
+  end
+
+  def git_index_pages_dependencies(website)
+    # TMP: add index_pages
+    index_pages_dependencies = [index_for(:home)] + index_for(:home).active_storage_blobs
+    index_pages_dependencies += [index_for(:communication_posts)] + index_for(:communication_posts).active_storage_blobs if has_communication_posts?
+    index_pages_dependencies += [index_for(:education_programs)] + index_for(:education_programs).active_storage_blobs if has_education_programs?
+    index_pages_dependencies += [index_for(:persons)] + index_for(:persons).active_storage_blobs if has_people?
+    index_pages_dependencies += [index_for(:authors)] + index_for(:authors).active_storage_blobs if has_authors?
+    index_pages_dependencies += [index_for(:administrators)] + index_for(:administrators).active_storage_blobs if has_administrators?
+    index_pages_dependencies += [index_for(:teachers)] + index_for(:teachers).active_storage_blobs if has_teachers?
+    index_pages_dependencies += [index_for(:research_volumes)] + index_for(:research_volumes).active_storage_blobs if has_research_volumes?
+    index_pages_dependencies += [index_for(:research_articles)] + index_for(:research_articles).active_storage_blobs if has_research_articles?
+    index_pages_dependencies += [index_for(:researchers)] + index_for(:researchers).active_storage_blobs if has_researchers?
+    index_pages_dependencies
+    # END TMP
   end
 
 end
