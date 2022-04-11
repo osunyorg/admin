ced5e4dbdfae68e308ee1015f042fcb2ce633bf1
pabois <pierreandre.boissinot@noesya.coop>
2022-04-11 10:52:53 +0200

    wording

--- db/schema.rb
+++ db/schema.rb
@@ -83,7 +83,7 @@ ActiveRecord::Schema.define(version: 2022_04_08_105051) do
     t.uuid "university_id", null: false
     t.string "about_type"
     t.uuid "about_id"
-    t.integer "template_kind", default: 0, null: false
+    t.integer "template", default: 0, null: false
     t.jsonb "data"
     t.integer "position", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
