359cc96a63a06614b97d0ece6b1ab7deb67cf1d7
Arnaud Levy <contact@arnaudlevy.com>
2022-04-13 23:08:56 +0200

    fix

--- app/models/communication/block/template/gallery.rb
+++ app/models/communication/block/template/gallery.rb
@@ -7,18 +7,23 @@ class Communication::Block::Template::Gallery < Communication::Block::Template
     unless @images_with_alt
       @images_with_alt = []
       elements.each do |element|
-        blob = find_blob element, 'file'
-        next if blob.nil?
-        @images_with_alt << {
-          blob: blob,
-          alt: element['alt']
-        }.to_dot
+        @images_with_alt << image_with_alt(element)
       end
+      @images_with_alt.compact!
     end
     @images_with_alt
   end
 
   protected
+
+  def image_with_alt(element)
+    blob = find_blob element, 'file'
+    return if blob.nil?
+    {
+      blob: blob,
+      alt: element['alt']
+    }.to_dot
+  end
 
   def images
     @images ||= images_with_alt.map { |hash| hash.blob }
