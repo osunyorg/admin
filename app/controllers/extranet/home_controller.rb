61de8470b1d36a3646b56bad8ed33536df2c9f37
Arnaud Levy <contact@arnaudlevy.com>
2022-04-15 21:17:34 +0200

    fix

--- app/controllers/extranet/home_controller.rb
+++ app/controllers/extranet/home_controller.rb
@@ -1,6 +1,6 @@
 class Extranet::HomeController < Extranet::ApplicationController
   def index
-    redirect_to admin_root_path unless current_extranet
+    return redirect_to admin_root_path unless current_extranet
     @cohorts = current_extranet.about&.cohorts || current_university.education_cohorts
     @cohorts = @cohorts.ordered.limit(5)
   end
