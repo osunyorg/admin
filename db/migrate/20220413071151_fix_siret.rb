84e96d0a0ee57315e4c97e6f416cd5bf33fb5fa9
pabois <pierreandre.boissinot@noesya.coop>
2022-04-28 17:09:53 +0200

    remove migration bug

--- db/migrate/20220413071151_fix_siret.rb
+++ db/migrate/20220413071151_fix_siret.rb
@@ -1,6 +1,5 @@
 class FixSiret < ActiveRecord::Migration[6.1]
   def change
-    rename_column :university_organizations, :sirene, :siren
     add_column :university_organizations, :nic, :string
   end
 end
