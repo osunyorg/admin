7f83f8d8ea8cf2036a0f6eec0f3b5012fdca76a1
pabois <pierreandre.boissinot@noesya.coop>
2022-04-28 17:04:03 +0200

    close #344

--- app/models/university/person.rb
+++ app/models/university/person.rb
@@ -138,6 +138,10 @@ class University::Person < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  def to_s_alphabetical
+    "#{last_name} #{first_name}"
+  end
+
   def roles
     LIST_OF_ROLES.reject do |role|
       ! send "is_#{role}"
