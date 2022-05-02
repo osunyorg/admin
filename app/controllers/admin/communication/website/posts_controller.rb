84ecc6aed37bf51dbeebb1976e9729bcf7f8484f
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-05-02 16:31:01 +0200

    fix

--- app/controllers/admin/communication/website/posts_controller.rb
+++ app/controllers/admin/communication/website/posts_controller.rb
@@ -12,9 +12,9 @@ class Admin::Communication::Website::PostsController < Admin::Communication::Web
 
   def index
     @posts = apply_scopes(@posts).ordered.page params[:page]
-    @authors =  apply_scopes(@website.authors.accessible_by(current_ability))
+    @authors =  @website.authors.accessible_by(current_ability)
                                 .ordered
-                                .page(params[:page])
+                                .page(params[:authors_page])
     @root_categories = @website.categories.root.ordered
     breadcrumb
   end
