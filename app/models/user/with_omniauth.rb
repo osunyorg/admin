09050cca9b175adebe96ad3318eee4075ea01cc3
pabois <pierreandre.boissinot@noesya.coop>
2022-05-02 14:40:47 +0200

    omniauth

--- app/models/user/with_omniauth.rb
+++ app/models/user/with_omniauth.rb
@@ -7,6 +7,13 @@ module User::WithOmniauth
       mapping = university.sso_mapping
       email = 'pierreandre.boissinot@noesya.coop'
 
+      # email_sso_key = mapping.select { |elmt| elmt['internal_key'] == 'email' }&.first&.dig('sso_key')
+      email_sso_key = 'email'
+      email = attributes.dig(email_sso_key)
+      return unless email
+      email = email.first if email.is_a?(Array)
+      email = email.downcase
+
       user = User.where(university: university, email: email).first_or_create do |u|
         u.password = "#{Devise.friendly_token[0,20]}!" # meets password complexity requirements
       end
