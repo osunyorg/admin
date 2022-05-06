a9649706d7fe62d6e96110a2e4937822e687c389
Arnaud Levy <contact@arnaudlevy.com>
2022-05-06 17:48:19 +0200

    clean

--- app/controllers/admin/communication/unsplash_controller.rb
+++ app/controllers/admin/communication/unsplash_controller.rb
@@ -4,16 +4,19 @@ class Admin::Communication::UnsplashController < Admin::Communication::Applicati
   def index
     if params[:query].blank?
       @search = []
+      @total = 0
+      @total_pages = 0
     else
-      @search = Unsplash::Search.search "/search/photos",
-        Unsplash::Photo,
-        {
-          query: params[:query],
-          page: (params[:page] || 1),
-          per_page: (params[:per_page] || 10),
-          orientation: (params[:orientation] || 'squarish'),
-          lang: (params[:lang] || 'en')
-        }
+      p = {
+        query: params[:query],
+        page: (params[:page] || 1),
+        per_page: (params[:per_page] || 10),
+        orientation: (params[:orientation] || 'squarish'),
+        lang: (params[:lang] || 'en')
+      }
+      @search = Unsplash::Search.search "/search/photos", Unsplash::Photo, p
+      @total = @search.total
+      @total_pages = @search.total_pages
     end
   end
 end
