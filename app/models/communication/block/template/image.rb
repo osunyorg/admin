5a837bafd1cd8566b47e3a6f34838f0ec2c6824f
Arnaud Levy <contact@arnaudlevy.com>
2022-05-11 11:36:02 +0200

    image

--- app/models/communication/block/template/image.rb
+++ app/models/communication/block/template/image.rb
@@ -1,4 +1,20 @@
 class Communication::Block::Template::Image < Communication::Block::Template
   def build_git_dependencies
+    add_dependency image&.blob
   end
+
+  def image
+    blob = find_blob data, 'image'
+    return if blob.nil?
+    {
+      blob: blob,
+      alt: data['image_alt'],
+      credit: data['image_credit']
+    }.to_dot
+  end
+
+  def text
+    "#{data['text']}"
+  end
+
 end
