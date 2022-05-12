2fd4a23ad79eb018a8ccc2c13804cb8333504a2f
Arnaud Levy <contact@arnaudlevy.com>
2022-05-12 17:10:16 +0200

    resave

--- app/controllers/server/blocks_controller.rb
+++ app/controllers/server/blocks_controller.rb
@@ -11,12 +11,17 @@ class Server::BlocksController < Server::ApplicationController
     add_breadcrumb t("enums.communication.block.template_kind.#{@template}")
   end
 
+  def resave
+    @template = params[:id]
+    @blocks = Communication::Block.send(@template)
+    @blocks.find_each &:save
+    redirect_to server_block_path(@template), notice: "#{@blocks.count} blocks saved"
+  end
+
   protected
 
   def breadcrumb
     super
     add_breadcrumb Communication::Block.model_name.human(count: 2), server_blocks_path
   end
-
-
 end
--- config/routes/server.rb
+++ config/routes/server.rb
@@ -2,5 +2,6 @@ namespace :server do
   resources :universities, :languages
   get 'blocks' => 'blocks#index', as: :blocks
   get 'blocks/:id' => 'blocks#show', as: :block
+  post 'blocks/:id' => 'blocks#resave', as: :resave_block
   root to: 'dashboard#index'
 end
