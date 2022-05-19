2cfc094c6938c52b255fb57462da3ca95cc57264
Arnaud Levy <contact@arnaudlevy.com>
2022-05-19 14:41:18 +0200

    cleaner

--- test/services/summernote_cleaner_test.rb
+++ test/services/summernote_cleaner_test.rb
@@ -2,8 +2,17 @@ require "test_helper"
 
 class SummernoteCleanerTest < ActiveSupport::TestCase
   test "add p around text if missing" do
-    text = 'Les étudiants doivent s\'acquitter des droits universitaires annuels (170€ pour 2021/2022) ainsi que de la Contribution de la Vie Étudiante et de Campus (92€ pour 2021/2022). Ils doivent également justifier d\'une assurance responsabilité civile.<br>Plus d\'informations dans la rubrique → <a href="https://bordeauxmontaigne-iut.netlify.app/vie-etudiante/scolarite/frais-dinscription">Scolarité</a>'
-    assert_equal '<p>Les étudiants doivent s\'acquitter des droits universitaires annuels (170€ pour 2021/2022) ainsi que de la Contribution de la Vie Étudiante et de Campus (92€ pour 2021/2022). Ils doivent également justifier d\'une assurance responsabilité civile.<br>Plus d\'informations dans la rubrique → <a href="https://bordeauxmontaigne-iut.netlify.app/vie-etudiante/scolarite/frais-dinscription">Scolarité</a></p>', SummernoteCleaner.clean(text)
+    text = 'Text<br><a href="#">link</a>'
+    assert_equal '<p>Text<br><a href="#">link</a></p>', SummernoteCleaner.clean(text)
   end
 
+  test "do nothing if p is there" do
+    text = '<p>Text</p>'
+    assert_equal '<p>Text</p>', SummernoteCleaner.clean(text)
+  end
+
+  test "add p before an existing p" do
+    text = 'Text<p>Second text</p>'
+    assert_equal '<p>Text</p><p>Second text</p>', SummernoteCleaner.clean(text)
+  end
 end
