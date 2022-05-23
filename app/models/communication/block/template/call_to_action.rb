3961e025b3169264d31b1e08be74bf4e81e86e10
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-05-23 18:32:34 +0200

    sanitize

--- app/models/communication/block/template/call_to_action.rb
+++ app/models/communication/block/template/call_to_action.rb
@@ -1,4 +1,19 @@
 class Communication::Block::Template::CallToAction < Communication::Block::Template
+  def sanitized_data
+    {
+      "text" => Osuny::Sanitizer.sanitize(text),
+      "url" => url,
+      "button" => Osuny::Sanitizer.sanitize(button, 'string'),
+      "url_secondary" => url_secondary,
+      "button_secondary" => Osuny::Sanitizer.sanitize(button_secondary, 'string'),
+      "url_tertiary" => url_tertiary,
+      "button_tertiary" => Osuny::Sanitizer.sanitize(button_tertiary, 'string'),
+      "image" => data['image'],
+      "image_alt" => Osuny::Sanitizer.sanitize(data['image_alt'], 'string'),
+      "image_credit" => Osuny::Sanitizer.sanitize(data['image_credit'], 'string')
+    }
+  end
+
   def build_git_dependencies
     add_dependency image&.blob
   end
