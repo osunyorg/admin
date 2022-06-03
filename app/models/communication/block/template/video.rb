6e170c7ffe3a4e8d95d837c6eb9b759b31bfeb0e
Arnaud Levy <contact@arnaudlevy.com>
2022-06-03 15:12:57 +0200

    video with video title

--- app/models/communication/block/template/video.rb
+++ app/models/communication/block/template/video.rb
@@ -1,5 +1,6 @@
 class Communication::Block::Template::Video < Communication::Block::Template::Base
   has_string :url
+  has_string :video_title
   has_text :transcription
 
   def build_git_dependencies
