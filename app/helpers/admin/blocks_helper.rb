e9e9efab691f8b93d493cf41a2f4f4991fe47d58
Arnaud Levy <contact@arnaudlevy.com>
2022-06-07 20:09:02 +0200

    element components

--- app/helpers/admin/blocks_helper.rb
+++ app/helpers/admin/blocks_helper.rb
@@ -1,26 +1,28 @@
 module Admin::BlocksHelper
 
   def block_component_edit(property, **options)
-    component = block_component property
+    component = block_component property, options[:element]
     partial = "admin/communication/blocks/components/#{component.kind}/edit"
     render partial, property: property, **options
   end
 
   def block_component_preview(property, **options)
-    component = block_component property
+    component = block_component property, options[:element]
     partial = "admin/communication/blocks/components/#{component.kind}/preview"
     render partial, property: property, **options
   end
 
   def block_component_static(property, **options)
-    component = block_component property
+    component = block_component property, options[:element]
     partial = "admin/communication/blocks/components/#{component.kind}/static"
     render partial, property: property, **options
   end
 
   protected
 
-  def block_component(property)
-    @block.template.public_send "#{property}_component"
+  def block_component(property, element)
+    template = element.nil? ? @block.template
+                            : element
+    template.public_send "#{property}_component"
   end
 end
--- app/models/communication/block/template/gallery/image.rb
+++ app/models/communication/block/template/gallery/image.rb
@@ -1,5 +1,7 @@
 class Communication::Block::Template::Gallery::Image < Communication::Block::Template::Base
   has_image :image
+  has_string :alt
+  has_rich_text :credit
   has_rich_text :text
 
   def default_data
