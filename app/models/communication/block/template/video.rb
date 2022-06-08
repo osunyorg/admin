1852410d7d2a08ae3fb1f7b836dcc67ff0bae3fb
Arnaud Levy <contact@arnaudlevy.com>
2022-06-08 10:16:19 +0200

    wip

--- app/models/communication/block/template/video.rb
+++ app/models/communication/block/template/video.rb
@@ -1,8 +1,7 @@
 class Communication::Block::Template::Video < Communication::Block::Template::Base
+
   has_string :url
   has_string :video_title
   has_text :transcription
 
-  def build_git_dependencies
-  end
 end
