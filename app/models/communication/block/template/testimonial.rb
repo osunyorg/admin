5a301b26f3d2cc9763ec704ad5dadfc93f82db28
Olivia Simonet <olivia.simonet@mmibordeaux.com>
2022-06-10 14:28:44 +0200

    Refactor unfinished block testimonials

--- app/models/communication/block/template/testimonial.rb
+++ app/models/communication/block/template/testimonial.rb
@@ -1,23 +1,5 @@
 class Communication::Block::Template::Testimonial < Communication::Block::Template::Base
-  def build_git_dependencies
-    add_dependency active_storage_blobs
-  end
 
-  def testimonials
-    @testimonials ||= elements.map { |element| testimonial(element) }
-                              .compact
-  end
+  has_elements Communication::Block::Template::Testimonial::Testimonial
 
-  def active_storage_blobs
-    @active_storage_blobs ||= testimonials.map { |testimonial| testimonial.blob }
-                                          .compact
-  end
-
-  protected
-
-  def testimonial(element)
-    blob = find_blob element, 'photo'
-    element['blob'] = blob
-    element.to_dot
-  end
 end
---
+++ app/models/communication/block/template/timeline/timeline.rb
@@ -0,0 +1,6 @@
+class Communication::Block::Template::Testimonial::Testimonial < Communication::Block::Template::Base
+
+  has_string :author
+  has_text :text
+
+end
