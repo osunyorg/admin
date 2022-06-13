32acb2cccd30025975a5259ced1de7eac6668d10
Olivia Simonet <olivia.simonet@mmibordeaux.com>
2022-06-13 17:25:26 +0200

    Refactor block organizations / missing select

--- app/models/communication/block/template/organization_chart.rb
+++ app/models/communication/block/template/organization_chart.rb
@@ -1,28 +1,5 @@
 class Communication::Block::Template::OrganizationChart < Communication::Block::Template::Base
-  def build_git_dependencies
-    add_dependency persons
-    persons.each do |person|
-      add_dependency person.active_storage_blobs
-    end
-  end
 
-  def persons_with_role
-    @persons_with_role ||= elements.map { |element| person_with_role(element) }
-                                   .compact
-  end
+  has_elements Communication::Block::Template::OrganizationChart::Person
 
-  protected
-
-  def persons
-    @persons ||= persons_with_role.map { |hash| hash[:person] }
-  end
-
-  def person_with_role(element)
-    person = block.university.people.find_by id: element['id']
-    return if person.nil?
-    {
-      person: person,
-      role: element['role']
-    }.to_dot
-  end
 end
