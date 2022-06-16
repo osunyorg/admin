c73556605a83c29e436eccff6c7b1458263b962b
Arnaud Levy <contact@arnaudlevy.com>
2022-06-16 18:35:53 +0200

    sanitize html

--- app/models/communication/block/component/rich_text.rb
+++ app/models/communication/block/component/rich_text.rb
@@ -1,7 +1,14 @@
 class Communication::Block::Component::RichText < Communication::Block::Component::Base
 
   def data=(value)
-    @data = SummernoteCleaner.clean value.to_s
+    @data = clean(value)
   end
 
+  protected
+
+  def clean(value)
+    value = SummernoteCleaner.clean value.to_s
+    value = ActionView::Base.full_sanitizer.sanitize value
+    value
+  end
 end
