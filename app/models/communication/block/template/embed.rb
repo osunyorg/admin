5d4be4a5791784a1f6427cb289e5ae21c089e5a2
Arnaud Levy <contact@arnaudlevy.com>
2022-06-20 15:31:04 +0200

    code

--- app/models/communication/block/template/embed.rb
+++ app/models/communication/block/template/embed.rb
@@ -1,6 +1,6 @@
 class Communication::Block::Template::Embed < Communication::Block::Template::Base
 
-  has_component :code, :text
+  has_component :code, :code
   has_component :iframe_title, :string
   has_component :transcription, :text
 
