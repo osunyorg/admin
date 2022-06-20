c496c5403a6a2e4d774de2be0fa37723182b833e
Arnaud Levy <contact@arnaudlevy.com>
2022-06-20 23:50:30 +0200

    i18n

--- app/models/communication/block/component/file.rb
+++ app/models/communication/block/component/file.rb
@@ -5,14 +5,6 @@ class Communication::Block::Component::File < Communication::Block::Component::B
     @blob ||= template.blob_with_id data['id']
   end
 
-  def data=(value)
-    super
-    # Fix nil objects
-    @data = {} if @data.nil?
-    # Fix objects with no id
-    @data['id'] = '' unless @data.has_key? 'id'
-  end
-
   def default_data
     {
       'id' => ''
