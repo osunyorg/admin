7fc6b4d937c38ca0d10ceed8f5346ff5e2364686
Olivia Simonet <olivia.simonet@mmibordeaux.com>
2022-06-20 15:18:03 +0200

    Front gallery/key figures/definitions updated

--- db/schema.rb
+++ db/schema.rb
@@ -504,23 +504,6 @@ ActiveRecord::Schema.define(version: 2022_06_09_085535) do
     t.index ["university_id"], name: "index_education_schools_on_university_id"
   end
 
-  create_table "external_organizations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.string "address"
-    t.string "zipcode"
-    t.string "city"
-    t.string "country"
-    t.string "website"
-    t.string "phone"
-    t.string "mail"
-    t.boolean "active"
-    t.string "sirene"
-    t.integer "kind"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "imports", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.integer "number_of_lines"
     t.jsonb "processing_errors"
@@ -715,8 +698,8 @@ ActiveRecord::Schema.define(version: 2022_06_09_085535) do
     t.string "linkedin"
     t.boolean "is_alumnus", default: false
     t.text "description_short"
-    t.string "name"
     t.boolean "is_author"
+    t.string "name"
     t.integer "gender"
     t.date "birthdate"
     t.string "phone_professional"
