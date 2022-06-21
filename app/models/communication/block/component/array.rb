a98277331973f1d49a4581a33eef0ef42ec0b3f8
Arnaud Levy <contact@arnaudlevy.com>
2022-06-21 12:21:56 +0200

    fix

--- app/models/communication/block/component/array.rb
+++ app/models/communication/block/component/array.rb
@@ -1,5 +1,10 @@
 class Communication::Block::Component::Array < Communication::Block::Component::Base
 
+  def data=(value)
+    # Nil values must be turned to ""
+    @data = value.map { |s| s.to_s }
+  end
+
   def default_data
     ['']
   end
