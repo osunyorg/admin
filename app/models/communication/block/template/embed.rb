8d3da1eeae1327fb9a9f068936a182a22f9a67c7
Olivia206 <olivia.simonet@mmibordeaux.com>
2022-06-22 10:10:33 +0200

    Delete input title from embed

--- app/models/communication/block/template/embed.rb
+++ app/models/communication/block/template/embed.rb
@@ -1,15 +1,15 @@
 class Communication::Block::Template::Embed < Communication::Block::Template::Base
 
   has_component :code, :code
-  has_component :iframe_title, :string
   has_component :transcription, :text
 
   protected
 
-  def check_accessibility
-    super
-    accessibility_error 'accessibility.blocks.templates.embed.title_missing' if  iframe_title.blank?
-    accessibility_error 'accessibility.blocks.templates.embed.transcription_missing' if transcription.blank?
-  end
+  
+  #def check_accessibility
+  #  super
+  #  accessibility_error 'accessibility.blocks.templates.embed.title_missing' if  iframe_title.blank?
+  #  accessibility_error 'accessibility.blocks.templates.embed.transcription_missing' if transcription.blank?
+  #end
 
 end
