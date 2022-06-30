9f536ef0744d75e6c047508877a1185f97910f02
Arnaud Levy <contact@arnaudlevy.com>
2022-06-30 19:01:39 +0200

    refactor

--- app/assets/javascripts/admin/communication/preview.js
+++ app/assets/javascripts/admin/communication/preview.js
@@ -1,17 +1,11 @@
 $(function() {
-    $('.preview__button__mobile').on('click', function(){
-      $('.preview__button').removeClass('btn-primary').addClass('btn-light');
-      $('.preview__button__mobile').removeClass('btn-light').addClass('btn-primary');
-      $('#preview').removeClass('preview--desktop').removeClass('preview--tablet').addClass('preview--mobile');
-    });
-    $('.preview__button__tablet').on('click', function(){
-      $('.preview__button').removeClass('btn-primary').addClass('btn-light');
-      $('.preview__button__tablet').removeClass('btn-light').addClass('btn-primary');
-      $('#preview').removeClass('preview--mobile').removeClass('preview--desktop').addClass('preview--tablet');
-    });
-    $('.preview__button__desktop').on('click', function(){
-      $('.preview__button').removeClass('btn-primary').addClass('btn-light');
-      $('.preview__button__desktop').removeClass('btn-light').addClass('btn-primary');
-      $('#preview').removeClass('preview--mobile').removeClass('preview--tablet').addClass('preview--desktop');
-    });
+  'use strict';
+  var modes = ['mobile', 'tablet', 'desktop'],
+      mode = '';
+  $('.preview__button').on('click', function(){
+    mode = $(this).data('mode');
+    $('.preview__button').removeClass('btn-primary').addClass('btn-light');
+    $(this).removeClass('btn-light').addClass('btn-primary');
+    $('#preview').removeClass('preview--desktop').removeClass('preview--tablet').removeClass('preview--mobile').addClass('preview--' + mode);
   });
+});
