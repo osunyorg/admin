389f1d528e4b37a19e10f595b2f042b3f7a03b9f
Arnaud Levy <contact@arnaudlevy.com>
2022-07-06 17:54:12 +0200

    code  embed

--- app/models/communication/block/component/code.rb
+++ app/models/communication/block/component/code.rb
@@ -1,8 +1,4 @@
 class Communication::Block::Component::Code < Communication::Block::Component::Base
 
-    def data=(value)
-      @data = Osuny::Sanitizer.sanitize value, 'text'
-    end
-  
-  end
+end
   
