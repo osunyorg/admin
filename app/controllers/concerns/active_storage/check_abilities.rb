52d1d91dece4597392d8fe7cf82c83564b5ab92e
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-07-06 17:02:27 +0200

    active storage

--- app/controllers/concerns/active_storage/check_abilities.rb
+++ app/controllers/concerns/active_storage/check_abilities.rb
@@ -7,7 +7,14 @@ module ActiveStorage::CheckAbilities
     render(file: Rails.root.join('public/403.html'), formats: [:html], status: 403, layout: false) and return if current_university.present? && @blob.university_id != current_university.id
   end
 
+  def current_extranet
+    @current_extranet ||= Communication::Extranet.with_host(request.host)
+  end
+
   def current_university
-    @current_university ||= University.with_host(request.host)
+    @current_university ||= begin
+      current_extranet.present? ? current_extranet.university
+                                : University.with_host(request.host)
+    end
   end
 end
