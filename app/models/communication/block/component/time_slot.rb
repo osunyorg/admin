bd47bcdba4b162fe5bb74b86fca4dbdd182c44a8
Arnaud Levy <contact@arnaudlevy.com>
2022-07-26 21:23:42 +0200

    better

--- app/models/communication/block/component/time_slot.rb
+++ app/models/communication/block/component/time_slot.rb
@@ -1,5 +1,21 @@
 class Communication::Block::Component::TimeSlot < Communication::Block::Component::Base
 
+  def from
+    data['from']
+  end
+  
+  def to
+    data['to']
+  end
+
+  def present?
+    from && from != '0' && to && to != '0' 
+  end
+
+  def blank?
+    !present?
+  end
+
   def default_data
     {
       'from' => '0',
--- app/models/communication/website/with_style.rb
+++ app/models/communication/website/with_style.rb
@@ -2,7 +2,6 @@ module Communication::Website::WithStyle
     extend ActiveSupport::Concern
 
   def preview_style
-    return '' if url.blank?
     load_style if style_outdated? 
     style
   end
@@ -14,6 +13,7 @@ module Communication::Website::WithStyle
     load_style_from url 
     load_style_from "#{url}/fr" if @style.blank?
     load_style_from "#{url}/en" if @style.blank?
+    load_style_from "https://example.osuny.org" if @style.blank?
     substitute_fonts_urls_in_style!
     self.update_columns style: @style, 
                         style_updated_at: Date.today
@@ -26,6 +26,7 @@ module Communication::Website::WithStyle
     css_files.each do |css_url|
       add_css_url_to_style css_url.to_s
     end
+  rescue
   end
 
   def add_css_url_to_style(css_url)
