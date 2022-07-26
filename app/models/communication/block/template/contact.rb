aa3572357da5fdfa3e4f3b424914c8c2a4c0c62a
Arnaud Levy <contact@arnaudlevy.com>
2022-07-26 20:54:09 +0200

    Clean contact

--- app/models/communication/block/template/contact.rb
+++ app/models/communication/block/template/contact.rb
@@ -1,9 +1,12 @@
 class Communication::Block::Template::Contact < Communication::Block::Template::Base
 
   has_component :name, :string
+  has_component :address, :text
+  has_component :zipcode, :string
+  has_component :city, :string
+  has_component :country, :string
   has_component :phone_numbers, :array
   has_component :emails, :array
-  has_component :address, :text
 
   has_elements
 
