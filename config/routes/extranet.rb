60087f16e31e9168d79669cf3eb6c8cf5435826f
Arnaud Levy <contact@arnaudlevy.com>
2022-08-25 11:42:08 +0200

    extranet account wip

--- config/routes/extranet.rb
+++ config/routes/extranet.rb
@@ -1,9 +1,12 @@
 get 'cohorts'           => 'extranet/cohorts#index', as: :education_cohorts
 get 'cohorts/:id'       => 'extranet/cohorts#show', as: :education_cohort
 get 'organizations'     => 'extranet/organizations#index', as: :university_organizations
-get 'organizations/:id'  => 'extranet/organizations#show', as: :university_organization
+get 'organizations/:id' => 'extranet/organizations#show', as: :university_organization
 get 'persons'           => 'extranet/persons#index', as: :university_persons
 get 'persons/:id'       => 'extranet/persons#show', as: :university_person
 get 'years'             => 'extranet/academic_years#index', as: :education_academic_years
 get 'years/:id'         => 'extranet/academic_years#show', as: :education_academic_year
+get 'account'           => 'extranet/account#show', as: :account 
+get 'account/edit'      => 'extranet/account#edit', as: :edit_account 
+patch 'account'         => 'extranet/account#update'
 root to: 'extranet/home#index'
