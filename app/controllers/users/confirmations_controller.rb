129bd5febc98d9b027fa46fa1eb06f3ef89f9e0e
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-09-15 12:28:51 +0200

    resend confirmation banner

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,3 +1,12 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
   include Users::AddUniversityToRequestParams
+
+  def resend
+    unless signed_in_resource.confirmed?
+      signed_in_resource.resend_confirmation_instructions
+      redirect_back(fallback_location: admin_root_path, notice: t('devise.confirmations.send_instructions'))
+    else
+      redirect_back(fallback_location: admin_root_path, alert: t('admin.users_alerts.already_confirmed'))
+    end
+  end
 end
