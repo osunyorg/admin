8998c2d8b3094e7d4e7be6bf482210af7e0210b9
alexisben <alex@noesya.coop>
2022-10-06 16:15:08 +0200

    fix autocollapse sidebar #531

---
+++ app/assets/javascripts/admin/commons/nav.js
@@ -0,0 +1,12 @@
+function autoCollapseSidebar() {
+  var sidebar = document.querySelector('#sidebar[data-auto-collapsed]');
+  if (sidebar && window.innerWidth >= 992) {
+    sidebar.classList.add('collapsed');
+    sidebar.removeAttribute('data-auto-collapsed');
+  }
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+  autoCollapseSidebar();
+});
