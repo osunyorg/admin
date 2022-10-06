5239dc28c765f7a4afe3accb5bd65aaa9cf97933
pabois <pierreandre.boissinot@noesya.coop>
2022-10-06 16:22:50 +0200

    close #531

--- app/assets/javascripts/admin/commons/nav.js
+++ app/assets/javascripts/admin/commons/nav.js
@@ -1,12 +1,13 @@
-function autoCollapseSidebar() {
-  var sidebar = document.querySelector('#sidebar[data-auto-collapsed]');
-  if (sidebar && window.innerWidth >= 992) {
-    sidebar.classList.add('collapsed');
-    sidebar.removeAttribute('data-auto-collapsed');
-  }
-}
+var autoCollapseSidebar = function () {
+    'use strict';
+    var sidebar = document.querySelector('#sidebar[data-auto-collapsed]');
+    if (sidebar && window.innerWidth >= 992) {
+        sidebar.classList.add('collapsed');
+        sidebar.removeAttribute('data-auto-collapsed');
+    }
+};
 
 window.addEventListener('DOMContentLoaded', function () {
-  'use strict';
-  autoCollapseSidebar();
+    'use strict';
+    autoCollapseSidebar();
 });
