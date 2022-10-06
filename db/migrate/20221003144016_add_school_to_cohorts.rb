f8abf8dab3aadabb56d9a256ded8c2a4e71dc669
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-10-06 10:50:26 +0200

    handle school-less programs in cohorts migration

--- db/migrate/20221003144016_add_school_to_cohorts.rb
+++ db/migrate/20221003144016_add_school_to_cohorts.rb
@@ -2,8 +2,11 @@ class AddSchoolToCohorts < ActiveRecord::Migration[6.1]
   def change
     add_reference :education_cohorts, :school, foreign_key: {to_table: :education_schools}, type: :uuid
     Education::Cohort.all.each do |cohort|
-      cohort.school_id = cohort.program.schools.first.id
-      cohort.save
+      if cohort.program.schools.any?
+        cohort.update(school_id: cohort.program.schools.first.id)
+      else
+        cohort.destroy
+      end
     end
     change_column_null :education_cohorts, :school_id, false
   end
