4d9f9871f082b30a1f75de682e9f43f5965bb453
Arnaud Levy <contact@arnaudlevy.com>
2022-10-20 17:55:15 +0200

    nav

--- config/extranet_navigation.rb
+++ config/extranet_navigation.rb
@@ -16,11 +16,15 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.item  :organizations,
                   University::Organization.model_name.human(count: 2),
                   university_organizations_path
-    primary.item  :personal_data,
-                  t('extranet.personal_data.title'),
-                  personal_data_path
     primary.item  :account,
-                  t('extranet.account.my'),
-                  account_path
+                  current_user.to_s,
+                  nil do |secondary|
+      secondary.item  :account,
+                    t('extranet.account.my'),
+                    account_path
+      secondary.item  :personal_data,
+                    t('extranet.personal_data.title'),
+                    personal_data_path
+    end
   end
 end
