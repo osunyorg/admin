6f3699316bfa5c0c1935725409bd48c7d33e92e8
Arnaud Levy <contact@arnaudlevy.com>
2022-10-24 11:10:40 +0200

    Good looking...

---
+++ config/extranet_navigation.rb
@@ -0,0 +1,20 @@
+SimpleNavigation::Configuration.run do |navigation|
+  navigation.renderer = ::SimpleNavigation::BootstrapRenderer
+  navigation.auto_highlight = true
+  navigation.highlight_on_subpath = true
+  navigation.selected_class = 'active'
+  navigation.items do |primary|
+    primary.item  :person,
+                  University::Person::Alumnus.model_name.human(count: 2),
+                  university_persons_path
+    primary.item  :years,
+                  Education::AcademicYear.model_name.human(count: 2),
+                  education_academic_years_path if current_extranet.should_show_years?
+    primary.item  :cohorts,
+                  Education::Cohort.model_name.human(count: 2),
+                  education_cohorts_path
+    primary.item  :organizations,
+                  University::Organization.model_name.human(count: 2),
+                  university_organizations_path
+  end
+end
