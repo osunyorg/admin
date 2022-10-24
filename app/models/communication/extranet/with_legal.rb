02988fcf5cafd376bd8775a121bda6215e29592a
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-10-24 18:13:32 +0200

    fix

--- app/models/communication/extranet/with_legal.rb
+++ app/models/communication/extranet/with_legal.rb
@@ -6,14 +6,14 @@ module Communication::Extranet::WithLegal
   end
 
   def has_terms?
-    strip_tags(terms).strip.present?
+    strip_tags(terms).to_s.strip.present?
   end
 
   def has_cookies_policy?
-    strip_tags(cookies_policy).strip.present?
+    strip_tags(cookies_policy).to_s.strip.present?
   end
 
   def has_privacy_policy?
-    strip_tags(privacy_policy).strip.present?
+    strip_tags(privacy_policy).to_s.strip.present?
   end
 end
