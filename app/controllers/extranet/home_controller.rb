1a1a5fdaebc8ad712a46276abd6663ee0f281e92
Arnaud Levy <contact@arnaudlevy.com>
2022-10-26 12:54:35 +0200

    recent

--- app/controllers/extranet/home_controller.rb
+++ app/controllers/extranet/home_controller.rb
@@ -1,6 +1,6 @@
 class Extranet::HomeController < Extranet::ApplicationController
   def index
     @cohorts = about&.education_cohorts.ordered.limit(5)
-    @experiences = about&.university_person_experiences.ordered.limit(10)
+    @experiences = about&.university_person_experiences.recent
   end
 end
