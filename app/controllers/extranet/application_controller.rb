300788c02de2977186c6a5c06b3f1b6a267286c9
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-10-26 11:56:07 +0200

    fix devise js + extranet redirection when visitor

--- app/controllers/extranet/application_controller.rb
+++ app/controllers/extranet/application_controller.rb
@@ -1,7 +1,8 @@
 class Extranet::ApplicationController < ApplicationController
   layout 'extranet/layouts/application'
 
-  before_action :authorize_extranet_access!
+  before_action :redirect_if_no_extranet!,
+                :authorize_extranet_access!
 
   def breadcrumb
     add_breadcrumb t('home'), root_path
@@ -12,6 +13,10 @@ class Extranet::ApplicationController < ApplicationController
   end
 
   private
+
+  def redirect_if_no_extranet!
+    redirect_to admin_root_path unless current_extranet
+  end
 
   def authorize_extranet_access!
     raise CanCan::AccessDenied if current_user.visitor? && about.alumni.find_by(id: current_user.person&.id).nil?
--- app/controllers/extranet/home_controller.rb
+++ app/controllers/extranet/home_controller.rb
@@ -1,6 +1,5 @@
 class Extranet::HomeController < Extranet::ApplicationController
   def index
-    return redirect_to admin_root_path unless current_extranet
     @cohorts = about&.education_cohorts.ordered.limit(5)
     @experiences = about&.university_person_experiences.ordered.limit(10)
   end
