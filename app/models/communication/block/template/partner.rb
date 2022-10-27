113e916986a59e2d9e81288eb4f932c1a037becf
Arnaud Levy <contact@arnaudlevy.com>
2022-10-27 12:13:04 +0200

    Better organization block

--- app/models/communication/block/template/partner.rb
+++ app/models/communication/block/template/partner.rb
@@ -2,5 +2,15 @@ class Communication::Block::Template::Partner < Communication::Block::Template::
 
   has_elements
   has_component :description, :rich_text
+  has_component :with_link, :boolean
+  has_component :alphabetical, :boolean
 
+  def elements
+    if alphabetical
+      @elements.sort_by! do |element|
+        "#{element.best_name&.parameterize&.downcase}"
+      end
+    end
+    @elements
+  end
 end
