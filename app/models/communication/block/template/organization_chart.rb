9da1ef7811e7e10eda4257b9dd80f052c4e83bc7
Arnaud Levy <contact@arnaudlevy.com>
2022-10-27 11:10:18 +0200

    better blocks

--- app/models/communication/block/template/organization_chart.rb
+++ app/models/communication/block/template/organization_chart.rb
@@ -4,5 +4,14 @@ class Communication::Block::Template::OrganizationChart < Communication::Block::
   has_component :description, :rich_text
   has_component :with_link, :boolean
   has_component :with_photo, :boolean
+  has_component :alphabetical, :boolean
 
+  def elements
+    if alphabetical
+      @elements.sort_by! do |element|
+        "#{element.person&.last_name&.parameterize&.downcase}"
+      end
+    end
+    @elements
+  end
 end
