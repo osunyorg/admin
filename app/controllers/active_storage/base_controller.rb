49294a348e4ed428466b2389af96581274c9f195
pabois <pierreandre.boissinot@noesya.coop>
2022-11-03 11:21:49 +0100

    update base controller

--- app/controllers/active_storage/base_controller.rb
+++ app/controllers/active_storage/base_controller.rb
@@ -9,4 +9,13 @@ class ActiveStorage::BaseController < ActionController::Base
   protect_from_forgery with: :exception
 
   self.etag_with_template_digest = false
+
+  private
+    def stream(blob)
+      blob.download do |chunk|
+        response.stream.write chunk
+      end
+    ensure
+      response.stream.close
+    end
 end
