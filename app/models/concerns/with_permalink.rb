20923682748e7a4c45f40d33974db1578a81fe3b
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-12-05 15:37:54 +0100

    rename

--- app/models/concerns/with_permalink.rb
+++ app/models/concerns/with_permalink.rb
@@ -23,8 +23,8 @@ module WithPermalink
     new_permalink = new_permalink_in_website(website)
 
     # If the object had no permalink or if its path changed, we create a new permalink
-    if new_permalink.computed_path.present? && (last_permalink.nil? || last_permalink.path != new_permalink.computed_path)
-      last_permalink&.update(is_current: false)
+    if new_permalink.computed_path.present? && (current_permalink.nil? || current_permalink.path != new_permalink.computed_path)
+      current_permalink&.update(is_current: false)
       new_permalink.path = new_permalink.computed_path
       new_permalink.save
     end
