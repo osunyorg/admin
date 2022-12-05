6e16b25cac40c2d5b43e93b7ee8a120d7d2d8b1c
Arnaud Levy <contact@arnaudlevy.com>
2022-12-05 15:06:05 +0100

    wip

--- app/models/communication/website/permalink/post.rb
+++ app/models/communication/website/permalink/post.rb
@@ -8,6 +8,7 @@ class Communication::Website::Permalink::Post < Communication::Website::Permalin
   end
 
   # /actualites/2022/10/21/un-article/
+  # Pas de /fr au début, parce qu'Hugo a besoin du permalink sans langue
   def self.pattern_in_website(website)
     "#{website.special_page(:communication_posts).path_without_language}:year/:month/:day/:slug/"
   end
