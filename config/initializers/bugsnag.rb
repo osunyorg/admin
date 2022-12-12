bf06eb0f5162f972b956d2914115826a773f2651
pabois <pierreandre.boissinot@noesya.coop>
2022-12-12 13:32:59 +0100

    add bugsnag parameter

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -2,4 +2,5 @@ Bugsnag.configure do |config|
   config.api_key = ENV['BUGSNAG_RUBY_KEY']
   config.release_stage = ENV['APPLICATION_ENV']
   config.notify_release_stages = ['production', 'staging']
+  config.meta_data_filters += ['access_token', 'sso_cert']
 end
