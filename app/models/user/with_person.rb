1a245cf4f9d0b23ec326bf810fbfa379fc9ee552
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-12-26 16:37:17 +0100

    do not sync email

--- app/models/user/with_person.rb
+++ app/models/user/with_person.rb
@@ -13,11 +13,10 @@ module User::WithPerson
   protected
 
   def find_or_create_person
-    person = university.people.where(email: email).first_or_initialize do |person|
-      person.first_name = first_name
-      person.last_name = last_name
-      person.slug = person.to_s.parameterize
-    end
+    person = university.people.where(email: email).first || university.people.new
+    person.first_name = first_name
+    person.last_name = last_name
+    person.slug = person.to_s.parameterize
     person.user = self
     person.save
   end
@@ -25,7 +24,6 @@ module User::WithPerson
   def sync_person
     person.first_name = first_name
     person.last_name = last_name
-    person.email = email
     person.slug = person.to_s.parameterize
     person.picture.purge if picture_infos.present? && person.picture&.attached?
     person.save
