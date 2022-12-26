8084d5b6dc86235006366a9543985927439a1c33
Arnaud Levy <contact@arnaudlevy.com>
2022-12-26 15:35:19 +0100

    precompute

--- app/models/concerns/with_blocks.rb
+++ app/models/concerns/with_blocks.rb
@@ -8,4 +8,9 @@ module WithBlocks
   def git_block_dependencies
     blocks.collect &:git_dependencies
   end
+
+  # Basic rule is: TOC if 2 titles or more
+  def show_toc?
+    (blocks.collect(&:title).uniq.compact - ['']).many?
+  end
 end
