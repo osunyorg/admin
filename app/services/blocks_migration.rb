329d92545067997caa0fec2d93b2745ed2b5e675
Arnaud Levy <contact@arnaudlevy.com>
2023-01-02 14:02:12 +0100

    dry run test

--- app/services/blocks_migration.rb
+++ app/services/blocks_migration.rb
@@ -9,11 +9,12 @@ class BlocksMigration
       puts "#{post} (#{post.id}, #{post.university})"
       next if post.blocks.any?
       puts "  migrating"
-      block = post.blocks.create university: post.university, template_kind: :chapter
-      data = block.data
-      data['text'] = post.text.to_html
-      block.data = data
-      block.save
+      puts post.text.to_html
+      # block = post.blocks.create university: post.university, template_kind: :chapter
+      # data = block.data
+      # data['text'] = post.text.to_html
+      # block.data = data
+      # block.save
     end
   end
 end
