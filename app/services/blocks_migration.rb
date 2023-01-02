d924dfdf5109ff55e9aae80fcd56b5a1326d0199
Arnaud Levy <contact@arnaudlevy.com>
2023-01-02 14:06:12 +0100

    complexity

--- app/services/blocks_migration.rb
+++ app/services/blocks_migration.rb
@@ -6,15 +6,21 @@ class BlocksMigration
   def self.cleanup
     Communication::Website::Post.find_each do |post|
       next if post.text.blank?
-      puts "#{post} (#{post.id}, #{post.university})"
-      next if post.blocks.any?
-      puts "  migrating"
-      puts post.text.to_html
-      # block = post.blocks.create university: post.university, template_kind: :chapter
-      # data = block.data
-      # data['text'] = post.text.to_html
-      # block.data = data
-      # block.save
+      cleanup_post post
     end
+  end
+
+  private
+
+  def self.cleanup_post(post)
+    puts "#{post} (#{post.id}, #{post.university})"
+    return if post.blocks.any?
+    puts "  migrating"
+    puts post.text.to_html
+    # block = post.blocks.create university: post.university, template_kind: :chapter
+    # data = block.data
+    # data['text'] = post.text.to_html
+    # block.data = data
+    # block.save
   end
 end
