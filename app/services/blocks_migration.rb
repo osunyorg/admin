50a1610d00db498bb2d1a0caa9c5bd2cba1731f1
Arnaud Levy <contact@arnaudlevy.com>
2023-01-02 15:07:13 +0100

    only migrated items

--- app/services/blocks_migration.rb
+++ app/services/blocks_migration.rb
@@ -4,10 +4,10 @@ require 'communication/block/template/chapter'
 class BlocksMigration
 
   def self.cleanup
-    Communication::Website::Post.order(:website).find_each do |post|
+    Communication::Website::Post.order(:website_id).find_each do |post|
       cleanup_item post
     end
-    Communication::Website::Page.order(:website).find_each do |page|
+    Communication::Website::Page.order(:website_id).find_each do |page|
       cleanup_item page
     end
   end
@@ -16,9 +16,9 @@ class BlocksMigration
 
   def self.cleanup_item(item)
     return if item.text.blank?
-    puts "#{item.university}, #{item.website}, #{item.id}, #{item}"
     return if item.blocks.any?
-    puts "  migrating"
+    puts "#{item.university}, #{item.website}, #{item.id}, #{item}"
+    # puts "  migrating"
     # puts item.text.to_html
     return
     block = item.blocks.create university: item.university, template_kind: :chapter
