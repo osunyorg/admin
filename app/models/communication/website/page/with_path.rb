5eedae4cc5d40fff51d9752feca2d399d106367a
pabois <pierreandre.boissinot@noesya.coop>
2023-01-11 10:37:28 +0100

    improve readibility

--- app/models/communication/website/page/with_path.rb
+++ app/models/communication/website/page/with_path.rb
@@ -29,7 +29,10 @@ module Communication::Website::Page::WithPath
     return unless published
     return if website.url.blank?
     # do not use a global Static.clean here because url has protocol with 2 slashes!
-    "#{website.url.end_with?('/') ? website.url[0..-2] : website.url}#{Static.clean_path path}"
+    website_url = website.url
+    # remove trailing slash if needed, because path begins with a slash
+    website_url = website_url[0..-2] if website_url[0..-2].end_with?('/')
+    "#{website_url}#{Static.clean_path path}"
   end
 
   protected
