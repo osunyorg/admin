f5c46c57dd009ba155406c214b59f09328a6d644
pabois <pierreandre.boissinot@noesya.coop>
2023-01-23 10:31:35 +0100

    wip

--- app/models/university/person/administrator.rb
+++ app/models/university/person/administrator.rb
@@ -2,87 +2,39 @@
 #
 # Table name: university_people
 #
-#  id                    :uuid             not null, primary key
-#  address               :string
-#  biography             :text
-#  birthdate             :date
-#  city                  :string
-#  country               :string
-#  email                 :string
-#  first_name            :string
-#  gender                :integer
-#  habilitation          :boolean          default(FALSE)
-#  is_administration     :boolean
-#  is_alumnus            :boolean          default(FALSE)
-#  is_author             :boolean
-#  is_researcher         :boolean
-#  is_teacher            :boolean
-#  last_name             :string
-#  linkedin              :string
-#  mastodon              :string
-#  meta_description      :text
-#  name                  :string
-#  phone_mobile          :string
-#  phone_personal        :string
-#  phone_professional    :string
-#  slug                  :string
-#  summary               :text
-#  tenure                :boolean          default(FALSE)
-#  twitter               :string
-#  url                   :string
-#  zipcode               :string
-#  created_at            :datetime         not null
-#  updated_at            :datetime         not null
-#  university_id         :uuid             not null, indexed
-#  user_id               :uuid             indexed
-#
-# Indexes
-#
-#  index_university_people_on_university_id  (university_id)
-#  index_university_people_on_user_id        (user_id)
-#
-# Foreign Keys
-#
-#  fk_rails_b47a769440  (user_id => users.id)
-#  fk_rails_da35e70d61  (university_id => universities.id)
-#
-# == Schema Information
-#
-# Table name: university_people
-#
-#  id                    :uuid             not null, primary key
-#  address               :string
-#  biography             :text
-#  birthdate             :date
-#  city                  :string
-#  country               :string
-#  email                 :string
-#  first_name            :string
-#  gender                :integer
-#  habilitation          :boolean          default(FALSE)
-#  is_administration     :boolean
-#  is_alumnus            :boolean          default(FALSE)
-#  is_author             :boolean
-#  is_researcher         :boolean
-#  is_teacher            :boolean
-#  last_name             :string
-#  linkedin              :string
-#  mastodon              :string
-#  meta_description      :text
-#  name                  :string
-#  phone_mobile          :string
-#  phone_personal        :string
-#  phone_professional    :string
-#  slug                  :string
-#  summary               :text
-#  tenure                :boolean          default(FALSE)
-#  twitter               :string
-#  url                   :string
-#  zipcode               :string
-#  created_at            :datetime         not null
-#  updated_at            :datetime         not null
-#  university_id         :uuid             not null, indexed
-#  user_id               :uuid             indexed
+#  id                 :uuid             not null, primary key
+#  address            :string
+#  biography          :text
+#  birthdate          :date
+#  city               :string
+#  country            :string
+#  email              :string
+#  first_name         :string
+#  gender             :integer
+#  habilitation       :boolean          default(FALSE)
+#  is_administration  :boolean
+#  is_alumnus         :boolean          default(FALSE)
+#  is_author          :boolean
+#  is_researcher      :boolean
+#  is_teacher         :boolean
+#  last_name          :string
+#  linkedin           :string
+#  mastodon           :string
+#  meta_description   :text
+#  name               :string
+#  phone_mobile       :string
+#  phone_personal     :string
+#  phone_professional :string
+#  slug               :string
+#  summary            :text
+#  tenure             :boolean          default(FALSE)
+#  twitter            :string
+#  url                :string
+#  zipcode            :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  university_id      :uuid             not null, indexed
+#  user_id            :uuid             indexed
 #
 # Indexes
 #
