26561686f34abd236d0981657d198ecfdd178b71
Sébastien Gaya <sebastien.gaya@gmail.com>
2023-02-09 14:48:24 +0100

    feedback

--- app/models/concerns/with_git.rb
+++ app/models/concerns/with_git.rb
@@ -13,13 +13,11 @@ module WithGit
   end
 
   def git_path_content_prefix(website)
-    # Handle legacy language-less websites
+    # Handle language-less objects
     # TODO I18n: Right now, we use the language of the object, fallbacking on the language of the website. In the end, we'll only use the language of the object
     path = "content/"
-    if website.languages.any?
-      path_language = respond_to?(:language_id) && language_id.present? ? language : website.default_language
-      path += "#{path_language.iso_code}/"
-    end
+    path_language = respond_to?(:language_id) && language_id.present? ? language : website.default_language
+    path += "#{path_language.iso_code}/"
     path
   end
 
