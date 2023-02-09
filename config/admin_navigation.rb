d3061ac4ade51a7403fbfe01f869a8681e8af70c
Arnaud Levy <contact@arnaudlevy.com>
2023-02-09 10:27:09 +0100

    images

--- config/admin_navigation.rb
+++ config/admin_navigation.rb
@@ -17,80 +17,39 @@ SimpleNavigation::Configuration.run do |navigation|
 
   navigation.items do |primary|
     if current_admin_theme == 'appstack'
-      primary.item  :dashboard, t('admin.dashboard'),
-                    admin_root_path, 
-                    { icon: Icon::DASHBOARD, highlights_on: /admin$/ }
+      primary.item  :dashboard, t('admin.dashboard'), admin_root_path,  { icon: Icon::DASHBOARD, highlights_on: /admin$/ }
     end
 
     if can?(:read, Education::Program)
-      primary.item :education,
-                    Education.model_name.human,
-                    admin_education_root_path,
-                    { kind: :header }
+      primary.item :education, Education.model_name.human, admin_education_root_path, { kind: :header, image: 'admin/education-thumb.jpg' }
       load_from_parts Education, primary
-      primary.item :education,
-                    'Ressources éducatives',
-                    nil,
-                    { icon: Icon::EDUCATION_RESOURCES }
-      primary.item :education,
-                    'Feedbacks',
-                    nil,
-                    { icon: Icon::EDUCATION_FEEDBACKS }
+      primary.item :education, 'Ressources éducatives', nil, { icon: Icon::EDUCATION_RESOURCES }
+      primary.item :education, 'Feedbacks', nil, { icon: Icon::EDUCATION_FEEDBACKS }
     end
 
     if can?(:read, Research::Journal) || can?(:read, Research::Laboratory)
-      primary.item :research,
-                    Research.model_name.human,
-                    admin_research_root_path,
-                    { kind: :header }
+      primary.item :research, Research.model_name.human, admin_research_root_path, { kind: :header, image: 'admin/research-thumb.jpg' }
       load_from_parts Research, primary
-      primary.item :research_watch,
-                    'Veille',
-                    nil,
-                    { icon: Icon::RESEARCH_WATCH }
+      primary.item :research_watch, 'Veille', nil, { icon: Icon::RESEARCH_WATCH }
     end
 
     if can?(:read, Communication::Website)
-      primary.item :communication,
-                    Communication.model_name.human,
-                    admin_communication_root_path,
-                    { kind: :header }
+      primary.item :communication, Communication.model_name.human, admin_communication_root_path, { kind: :header, image: 'admin/communication-thumb.jpg' }
       load_from_parts Communication, primary
-      primary.item :communication_newsletters,
-                    'Lettres d\'information',
-                    nil,
-                    { icon: Icon::COMMUNICATION_NEWSLETTERS }
+      primary.item :communication_newsletters, 'Lettres d\'information', nil, { icon: Icon::COMMUNICATION_NEWSLETTERS }
     end
 
     if can?(:read, Administration::Qualiopi::Criterion)
-      primary.item :administration,
-                    Administration.model_name.human,
-                    admin_administration_root_path,
-                    { kind: :header }
+      primary.item :administration, Administration.model_name.human, admin_administration_root_path, { kind: :header, image: 'admin/administration-thumb.jpg' }
       load_from_parts Administration, primary
-      primary.item :administration_campus,
-                    'Campus',
-                    nil,
-                    { icon: Icon::ADMINISTRATION_CAMPUS }
-      primary.item :administration_admissions,
-                    'Admissions',
-                    nil,
-                    { icon: Icon::ADMINISTRATION_ADMISSIONS }
-      primary.item :administration_internship,
-                    'Stages',
-                    nil,
-                    { icon: Icon::ADMINISTRATION_INTERNSHIPS }
-      primary.item :administration_statistics,
-                    'Statistiques',
-                    nil,
-                    { icon: Icon::ADMINISTRATION_STATISTICS }
+      primary.item :administration_campus, 'Campus', nil, { icon: Icon::ADMINISTRATION_CAMPUS }
+      primary.item :administration_admissions, 'Admissions', nil, { icon: Icon::ADMINISTRATION_ADMISSIONS }
+      primary.item :administration_internship, 'Stages', nil, { icon: Icon::ADMINISTRATION_INTERNSHIPS }
+      primary.item :administration_statistics, 'Statistiques', nil, { icon: Icon::ADMINISTRATION_STATISTICS }
     end
 
     if can?(:read, University::Person) || can?(:read, University::Organization)
-      primary.item :university,
-                    University.model_name.human,
-                    admin_university_root_path,
-                    { kind: :header }
+      primary.item :university, University.model_name.human, admin_university_root_path, { kind: :header, image: 'admin/university-thumb.jpg' }
       load_from_parts University, primary
     end
 
