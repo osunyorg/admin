4448e489acf03a20f3f617a71909a0f7f69e45e0
Sébastien Gaya <sebastien.gaya@gmail.com>
2023-02-10 15:01:46 +0100

    breadcrumb

--- app/controllers/admin/communication/websites_controller.rb
+++ app/controllers/admin/communication/websites_controller.rb
@@ -77,12 +77,6 @@ class Admin::Communication::WebsitesController < Admin::Communication::Websites:
 
   protected
 
-  def breadcrumb
-    super
-    add_breadcrumb Communication::Website.model_name.human(count: 2), admin_communication_websites_path
-    breadcrumb_for @website
-  end
-
   def website_params
     attribute_names = [
       :name, :url, :repository, :access_token, :about_type, :about_id, :in_production,
