5208a70a664ff9c8b80c13fd932cf92ee1c1b809
Arnaud Levy <contact@arnaudlevy.com>
2023-02-13 09:37:05 +0100

    index on docid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_10_132818) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_13_083544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -697,6 +697,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_10_132818) do
     t.date "publication_date"
     t.string "doi"
     t.string "slug"
+    t.index ["docid"], name: "index_research_publications_on_docid"
   end
 
   create_table "research_publications_university_people", id: false, force: :cascade do |t|
@@ -811,11 +812,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_10_132818) do
     t.string "city"
     t.string "country"
     t.string "hal_person_identifier"
+    t.string "hal_doc_identifier"
+    t.string "hal_form_identifier"
     t.string "mastodon"
     t.uuid "language_id", null: false
     t.uuid "original_id"
-    t.string "hal_doc_identifier"
-    t.string "hal_form_identifier"
     t.index ["language_id"], name: "index_university_people_on_language_id"
     t.index ["original_id"], name: "index_university_people_on_original_id"
     t.index ["university_id"], name: "index_university_people_on_university_id"
