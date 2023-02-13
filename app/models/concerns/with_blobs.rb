8458c03d962624bb2ac3fff99150d166c5e9a7a1
Sébastien Gaya <sebastien.gaya@gmail.com>
2023-02-13 12:57:20 +0100

    remove summernote embeds + to_html

--- app/models/concerns/with_blobs.rb
+++ app/models/concerns/with_blobs.rb
@@ -13,33 +13,17 @@ module WithBlobs
     blobs_with_ids inherited_blob_ids
   end
 
-  def summernote_embeds
-    summernote_embeds_reflection_names.map { |summernote_reflection_name|
-      public_send(summernote_reflection_name)
-    }.flatten
-  end
-
   protected
 
   def explicit_blob_ids
-    [summernote_blob_ids]
+    []
   end
 
   def inherited_blob_ids
     []
   end
 
-  def summernote_blob_ids
-    summernote_embeds_reflection_names.map { |summernote_reflection_name|
-      public_send(summernote_reflection_name).pluck(:blob_id)
-    }.flatten
-  end
-
   def blobs_with_ids(ids)
     university.active_storage_blobs.where(id: ids.flatten.compact)
-  end
-
-  def summernote_embeds_reflection_names
-    @summernote_embeds_reflection_names ||= _reflections.keys.select { |name| name.ends_with?('_summernote_embeds_attachments') }
   end
 end
