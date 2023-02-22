7ff3e92bdc0f12f4e92fbcc048dc3a68f72a1844
Arnaud Levy <contact@arnaudlevy.com>
2023-02-22 07:36:39 +0100

    Fix #853

--- app/controllers/admin/university/people_controller.rb
+++ app/controllers/admin/university/people_controller.rb
@@ -93,8 +93,7 @@ class Admin::University::PeopleController < Admin::University::ApplicationContro
       :meta_description, :summary,
       :biography,  :picture, :picture_delete, :picture_infos,
       :habilitation, :tenure, :url, :linkedin, :twitter, :mastodon,
-      :is_researcher, :is_teacher, :is_administration, :is_alumnus,
-      :hal_person_identifier, :hal_doc_identifier, :hal_form_identifier, :user_id
+      :is_researcher, :is_teacher, :is_administration, :is_alumnus, :user_id
     ).merge(university_id: current_university.id)
   end
 end
