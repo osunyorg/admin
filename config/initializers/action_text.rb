2e69f35720f92eef1e75086b1508ade49dd26189
Sébastien Gaya <sebastien.gaya@gmail.com>
2023-02-23 11:01:02 +0100

    Remove initializer

--- config/initializers/action_text.rb
+++
@@ -1,8 +0,0 @@
-Rails.configuration.to_prepare do
-  ActionText::RichText.class_eval do
-    delegate :university, :university_id, to: :record
-  end
-
-  ActionText::ContentHelper.allowed_tags += Rails.application.config.action_view.sanitized_allowed_tags
-  ActionText::ContentHelper.allowed_attributes += Rails.application.config.action_view.sanitized_allowed_attributes
-end
