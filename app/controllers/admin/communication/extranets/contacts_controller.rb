b567f1d6741430f3770328bbed93f03236858c5a
Arnaud Levy <contact@arnaudlevy.com>
2023-03-06 17:36:52 +0100

    declarative connections

--- app/controllers/admin/communication/extranets/contacts_controller.rb
+++ app/controllers/admin/communication/extranets/contacts_controller.rb
@@ -1,7 +1,29 @@
 class Admin::Communication::Extranets::ContactsController < Admin::Communication::Extranets::ApplicationController
   def index
-    @persons = @extranet.connections.where(object_type: 'University::Person').page params[:page]
+    @persons = current_university.people.ordered
+    @organizations = current_university.organizations.ordered
     breadcrumb
     add_breadcrumb Communication::Extranet.human_attribute_name(:feature_contacts)
+  end
+
+  def connect
+    load_object
+    @extranet.connect @object
+    redirect_back(fallback_location: admin_communication_extranet_contacts_path(@extranet))
+  end
+
+  def disconnect
+    load_object
+    @extranet.disconnect @object
+    redirect_back(fallback_location: admin_communication_extranet_contacts_path(@extranet))
+  end
+
+
+  protected
+
+  def load_object
+    object_type = params[:objectType]
+    object_id = params[:objectId]
+    @object = object_type.constantize.find object_id
   end
 end
--- app/models/communication/extranet/with_connections.rb
+++ app/models/communication/extranet/with_connections.rb
@@ -4,5 +4,16 @@ module Communication::Extranet::WithConnections
   included do
     has_many :connections
   end
-  
+
+  def connected?(object)
+    connections.where(university: university, object: object).any?
+  end
+
+  def connect(object)
+    connections.where(university: university, object: object).first_or_create
+  end
+
+  def disconnect(object)
+    connections.where(university: university, object: object).destroy_all
+  end
 end
