95ecd8fd0761f5db9fc7e7362ecc5dd87537d6fd
pabois <pierreandre.boissinot@noesya.coop>
2023-03-09 11:31:25 +0100

    code climate

--- app/models/user/with_registration_context.rb
+++ app/models/user/with_registration_context.rb
@@ -9,14 +9,18 @@ module User::WithRegistrationContext
     private
 
     def extranet_access
-      unless (registration_context.has_feature?(:alumni) && registration_context.alumni.where(email: email).any?) || 
-             (registration_context.has_feature?(:contacts) && registration_context.connected_persons.where(email: email).any?)
+      unless user_can_access_registration_context?
         if registration_context.registration_contact.present?
           errors.add :email, I18n.t('extranet.errors.email_not_allowed_with_contact', contact: registration_context.registration_contact)
         else
           errors.add :email, I18n.t('extranet.errors.email_not_allowed')
         end
       end
+    end
+
+    def user_can_access_registration_context?
+      (registration_context.has_feature?(:alumni) && registration_context.alumni.where(email: email).any?) ||
+        (registration_context.has_feature?(:contacts) && registration_context.connected_persons.where(email: email).any?)
     end
 
   end
