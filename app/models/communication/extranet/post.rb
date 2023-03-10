44798a67f38988b78245297ff46da7f39eb59d10
pabois <pierreandre.boissinot@noesya.coop>
2023-03-10 09:58:31 +0100

    up

--- app/models/communication/extranet/post.rb
+++ app/models/communication/extranet/post.rb
@@ -13,12 +13,14 @@
 #  created_at            :datetime         not null
 #  updated_at            :datetime         not null
 #  author_id             :uuid             indexed
+#  category_id           :uuid             indexed
 #  extranet_id           :uuid             not null, indexed
 #  university_id         :uuid             not null, indexed
 #
 # Indexes
 #
 #  index_communication_extranet_posts_on_author_id      (author_id)
+#  index_communication_extranet_posts_on_category_id    (category_id)
 #  index_communication_extranet_posts_on_extranet_id    (extranet_id)
 #  index_communication_extranet_posts_on_university_id  (university_id)
 #
@@ -26,6 +28,7 @@
 #
 #  fk_rails_0232de42a1  (university_id => universities.id)
 #  fk_rails_4341823eab  (extranet_id => communication_extranets.id)
+#  fk_rails_7827da1fd1  (category_id => communication_extranet_post_categories.id)
 #  fk_rails_86cc935add  (author_id => university_people.id)
 #
 class Communication::Extranet::Post < ApplicationRecord
