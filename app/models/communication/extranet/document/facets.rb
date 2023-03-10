b2c07d7dc3575482533c3b15a5d9b836d15ea1de
Arnaud Levy <contact@arnaudlevy.com>
2023-03-10 18:15:04 +0100

    working facets

--- app/models/communication/extranet/document/facets.rb
+++ app/models/communication/extranet/document/facets.rb
@@ -2,12 +2,18 @@ class Communication::Extranet::Document::Facets < FacetedSearch::Facets
   def initialize(params, extranet)
     super(params)
     @model = extranet.documents
-    filter_with_text :name
-    # filter_with_list :category, {
-    #   source: extranet.document_categories
-    # }
-    # filter_with_list :kind, {
-    #   source: extranet.document_kinds
-    # }
+    filter_with_text :name, {
+      title: Communication::Extranet::Document.human_attribute_name('name')
+    }
+    filter_with_list :category, {
+      title: Communication::Extranet::Document::Category.model_name.human(count: 2),
+      source: extranet.document_categories,
+      habtm: false
+    }
+    filter_with_list :kind, {
+      title: Communication::Extranet::Document::Kind.model_name.human(count: 2),
+      source: extranet.document_kinds,
+      habtm: false
+    }
   end
 end
