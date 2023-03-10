c930908d15a4e54ff9c41b5017a5e565a03bec9a
Arnaud Levy <contact@arnaudlevy.com>
2023-03-10 09:23:46 +0100

    Models #889 #890

---
+++ app/models/communication/extranet/document/category.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: communication_extranet_document_categories
+#
+#  id            :uuid             not null, primary key
+#  name          :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  extranet_id   :uuid             not null, indexed
+#  university_id :uuid             not null, indexed
+#
+# Indexes
+#
+#  extranet_document_categories_universities                        (university_id)
+#  index_communication_extranet_document_categories_on_extranet_id  (extranet_id)
+#
+# Foreign Keys
+#
+#  fk_rails_6f2232d9f8  (university_id => universities.id)
+#  fk_rails_76e327b90f  (extranet_id => communication_extranets.id)
+#
+class Communication::Extranet::Document::Category < ApplicationRecord
+  belongs_to :extranet
+  belongs_to :university
+end
---
+++ app/models/communication/extranet/document/kind.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: communication_extranet_document_kinds
+#
+#  id            :uuid             not null, primary key
+#  name          :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  extranet_id   :uuid             not null, indexed
+#  university_id :uuid             not null, indexed
+#
+# Indexes
+#
+#  extranet_document_kinds_universities                        (university_id)
+#  index_communication_extranet_document_kinds_on_extranet_id  (extranet_id)
+#
+# Foreign Keys
+#
+#  fk_rails_27a9b91ed8  (extranet_id => communication_extranets.id)
+#  fk_rails_2a55cf899a  (university_id => universities.id)
+#
+class Communication::Extranet::Document::Kind < ApplicationRecord
+  belongs_to :extranet
+  belongs_to :university
+end
