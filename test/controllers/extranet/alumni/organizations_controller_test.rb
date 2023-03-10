cb64c33cdf552cc26bded60eb422aaf29d64dfa5
pabois <pierreandre.boissinot@noesya.coop>
2023-03-10 16:17:28 +0100

    fix alumni

--- test/controllers/extranet/alumni/organizations_controller_test.rb
+++ test/controllers/extranet/alumni/organizations_controller_test.rb
@@ -8,14 +8,6 @@ class Extranet::Alumni::OrganizationsControllerTest < ActionDispatch::Integratio
     assert_response(:success)
   end
 
-  def test_search
-    get alumni_search_university_organizations_path(term: "Organisation de test")
-    assert_response(:success)
-    results = JSON.parse(response.body)
-    assert_equal(1, results.size)
-    assert_equal("Organisation de test", results.first["label"])
-  end
-
   def test_show
     get alumni_university_organization_path(university_organizations(:default_organization))
     assert_response(:success)
