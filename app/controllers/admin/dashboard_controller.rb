a4b6b3773762831458cd8cf47984eb16d873a677
Sébastien Gaya <sebastien.gaya@gmail.com>
2023-03-13 15:43:40 +0100

    fix dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,10 +1,10 @@
 class Admin::DashboardController < Admin::ApplicationController
   def index
     @namespaces = []
-    @namespaces << Education if helpers.feature_education?
-    @namespaces << Research if helpers.feature_research?
-    @namespaces << Communication if helpers.feature_communication?
-    @namespaces << Administration if helpers.feature_administration?
+    @namespaces << Education if feature_education?
+    @namespaces << Research if feature_research?
+    @namespaces << Communication if feature_communication?
+    @namespaces << Administration if feature_administration?
     breadcrumb
   end
 end
