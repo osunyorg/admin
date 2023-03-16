9cd7d8b0fd0870c9ce0700a767047b8696ba60f0
Arnaud Levy <contact@arnaudlevy.com>
2023-03-16 16:04:33 +0100

    flatten

--- app/models/concerns/with_dependencies.rb
+++ app/models/concerns/with_dependencies.rb
@@ -24,7 +24,7 @@ module WithDependencies
     reference_dependencies.each do |dependency|
       add_dependency_to_array array, dependency
     end
-    array.compact
+    array.compact.flatten
   end
 
   protected
