4a449aa8b4f374ff8e00ca804406f592503e67e2
Arnaud Levy <contact@arnaudlevy.com>
2023-03-16 10:57:42 +0100

    clean

--- app/models/communication/website/with_connections.rb
+++ app/models/communication/website/with_connections.rb
@@ -14,28 +14,21 @@ module Communication::Website::WithConnections
   end
 
   def connect(object)
-    # On ne connecte pas le site à lui-même, ni un objet à lui-même    
+    # On ne connecte pas le site à lui-même
     connect_object object unless object.is_a?(Communication::Website)
     return unless object.respond_to?(:dependencies)
-    dependencies = object.dependencies
-    dependencies.each do |dependency|
+    object.dependencies.each do |dependency|
       connect_object dependency
     end
   end
 
-  # TODO pas pensé
   def disconnect(object)
     disconnect_object object
-    return unless object.respond_to?(:dependencies)
-    object.dependencies.each do |dependency|
-      disconnect_object dependency
-    end
   end
 
   protected
 
   def connect_object(object)
-    # puts "connect_object #{object} from #{source}"
     connection = connections.where(university: university, object: object).first_or_create
     connection.touch if connection.persisted?
   end
