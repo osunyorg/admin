03fe1a8630f640499835251026fdcde88a952c30
Sébastien Gaya <sebastien.gaya@gmail.com>
2023-03-17 12:02:06 +0100

    fix

--- app/controllers/extranet/pages_controller.rb
+++ app/controllers/extranet/pages_controller.rb
@@ -22,7 +22,7 @@ class Extranet::PagesController < Extranet::ApplicationController
       @metrics.concat [
         { value: current_extranet.alumni.count, name: University::Person::Alumnus.model_name.human(count: 2) },
         { value: current_extranet.academic_years.count, name: Education::AcademicYear.model_name.human(count: 2) },
-        { value: current_extranet.cohorts.count, name: Education::Cohort.model_name.human(count: 2) }
+        { value: current_extranet.cohorts.count, name: Education::Cohort.model_name.human(count: 2) },
         { value: current_extranet.about.university_person_alumni_organizations.count, name: University::Organization.model_name.human(count: 2) }
       ]
     end
