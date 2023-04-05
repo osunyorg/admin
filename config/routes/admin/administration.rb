ae4ff8954213a4d484b1e1fc7bbc9f7672963f5a
Arnaud Levy <contact@arnaudlevy.com>
2023-04-05 16:01:56 +0200

    Fix #817

--- config/routes/admin/administration.rb
+++ config/routes/admin/administration.rb
@@ -2,6 +2,7 @@ namespace :administration do
   namespace :qualiopi do
     resources :criterions, only: [:index, :show]
     resources :indicators, only: [:index, :show]
+    root to: 'criterions#index'
   end
   root to: 'dashboard#index'
 end
