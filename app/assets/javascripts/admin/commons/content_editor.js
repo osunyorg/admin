d3c0c27a8dfadeb7a6d4b342674aa91c7d001d90
Sébastien Gaya <sebastien.gaya@gmail.com>
2023-05-02 12:15:06 +0200

    wip content editor
    Co-authored-by: Arnaud Levy <arnaud.levy@noesya.coop>

--- app/assets/javascripts/admin/commons/content_editor.js
+++ app/assets/javascripts/admin/commons/content_editor.js
@@ -4,77 +4,111 @@ window.osuny.contentEditor = {
 
     init: function () {
         'use strict';
+        this.container = document.querySelector('.js-content-editor');
+        if (this.container === null) {
+            return;
+        }
+
+        this.initElements();
         this.initSortable();
+    },
+
+    initElements: function () {
+        'use strict';
+        var elementsContainers = this.container.querySelectorAll('.js-content-editor-element'),
+            elementInstance,
+            i;
+
+        this.elements = [];
+        for (i = 0 ; i < elementsContainers.length ; i += 1) {
+            elementInstance = new window.osuny.contentEditor.Element(elementsContainers[i]);
+            this.elements.push(elementInstance);
+        }
     },
 
     initSortable: function () {
         'use strict';
-        var nestedSortables,
+        var sortableContainers = this.container.querySelectorAll('.js-content-editor-sortable-container'),
+            sortableInstance,
             i;
 
-        nestedSortables = [].slice.call(document.querySelectorAll('.js-content-editor-sortable-container'));
-        for (i = 0; i < nestedSortables.length; i += 1) {
-            new Sortable(nestedSortables[i], {
+        this.sortableRootContainer = document.getElementById('content-editor-elements-root');
+        this.sortableInstances = [];
+
+        // var root = document.getElementById("content-editor-elements-root");
+
+        // onStart
+        // this.sortableRootContainer.classList.add("content-editor__elements__root--dragging")
+
+        // onEnd
+        // this.sortableRootContainer.classList.remove("content-editor__elements--root--dragging")
+
+        for (i = 0; i < sortableContainers.length; i += 1) {
+            sortableInstance = new Sortable(sortableContainers[i], {
                 group: 'nested',
                 animation: 150,
                 fallbackOnBody: true,
                 swapThreshold: 0.65,
-                onEnd: function (evt) {
-                    var item = evt.item,
-                        from = evt.from,
-                        to = evt.to,
-                        ids = [],
-                        parentId,
-                        url;
-
-                    // get list of ids
-                    $('> .js-content-editor-element', to).each(function () {
-                        ids.push($(this).attr('data-id'));
-                    });
-
-                    // as the "to" can be the root object where the data-sort-url is set we use "closest" and not "parents"
-                    url = $(to).closest('.js-content-editor-sortable')
-                        .attr('data-sort-url');
-                    parentId = to.dataset.id;
-
-                    // manage emptyness
-                    $(to).closest('.js-content-editor-element')
-                        .removeClass('content-editor__element--empty');
-                    if ($('> .js-content-editor-element', from).length === 0) {
-                        $(from).closest('.js-content-editor-element')
-                            .addClass('content-editor__element--empty');
-                    }
-
-                    // call to application
-                    $.post(url, {
-                        oldParentId: from.dataset.id,
-                        parentId: parentId,
-                        ids: ids,
-                        itemId: item.dataset.id
-                    });
-                }
+                onStart: this.onSortableStart.bind(this),
+                onEnd: this.onSortableEnd.bind(this)
             });
+            this.sortableInstances.push(sortableInstance);
         }
     },
 
-    branchClicked: function (e) {
+    onSortableStart: function (evt) {
         'use strict';
-        var $target = $(e.currentTarget),
-            $branch = $target.closest('.js-content-editor-element');
+        console.log('start');
+        this.sortableRootContainer.classList.add('content-editor__elements__root--dragging');
+    },
 
-        $branch.toggleClass('content-editor__element--opened');
+    onSortableEnd: function (evt) {
+        'use strict';
+        var item = evt.item,
+            from = evt.from,
+            to = evt.to,
+            ids = [],
+            parentId,
+            url;
 
-        if ($branch.hasClass('content-editor__element--loaded')) {
-            e.preventDefault();
-            e.stopPropagation();
-        }
+        console.log('end');
+        this.sortableRootContainer.classList.remove('content-editor__elements__root--dragging');
+        // // get list of ids
+        // $('> .js-content-editor-element', to).each(function () {
+        //     ids.push($(this).attr('data-id'));
+        // });
+
+        // // as the "to" can be the root object where the data-sort-url is set we use "closest" and not "parents"
+        // url = $(to).closest('.js-content-editor-sortable')
+        //     .attr('data-sort-url');
+        // parentId = to.dataset.id;
+
+        // // manage emptyness
+        // $(to).closest('.js-content-editor-element')
+        //     .removeClass('content-editor__element--empty');
+        // if ($('> .js-content-editor-element', from).length === 0) {
+        //     $(from).closest('.js-content-editor-element')
+        //         .addClass('content-editor__element--empty');
+        // }
+
+        // // call to application
+        // $.post(url, {
+        //     oldParentId: from.dataset.id,
+        //     parentId: parentId,
+        //     ids: ids,
+        //     itemId: item.dataset.id
+        // });
+    },
+
+    debugTree: function () {
+        'use strict';
+        console.log('TODO Debug Tree');
     },
 
     invoke: function () {
         'use strict';
         return {
-            init: this.init.bind(this),
-            initSortable: this.initSortable.bind(this)
+            init: this.init.bind(this)
         };
     }
 }.invoke();
---
+++ app/assets/javascripts/admin/commons/content_editor/element.js
@@ -0,0 +1,10 @@
+window.osuny.contentEditor.Element = function Element (container) {
+    'use strict';
+    this.container = container;
+};
+
+
+window.osuny.contentEditor.Element.prototype.XXX = function () {
+    'use strict';
+    // ...
+};
