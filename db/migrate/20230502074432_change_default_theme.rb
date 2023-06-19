b1507fdd6024f55c68a9bd6f1444398516c17520
Arnaud Levy <contact@arnaudlevy.com>
2023-06-19 17:00:36 +0200

    fix

--- db/migrate/20230502074432_change_default_theme.rb
+++ db/migrate/20230502074432_change_default_theme.rb
@@ -1,5 +1,5 @@
 class ChangeDefaultTheme < ActiveRecord::Migration[7.0]
   def change
-    change_column :users, :admin_theme, :integer, default: 1 
+    change_column_default(:users, :admin_theme, from: 0, to: 1)
   end
 end
