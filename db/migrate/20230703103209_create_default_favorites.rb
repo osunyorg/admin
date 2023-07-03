1ea1bc7a346e6fd60f06d9f470257ef470d2497a
pabois <pierreandre.boissinot@noesya.coop>
2023-07-03 12:56:29 +0200

    adjust migration

--- db/migrate/20230703103209_create_default_favorites.rb
+++ db/migrate/20230703103209_create_default_favorites.rb
@@ -1,9 +1,17 @@
 class CreateDefaultFavorites < ActiveRecord::Migration[7.0]
   def up
     User.not_visitor.not_contributor.not_author.not_teacher.not_program_manager.each do |user|
-      return if user.favorites.any?
-      return if user.university.websites.many?
-      user.favorites.create(about: user.university.websites.first)
+      next if user.favorites.any?
+      if user.website_manager?
+        user.websites_to_manage.each do |website|
+          user.favorites.create(about: website)
+        end
+      elsif user.university.websites.count < 5
+        user.university.websites.each do |website|
+          user.favorites.create(about: website)
+        end
+      end
     end
+
   end
 end
