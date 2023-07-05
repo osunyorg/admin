84b1cc4cf7f2dca41fe363cee5d502c9db6f3749
Arnaud Levy <contact@arnaudlevy.com>
2023-07-05 18:09:27 +0200

    fix

--- app/models/communication/website/permalink/publication.rb
+++ app/models/communication/website/permalink/publication.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: communication_website_permalinks
+#
+#  id            :uuid             not null, primary key
+#  about_type    :string           not null, indexed => [about_id]
+#  is_current    :boolean          default(TRUE)
+#  path          :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  about_id      :uuid             not null, indexed => [about_type]
+#  university_id :uuid             not null, indexed
+#  website_id    :uuid             not null, indexed
+#
+# Indexes
+#
+#  index_communication_website_permalinks_on_about          (about_type,about_id)
+#  index_communication_website_permalinks_on_university_id  (university_id)
+#  index_communication_website_permalinks_on_website_id     (website_id)
+#
+# Foreign Keys
+#
+#  fk_rails_e9646cce64  (university_id => universities.id)
+#  fk_rails_f389ba7d45  (website_id => communication_websites.id)
+#
 class Communication::Website::Permalink::Publication < Communication::Website::Permalink
   def self.required_in_config?(website)
     website.connected_hal_publications.any?
