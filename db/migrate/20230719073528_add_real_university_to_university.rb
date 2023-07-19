96a06f7d7ee13cfb4609df63f30a3cccfc8566d1
Arnaud Levy <contact@arnaudlevy.com>
2023-07-19 14:50:56 +0200

    reversable migration

--- db/migrate/20230719073528_add_real_university_to_university.rb
+++ db/migrate/20230719073528_add_real_university_to_university.rb
@@ -1,9 +1,9 @@
 class AddRealUniversityToUniversity < ActiveRecord::Migration[7.0]
   def change
     add_column :universities, :is_really_a_university, :boolean, default: true
-    remove_column :universities, :feature_education
-    remove_column :universities, :feature_research
-    remove_column :universities, :feature_communication
-    remove_column :universities, :feature_administration
+    remove_column :universities, :feature_education, :boolean, default: true
+    remove_column :universities, :feature_research, :boolean, default: true
+    remove_column :universities, :feature_communication, :boolean, default: true
+    remove_column :universities, :feature_administration, :boolean, default: true
   end
 end
