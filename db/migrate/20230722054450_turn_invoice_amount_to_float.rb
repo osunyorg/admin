a52edcc03f59e1ab8d1fa2109f65e6add03a5236
Arnaud Levy <contact@arnaudlevy.com>
2023-07-22 08:14:19 +0200

    reversible migration

--- db/migrate/20230722054450_turn_invoice_amount_to_float.rb
+++ db/migrate/20230722054450_turn_invoice_amount_to_float.rb
@@ -1,9 +1,14 @@
 class TurnInvoiceAmountToFloat < ActiveRecord::Migration[7.0]
-  def change
+  def up
     add_column :universities, :contribution_amount, :float
     University.find_each do |university|
       university.update_column :contribution_amount, university.invoice_amount.to_f
     end
     remove_column :universities, :invoice_amount
+  end
+
+  def down
+    remove_column :universities, :contribution_amount, :float
+    add_column :universities, :invoice_amount, :string
   end
 end
