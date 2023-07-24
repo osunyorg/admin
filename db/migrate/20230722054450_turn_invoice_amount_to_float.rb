f8d6729d070acf24f61cd7494eadda9b90a32934
Arnaud Levy <contact@arnaudlevy.com>
2023-07-24 16:58:16 +0200

    fix

--- db/migrate/20230722054450_turn_invoice_amount_to_float.rb
+++ db/migrate/20230722054450_turn_invoice_amount_to_float.rb
@@ -1,6 +1,7 @@
 class TurnInvoiceAmountToFloat < ActiveRecord::Migration[7.0]
   def up
     add_column :universities, :contribution_amount, :float
+    University.reset_column_information
     University.find_each do |university|
       university.update_column :contribution_amount, university.invoice_amount.to_f
     end
