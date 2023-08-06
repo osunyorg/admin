1e7ed9ddb0195820beddfdcebd6b1d5062cb3c68
Arnaud Levy <contact@arnaudlevy.com>
2023-08-06 11:05:03 +0200

    refactor

--- app/services/licenses/creative_commons.rb
+++ app/services/licenses/creative_commons.rb
@@ -9,26 +9,43 @@ class Licenses::CreativeCommons
   end
   
   def initialize(attribution, commercial_use, derivatives, sharing)
-    @attribution = attribution
-    @commercial_use = commercial_use
-    @derivatives = derivatives
-    @sharing = sharing
+    @attribution = attribution == 'true'
+    @commercial_use = commercial_use == 'true'
+    @derivatives = derivatives == 'true'
+    @sharing = sharing == 'true'
   end
 
   def badge
     unless @badge
       @badge = 'cc-zero'
-      if attribution == 'true'
+      if attribution
         @badge = 'by'
-        @badge += '-nc' if commercial_use == 'false'
-        if derivatives == 'false'
+        @badge += '-nc' unless commercial_use
+        if !derivatives
           @badge += '-nd'
-        elsif sharing == 'false'
+        elsif !sharing
           @badge += '-sa'
         end
       end
     end
     @badge
+  end
+
+  def url
+    unless @url
+      if attribution
+        @url = 'https://creativecommons.org/licenses/by'
+        @url += '-nc' unless commercial_use
+        @url += '-nd' unless derivatives
+        if derivatives && !sharing
+          @url += '-sa'
+        end
+        @url += '/4.0/'
+      else
+        @url = 'http://creativecommons.org/publicdomain/zero/1.0/'
+      end
+    end
+    @url
   end
 
 end
