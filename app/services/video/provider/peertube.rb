7997206ea709f36e681ab80eba734dc7098e9221
Sébastien Gaya <sebastien.gaya@gmail.com>
2023-08-10 15:44:27 +0200

    peertube: handle second domain

--- app/services/video/provider/peertube.rb
+++ app/services/video/provider/peertube.rb
@@ -1,13 +1,17 @@
 class Video::Provider::Peertube < Video::Provider::Default
-  DOMAINS = ['peertube.fr']
+  DOMAINS = ['peertube.fr', 'peertude.my.noesya.coop']
 
   # "https://peertube.fr/w/1i848Qvi7Q3ytW2uPY8AxG"
   def identifier
     video_url.split('/w/').last
   end
 
+  def host
+    video_url.split('/w/').first
+  end
+
   # https://docs.joinpeertube.org/support/doc/api/embeds#quick-start
   def iframe_url
-    "https://peertube.fr/videos/embed/#{identifier}"
+    "#{host}/videos/embed/#{identifier}"
   end
 end
