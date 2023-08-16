21fc4240529d048ecdd5e47283ea6745b347467b
Sébastien Gaya <sebastien.gaya@gmail.com>
2023-08-16 09:48:51 +0200

    syntax

--- test/services/video/provider_test.rb
+++ test/services/video/provider_test.rb
@@ -2,31 +2,31 @@ require "test_helper"
 
 class Video::ProviderTest < ActiveSupport::TestCase
 
-  test "provider empty" do
+  def test_provider_empty
     provider = Video::Provider.find('')
     assert_equal Video::Provider::Default, provider.class
   end
 
-  test "vimeo" do
+  def test_vimeo
     provider = Video::Provider.find('https://vimeo.com/248482251')
     assert_equal Video::Provider::Vimeo, provider.class
   end
 
-  test "youtube" do
+  def test_youtube
     provider = Video::Provider.find('https://www.youtube.com/watch?v=sN8Cq5HEBug')
     assert_equal Video::Provider::Youtube, provider.class
     provider = Video::Provider.find('https://youtu.be/sN8Cq5HEBug')
     assert_equal Video::Provider::Youtube, provider.class
   end
 
-  test "dailymotion" do
+  def test_dailymotion
     provider = Video::Provider.find('https://www.dailymotion.com/video/x35l6b8')
     assert_equal Video::Provider::Dailymotion, provider.class
     provider = Video::Provider.find('https://dai.ly/x35l6b8')
     assert_equal Video::Provider::Dailymotion, provider.class
   end
 
-  test "peertube" do
+  def test_peertube
     provider = Video::Provider.find('https://peertube.fr/w/1i848Qvi7Q3ytW2uPY8AxG')
     assert_equal Video::Provider::Peertube, provider.class
     provider = Video::Provider.find('https://peertube.my.noesya.coop/w/qBMwAAULLA9oadFgbtdyq8')
