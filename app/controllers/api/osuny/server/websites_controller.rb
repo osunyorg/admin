7ff701c998f16430a9812f0ece88080d9f00587d
Arnaud Levy <contact@arnaudlevy.com>
2023-08-24 10:15:42 +0200

    refactor

--- app/controllers/api/osuny/server/websites_controller.rb
+++ app/controllers/api/osuny/server/websites_controller.rb
@@ -5,9 +5,10 @@ class Api::Osuny::Server::WebsitesController < Api::ApplicationController
   end
 
   def theme_released
-    render_forbidden unless params[:secret_key].present? && params[:secret_key] == ENV['OSUNY_API_AUTOUPDATE_THEME_KEY']
-    Communication::Website.with_automatic_update.find_each do |website|
-      website.update_theme_version
+    if params[:secret_key].present? && params[:secret_key] == ENV['OSUNY_API_AUTOUPDATE_THEME_KEY']
+      Communication::Website.autoupdate_websites
+    else
+      render_forbidden 
     end
   end
 end
--- app/models/communication/website/with_theme.rb
+++ app/models/communication/website/with_theme.rb
@@ -4,7 +4,14 @@ module Communication::Website::WithTheme
   included do
     scope :with_automatic_update, -> { where(autoupdate_theme: true) }
     scope :with_manual_update, -> { where(autoupdate_theme: false) }
+
+    def self.autoupdate_websites
+      Communication::Website.with_automatic_update.find_each do |website|
+        website.update_theme_version
+      end
+    end
   end
+
 
   def get_current_theme_version!
     self.update_column :theme_version, current_theme_version
