2161cb8f90b8596ade9fe8f5d1f955a95a86b9f1
Arnaud Levy <contact@arnaudlevy.com>
2023-09-07 17:30:11 +0200

    gem

--- db/migrate/20230907152751_add_signature_to_delayed_job.rb
+++ db/migrate/20230907152751_add_signature_to_delayed_job.rb
@@ -1,4 +1,6 @@
-class AddKeyToDelayedJobs < ActiveRecord::Migration[7.0]
+# frozen_string_literal: true
+
+class AddSignatureToDelayedJob < ActiveRecord::Migration[7.0]
   def change
     add_column :delayed_jobs, :signature, :string
     add_column :delayed_jobs, :args, :text
