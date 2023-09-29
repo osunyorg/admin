48d9f9e4e5a14dff243b989945605f26d975b325
Arnaud Levy <contact@arnaudlevy.com>
2023-09-29 17:39:39 +0200

    github et email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_27_095534) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_29_153351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -563,6 +563,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_095534) do
     t.string "social_tiktok"
     t.boolean "deuxfleurs_hosting", default: true
     t.string "deuxfleurs_identifier"
+    t.string "social_email"
+    t.string "social_github"
     t.index ["about_type", "about_id"], name: "index_communication_websites_on_about"
     t.index ["default_language_id"], name: "index_communication_websites_on_default_language_id"
     t.index ["university_id"], name: "index_communication_websites_on_university_id"
