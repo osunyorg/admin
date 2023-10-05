40a3e10505b043c536357a50404bd8129b72af14
Sébastien Gaya <sebastien.gaya@gmail.com>
2023-10-05 11:10:55 +0200

    after the quiche lorraine, the quiche arnaud!

--- db/migrate/20231005070935_remove_wordpress_tables.rb
+++ db/migrate/20231005070935_remove_wordpress_tables.rb
@@ -1,10 +1,10 @@
 class RemoveWordpressTables < ActiveRecord::Migration[7.0]
   def change
-    drop_table :communication_website_imported_media, force: :cascade
-    drop_table :communication_website_imported_categories, force: :cascade
-    drop_table :communication_website_imported_authors, force: :cascade
-    drop_table :communication_website_imported_pages, force: :cascade
-    drop_table :communication_website_imported_posts, force: :cascade
-    drop_table :communication_website_imported_websites, force: :cascade
+    drop_table :communication_website_imported_categories
+    drop_table :communication_website_imported_authors
+    drop_table :communication_website_imported_pages
+    drop_table :communication_website_imported_posts
+    drop_table :communication_website_imported_media
+    drop_table :communication_website_imported_websites
   end
 end
