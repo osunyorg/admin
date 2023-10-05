34f1961337f23b36bfa08151104616847bed7ae0
Arnaud Levy <contact@arnaudlevy.com>
2023-10-05 12:43:15 +0200

    whitelisted

--- app/models/communication/block/template/agenda.rb
+++ app/models/communication/block/template/agenda.rb
@@ -7,16 +7,35 @@ class Communication::Block::Template::Agenda < Communication::Block::Template::B
   has_component :time, :option, options: [:future_or_present, :future, :present, :archive]
 
   def selected_events
-    @selected_events ||= block.about&.website
-                                    .events
-                                    .for_language(block.language)
-                                    .published
-                                    .send(time)
-                                    .limit(quantity)
+    @selected_events ||= events_with_time_scope
   end
 
   def allowed_for_about?
     website.present?
+  end
+
+  protected
+
+  def events_with_time_scope
+    events = block.about&.website
+                          .events
+                          .for_language(block.language)
+                          .published
+                          .limit(quantity)
+    # Whitelist for security
+    # (not very elegant though)
+    case time
+    when 'future_or_present'
+      return events.future_or_present
+    when 'future'
+      return events.future
+    when 'present'
+      return events.present
+    when 'archive'
+      return events.archive
+    else
+      return events
+    end
   end
 
 end
