e9e82ba050704a53ee0896a6ba22d76276cecc92
Arnaud Levy <contact@arnaudlevy.com>
2023-10-11 09:54:34 +0200

    ui

--- app/controllers/server/websites_controller.rb
+++ app/controllers/server/websites_controller.rb
@@ -1,5 +1,5 @@
 class Server::WebsitesController < Server::ApplicationController
-  before_action :load_website, only: [:sync_theme_version, :update_theme]
+  before_action :load_website, except: :index
 
   has_scope :for_theme_version
   has_scope :for_production
@@ -10,7 +10,6 @@ class Server::WebsitesController < Server::ApplicationController
   def index
     @websites = apply_scopes(Communication::Website.all).ordered
     breadcrumb
-    add_breadcrumb Communication::Website.model_name.human(count: 2), server_websites_path
   end
 
   def sync_theme_version
@@ -21,9 +20,28 @@ class Server::WebsitesController < Server::ApplicationController
     @website.update_theme_version
   end
 
+  def show
+    breadcrumb
+    add_breadcrumb @website
+  end
+
+  def update
+    @website.update(website_params)
+    redirect_to server_website_path(@website), notice: t('admin.successfully_updated_html', model: @website.to_s)
+  end
+
   protected
+
+  def breadcrumb
+    super
+    add_breadcrumb Communication::Website.model_name.human(count: 2), server_websites_path
+  end
 
   def load_website
     @website = Communication::Website.find params[:id]
+  end
+
+  def website_params
+    params.require(:communication_website).permit(:university_id)
   end
 end
