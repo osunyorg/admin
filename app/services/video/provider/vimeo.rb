7e2e1a0dfe52e94ef44007f9115cf225521e7d14
Arnaud Levy <contact@arnaudlevy.com>
2023-10-11 12:25:13 +0200

    vimeo default quality

--- app/services/video/provider/vimeo.rb
+++ app/services/video/provider/vimeo.rb
@@ -25,6 +25,6 @@ class Video::Provider::Vimeo < Video::Provider::Default
 
   # L'autoplay est à 1 uniquement parce que l'iframe n'est pas chargée
   def embed_with_defaults
-    "#{iframe_url}?autoplay=1"
+    "#{iframe_url}?autoplay=1&quality=360p"
   end
 end
