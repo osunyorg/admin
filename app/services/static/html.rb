ddc050ad9599f967a939fc438415d348dc9e55ea
Arnaud Levy <contact@arnaudlevy.com>
2023-10-16 19:15:09 +0200

    orthotypo

--- app/services/static/html.rb
+++ app/services/static/html.rb
@@ -3,7 +3,7 @@ class Static::Html < Static::Default
   def prepared
     unless @prepared
       @prepared = @text.to_s.strip.dup
-      # @prepared = @prepared.ortho(locale: ortho_locale)
+      @prepared = @prepared.ortho(locale: ortho_locale)
       @prepared = ActionController::Base.helpers.sanitize @prepared
       @prepared.gsub! "\r", ''
       @prepared.gsub! "\n", ' '
