3dcbc111be31812da331ac429a88b421142ec444
Arnaud Levy <contact@arnaudlevy.com>
2023-10-31 12:55:19 +0100

    Fix #1369

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,6 +5,7 @@ class Admin::DashboardController < Admin::ApplicationController
     @namespaces << Research if feature_research?
     @namespaces << Communication if feature_communication?
     @namespaces << Administration if feature_administration?
+    @background_tasks_count = Delayed::Job.where(queue: 'default').length
     breadcrumb
   end
 end
