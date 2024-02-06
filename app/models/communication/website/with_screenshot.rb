c8936a3c83a15cb49cff0d41b71dc8df96e6a8af
Arnaud Levy <contact@arnaudlevy.com>
2024-02-06 08:22:46 +0100

    fix

--- app/models/communication/website/with_screenshot.rb
+++ app/models/communication/website/with_screenshot.rb
@@ -8,6 +8,7 @@ module Communication::Website::WithScreenshot
   def screenshot!
     return if url.blank?
     screenshot_url = Screenshot.capture(url)
+    return if screenshot_url.blank?
     downloaded_image = URI.open(screenshot_url)
     self.screenshot.attach  io: downloaded_image, 
                             filename: "screenshot.png"
