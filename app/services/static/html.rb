1ab9b4cc201746e5f4095e4c70cce3936b2b4adc
Arnaud Levy <contact@arnaudlevy.com>
2024-02-16 16:32:58 +0100

    fix

--- app/services/static/html.rb
+++ app/services/static/html.rb
@@ -8,6 +8,7 @@ class Static::Html < Static::Default
       @prepared = clean_empty_paragraphs_at_beginning_and_end @prepared
       @prepared.gsub! "\n", ' '
       @prepared = @prepared.ortho(locale: locale)
+      # TODO ça ne doit plus être utile depuis un siècle
       @prepared.gsub! "/rails/active_storage", "#{@university.url}/rails/active_storage"      
       @prepared = sanitize @prepared
     end
