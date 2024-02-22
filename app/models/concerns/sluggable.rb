fb57d0df5bc02cecdf5b5bc224ed4f017f081c9f
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-02-22 09:48:25 +0100

    limit slug size

--- app/models/concerns/sluggable.rb
+++ app/models/concerns/sluggable.rb
@@ -2,6 +2,11 @@ module Sluggable
   extend ActiveSupport::Concern
 
   included do
+    # Filenames uses 255 bytes so should have 255 characters max.
+    # To be safe, we limit to 200.
+    # Source : https://askubuntu.com/questions/166764/how-long-can-file-names-be/166767#166767
+    SLUG_MAX_LENGTH = 200
+
     validates :slug, presence: true
     validate :slug_must_be_unique
     validates :slug, format: { with: /\A[a-z0-9\-]+\z/, message: I18n.t('slug_error') }
@@ -9,7 +14,7 @@ module Sluggable
     before_validation :check_slug, :make_path
 
     def check_slug
-      self.slug = to_s.parameterize if self.slug.blank?
+      self.slug = generated_slug if self.slug.blank?
       current_slug = self.slug
       n = 0
       while slug_unavailable?(self.slug)
@@ -23,6 +28,16 @@ module Sluggable
     end
 
     protected
+
+    def generated_slug
+      # "My very long and detailed blog post" => "my-very-long-and-detailed-first-blog-post"
+      generated_slug = to_s.parameterize
+      # "my-very-long-and-detailed-first-blog-post" => "my-very-long-and-detailed-first-"
+      generated_slug = generated_slug[0, SLUG_MAX_LENGTH]
+      # "my-very-long-and-detailed-first-" => "my-very-long-and-detailed-first"
+      generated_slug.chop! if generated_slug.ends_with?('-')
+      generated_slug
+    end
 
     def slug_unavailable?(slug)
       existence_params = { university_id: self.university_id, slug: slug }
