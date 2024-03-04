f529e3fbb68381139d2772cded7a804097c304f8
Arnaud Levy <contact@arnaudlevy.com>
2024-03-04 10:49:21 +0100

    duration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_16_125958) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_04_094436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -666,7 +666,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_16_125958) do
     t.string "featured_image_alt"
     t.text "accessibility"
     t.text "contacts"
-    t.text "duration"
+    t.string "duration"
     t.text "evaluation"
     t.text "objectives"
     t.text "opportunities"
