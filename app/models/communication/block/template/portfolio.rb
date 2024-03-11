d09738c3b0d03f587d68fd177da189a84ff360a3
Arnaud Levy <contact@arnaudlevy.com>
2024-03-11 22:59:39 +0100

    still missing show

--- app/models/communication/block/template/portfolio.rb
+++ app/models/communication/block/template/portfolio.rb
@@ -1,24 +1,39 @@
 class Communication::Block::Template::Portfolio < Communication::Block::Template::Base
 
+  has_elements
   has_layouts [
     :grid,
     :list,
     :alternate,
     :large
   ]
-
+  has_component :mode, :option, options: [
+    :all, 
+    :category, 
+    :selection,
+    :categories
+  ]
   has_component :projects_quantity, :number, options: 3
+  has_component :category_id, :project_category
+
+  def category
+    category_id_component.category
+  end
 
   def dependencies
     selected_projects
   end
 
   def selected_projects
-    @selected_projects ||= selected_projects_all
+    @selected_projects ||= send "selected_projects_#{mode}"
   end
 
   def allowed_for_about?
     website.present? && website.projects.any?
+  end
+
+  def children
+    selected_projects
   end
 
   protected
@@ -32,10 +47,35 @@ class Communication::Block::Template::Portfolio < Communication::Block::Template
                 .limit(projects_quantity)
   end
 
+  def selected_projects_category
+    return [] if category.nil?
+    category_ids = [category.id, category.descendants.map(&:id)].flatten
+    university.communication_website_projects.for_language(block.language)
+                                            .joins(:categories)
+                                            .where(categories: { id: category_ids })
+                                            .distinct
+                                            .published
+                                            .ordered
+                                            .limit(projects_quantity)
+  end
+
   def selected_projects_selection
     elements.map { |element|
-      element.projet
+      project(element.id)
     }.compact
+  end
+
+  def selected_projects_categories
+    []
+  end
+
+  def project(id)
+    return if id.blank?
+    block.about&.website
+                .projects
+                .for_language(block.language)
+                .published
+                .find_by(id: id)
   end
 
 end
---
+++ app/models/communication/block/template/portfolio/element.rb
@@ -0,0 +1,5 @@
+class Communication::Block::Template::Portfolio::Element < Communication::Block::Template::Base
+
+  has_component :id, :project
+
+end
