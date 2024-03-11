4608e2d45262fe2c2c290df40054b8f0a8ae206e
Arnaud Levy <contact@arnaudlevy.com>
2024-03-11 22:39:21 +0100

    all

--- app/models/communication/block/template/portfolio.rb
+++ app/models/communication/block/template/portfolio.rb
@@ -14,11 +14,28 @@ class Communication::Block::Template::Portfolio < Communication::Block::Template
   end
 
   def selected_projects
-    @selected_projects ||= elements.map { |element| element.projet }.compact
+    @selected_projects ||= selected_projects_all
   end
 
   def allowed_for_about?
     website.present? && website.projects.any?
+  end
+
+  protected
+
+  def selected_projects_all
+    block.about&.website
+                .projects
+                .for_language(block.language)
+                .published
+                .ordered
+                .limit(projects_quantity)
+  end
+
+  def selected_projects_selection
+    elements.map { |element|
+      element.projet
+    }.compact
   end
 
 end
