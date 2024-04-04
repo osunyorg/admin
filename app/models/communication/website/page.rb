fb87a3727ebba9f64ed87e7208a128a1fc2a6572
Arnaud Levy <contact@arnaudlevy.com>
2024-04-04 15:29:30 +0200

    keep ancestors' bodyclasses
    Co-authored-by: Pierre-André Boissinot <pierreandre.boissinot@noesya.coop>

--- app/models/communication/website/page.rb
+++ app/models/communication/website/page.rb
@@ -138,12 +138,14 @@ class Communication::Website::Page < ApplicationRecord
     parent&.best_featured_image_source
   end
 
+  # La page actuelle a les bodyclass classe1 et classe2 ("classe1 classe2")
+  # Les différents ancêtres ont les classes home, bodyclass et secondclass
+  # -> "page-classe1 page-classe2 ancestor-home ancestor-bodyclass ancestor-secondclass"
   def best_bodyclass
-    if bodyclass.present?
-      add_prefix_to_bodyclass(bodyclass, 'page')
-    elsif inherited_bodyclass
-      add_prefix_to_bodyclass(inherited_bodyclass, 'ancestor')
-    end
+    classes = []
+    classes += add_prefix_to_classes(bodyclass.split(' '), 'page') unless bodyclass.blank?
+    classes += add_prefix_to_classes(ancestor_classes, 'ancestor') unless ancestor_classes.blank?
+    classes.join(' ')
   end
 
   def siblings
@@ -160,15 +162,18 @@ class Communication::Website::Page < ApplicationRecord
 
   protected
 
-  def add_prefix_to_bodyclass(bodyclasses, prefix)
-    bodyclasses.split(' ')
-               .map { |bodyclass| "#{prefix}-#{bodyclass}" }
-               .join(' ')
+  # ["class1", "class2"], "page" -> ["page-class1", "page-class2"]
+  def add_prefix_to_classes(classes, prefix)
+    classes.map { |single_class| "#{prefix}-#{single_class}" }
   end
 
-  def inherited_bodyclass
-    bodyclass.present?  ? bodyclass
-                        : parent&.inherited_bodyclass
+  # ["class1", "class2", "class3 class4"] -> ["class1", "class2", "class3", "class4"]
+  def ancestor_classes
+    @ancestor_classes ||= ancestors.pluck(:bodyclass)
+                                   .compact_blank
+                                   .join(' ')
+                                   .split(' ')
+                                   .compact_blank
   end
 
   def slug_unavailable?(slug)
