171d6fe3ffe781ea8da5e654fc7121918afcc7eb
Arnaud Levy <contact@arnaudlevy.com>
2024-04-17 10:40:15 +0200

    missing ui

--- app/controllers/showcase/home_controller.rb
+++ app/controllers/showcase/home_controller.rb
@@ -2,5 +2,6 @@ class Showcase::HomeController < Showcase::ApplicationController
   def index
     @websites = Communication::Website.in_showcase
                                       .order(created_at: :desc)
+                                      .page(params[:page])
   end
 end
