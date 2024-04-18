cd63c3360eb22fd1d78e18005f90c51d1559e6f7
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-04-18 08:46:37 +0200

    set slug after migration

--- db/migrate/20240403122003_add_slugs_to_person_and_organizations_categories.rb
+++ db/migrate/20240403122003_add_slugs_to_person_and_organizations_categories.rb
@@ -2,5 +2,17 @@ class AddSlugsToPersonAndOrganizationsCategories < ActiveRecord::Migration[7.1]
   def change
     add_column :university_organization_categories, :slug, :string
     add_column :university_person_categories, :slug, :string
+
+    University::Organization::Category.reset_column_information
+    University::Organization::Category.all.find_each do |category|
+      category.set_slug
+      category.update_column(:slug, category.slug)
+    end
+
+    University::Person::Category.reset_column_information
+    University::Person::Category.all.find_each do |category|
+      category.set_slug
+      category.update_column(:slug, category.slug)
+    end
   end
 end
