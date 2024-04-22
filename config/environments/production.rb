265a9c406e431bfdb1652d30cda2ac1cba52e6f2
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-04-22 13:15:56 +0200

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   config.cache_store = :redis_cache_store, {
-    url: "#{ ENV['REDIS_URL'] }/0:#{ ENV['SOURCE_VERSION'] || ENV['CONTAINER_VERSION'] }"
+    url: ENV['REDIS_URL']
   }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
