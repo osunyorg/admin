62810fb06c51dbb9b51477101a0f2613f9eb9cbb
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-05-06 00:37:48 +0200

    WIP visibility radio buttons

--- app/helpers/university/people_helper.rb
+++ app/helpers/university/people_helper.rb
@@ -2,7 +2,9 @@ module University::PeopleHelper
 
   def personal_data_visibility_input_options
     {
-      include_blank: false,
+      as: :radio_buttons,
+      wrapper: :vertical_collection_inline,
+      wrapper_html: { class: "visibility_radio_buttons" },
       label_method: -> (option) { t("university.person.personal_data_visibilities.#{option[1]}") },
       hint: t('simple_form.hints.university_person.personal_data_visibility')
     }
