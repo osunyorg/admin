5189ca7acedbe4596a0c42330a5d6d7a62730570
Arnaud Levy <contact@arnaudlevy.com>
2024-05-07 23:48:27 +0200

    better

--- app/controllers/showcase/home_controller.rb
+++ app/controllers/showcase/home_controller.rb
@@ -1,13 +1,11 @@
 class Showcase::HomeController < Showcase::ApplicationController
   def index
     @websites = Communication::Website.in_showcase
-                                      .ordered_for_showcase
     @tags = Communication::Website::Showcase::Tag.all.ordered
   end
 
   def tag
     @tag = Communication::Website::Showcase::Tag.find_by(slug: params[:tag])
     @websites = @tag.websites.in_showcase
-                             .ordered_for_showcase
   end
 end
