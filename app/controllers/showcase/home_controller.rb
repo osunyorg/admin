23ca5544c358f75d916f5ad141ef5ff26babc49a
Arnaud Levy <contact@arnaudlevy.com>
2024-05-12 11:29:18 +0200

    fix

--- app/controllers/showcase/home_controller.rb
+++ app/controllers/showcase/home_controller.rb
@@ -4,17 +4,22 @@ class Showcase::HomeController < Showcase::ApplicationController
     @features = Communication::Website::Showcase.features
     @websites = Communication::Website.in_showcase
                                       .page(params[:page])
+                                      .per(24)
   end
 
   def tag
     @tag = Communication::Website::Showcase::Tag.find_by!(slug: params[:tag])
     @websites = @tag.websites.in_showcase
                              .page(params[:page])
+                             .per(24)
   end
 
   def feature
     feature = params[:feature].to_sym
     @title = Communication::Website::Showcase.title_for_feature(feature)
-    @websites = Communication::Website::Showcase.websites_for_feature(feature).page(params[:page])
+    @websites = Communication::Website::Showcase.websites_for_feature(feature)
+                                                .in_showcase
+                                                .page(params[:page])
+                                                .per(24)
   end
 end
