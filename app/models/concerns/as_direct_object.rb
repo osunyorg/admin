f3a219ef1da6b4f4d1940c98629f08ac0946fc11
Arnaud Levy <contact@arnaudlevy.com>
2024-05-17 16:35:25 +0200

    syntax

--- app/models/concerns/as_direct_object.rb
+++ app/models/concerns/as_direct_object.rb
@@ -44,7 +44,10 @@ module AsDirectObject
   # TODO Cette méthode devrait être appelée dès qu'on enregistre un objet indirect, 
   # sur chaque `direct_source` connectée (via les connexions).
   def delete_obsolete_connections
-    Communication::Website::Connection.delete_useless_connections(connections, recursive_dependencies)
+    Communication::Website::Connection.delete_useless_connections(
+      connections, 
+      recursive_dependencies
+    )
   end
 
 end
