9aa12403d92e9630b233f8d3efd919985ec5e8d5
Arnaud Levy <contact@arnaudlevy.com>
2024-05-21 15:12:45 +0200

    clean

--- app/services/filters/admin/university/alumni/experiences/imports.rb
+++ app/services/filters/admin/university/alumni/experiences/imports.rb
@@ -3,7 +3,12 @@ module Filters
     def initialize(user)
       super
       add :for_status,
-          ::Import::statuses.keys.map { |r| { to_s: I18n.t("enums.import.status.#{r}"), id: r } },
+          ::Import::statuses.keys.map { |r| 
+            { 
+              to_s: I18n.t("enums.import.status.#{r}"), 
+              id: r 
+            }
+          },
           I18n.t(
             'filters.attributes.element',
             element: Import.human_attribute_name('status').downcase
