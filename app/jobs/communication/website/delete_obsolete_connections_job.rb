e7221acb980ee5dc2584a0814aef995550290286
Arnaud Levy <contact@arnaudlevy.com>
2024-05-24 09:53:53 +0200

    fix

--- app/jobs/communication/website/delete_obsolete_connections_job.rb
+++ app/jobs/communication/website/delete_obsolete_connections_job.rb
@@ -1,6 +1,4 @@
 class Communication::Website::DeleteObsoleteConnectionsJob < Communication::Website::BaseJob
-  queue_as :low_priority
-
   def execute
     website.delete_obsolete_connections
   end
--- app/jobs/communication/website/indirect_object/sync_with_git_job.rb
+++ app/jobs/communication/website/indirect_object/sync_with_git_job.rb
@@ -1,5 +1,5 @@
 class Communication::Website::IndirectObject::SyncWithGitJob < ApplicationJob
-  queue_as :high_priority
+  queue_as :mice
 
   def perform(website, indirect_object)
     website.sync_indirect_object_with_git(indirect_object)
