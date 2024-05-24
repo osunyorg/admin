3aaef8c9243fd68f816e374bb41ab5f16ce4714b
pabois <pierreandre.boissinot@noesya.coop>
2024-05-24 15:14:10 +0200

    adjust queues

--- app/jobs/communication/website/direct_object/sync_with_git_job.rb
+++ app/jobs/communication/website/direct_object/sync_with_git_job.rb
@@ -1,5 +1,5 @@
 class Communication::Website::DirectObject::SyncWithGitJob < ApplicationJob
-  queue_as :mice
+  queue_as :elephant
 
   def perform(direct_object)
     direct_object.sync_with_git_safely
