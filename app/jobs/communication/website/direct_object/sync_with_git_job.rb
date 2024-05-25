24db57e7911255246cd5e1840c1da7f8a00ed402
Arnaud Levy <contact@arnaudlevy.com>
2024-05-25 16:04:27 +0200

    better

--- app/jobs/communication/website/direct_object/sync_with_git_job.rb
+++ app/jobs/communication/website/direct_object/sync_with_git_job.rb
@@ -1,9 +1,5 @@
 class Communication::Website::DirectObject::SyncWithGitJob < ApplicationJob
-  queue_as :elephant
-
   def perform(direct_object)
     direct_object.sync_with_git_safely
-  rescue ActiveJob::DeserializationError
-    # Website or direct_object does not exist anymore
   end
 end
--- app/jobs/communication/website/indirect_object/sync_with_git_job.rb
+++ app/jobs/communication/website/indirect_object/sync_with_git_job.rb
@@ -1,9 +1,5 @@
-class Communication::Website::IndirectObject::SyncWithGitJob < ApplicationJob
-  queue_as :elephant
-
+class Communication::Website::IndirectObject::SyncWithGitJob < Communication::Website::BaseJob
   def perform(website, indirect_object)
     website.sync_indirect_object_with_git(indirect_object)
-  rescue ActiveJob::DeserializationError
-    # Website or indirect_object does not exist anymore
   end
 end
