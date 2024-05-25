9ebf5f4fada87c4dabb8807c4351966f344fc551
Arnaud Levy <contact@arnaudlevy.com>
2024-05-25 16:05:32 +0200

    fix

--- app/jobs/communication/website/direct_object/sync_with_git_job.rb
+++ app/jobs/communication/website/direct_object/sync_with_git_job.rb
@@ -1,4 +1,4 @@
-class Communication::Website::DirectObject::SyncWithGitJob < ApplicationJob
+class Communication::Website::DirectObject::SyncWithGitJob < Communication::Website::BaseJob
   def perform(direct_object)
     direct_object.sync_with_git_safely
   end
