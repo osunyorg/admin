023b4ddf51b367b18577b61d0a02d53a303ecf04
pabois <pierreandre.boissinot@noesya.coop>
2024-05-27 10:46:16 +0200

    tentative d'opti

--- app/models/communication/website/with_git_repository.rb
+++ app/models/communication/website/with_git_repository.rb
@@ -35,9 +35,29 @@ module Communication::Website::WithGitRepository
 
   # Synchronisation optimale d'objet indirect
   def sync_indirect_object_with_git(indirect_object)
+    all_dependencies = []
     indirect_object.direct_sources.each do |direct_source|
-      add_direct_source_to_sync(direct_source)
+      # Ne pas traiter les sources d'autres sites
+      next unless direct_source.website.id == self.id
+      # Ne pas traiter les sources non synchronisables
+      next unless direct_source.syncable?
+      # Ne pas traiter si la source directe est déjà dans le tableau de dépendances
+      next if all_dependencies.include?(direct_source)
+      all_dependencies << direct_source
+      # On passe le tableau de dépendances à la méthode recursive_dependencies
+      # pour qu'il soit capable d'early return en cas de doublon
+      all_dependencies += direct_source.recursive_dependencies(array: all_dependencies, syncable_only: true)
+      # Communication::Website::GitFile.sync self, direct_source
+      # direct_source.recursive_dependencies(syncable_only: true).each do |object|
+      #   Communication::Website::GitFile.sync self, object
+      # end
+      # On ne synchronise pas les références de l'objet direct, car on ne le modifie pas lui.
+      # On ne synchronise pas les références de l'objet dir
     end
+    all_dependencies.each do |dependency|
+      Communication::Website::GitFile.sync self, dependency
+    end
+
     git_repository.sync!
   end
 
@@ -89,16 +109,4 @@ module Communication::Website::WithGitRepository
   end
 
   protected
-
-  def add_direct_source_to_sync(direct_source)
-    # Ne pas traiter les sources d'autres sites
-    return unless direct_source.website.id == self.id
-    # Ne pas traiter les sources non synchronisables
-    return unless direct_source.syncable?
-    Communication::Website::GitFile.sync self, direct_source
-    direct_source.recursive_dependencies(syncable_only: true).each do |object|
-      Communication::Website::GitFile.sync self, object
-    end
-    # On ne synchronise pas les références de l'objet direct, car on ne le modifie pas lui.
-  end
 end
