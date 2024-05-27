94008478f24d8039977f0e555db76db1829fb922
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-05-27 11:22:10 +0200

    Opti sync indirect objects (#1924)
    * tentative d'opti
    
    * up
    
    * bundle
    
    * cc
    
    ---------
    
    Co-authored-by: pabois <pierreandre.boissinot@noesya.coop>

--- app/models/communication/website/with_git_repository.rb
+++ app/models/communication/website/with_git_repository.rb
@@ -35,9 +35,14 @@ module Communication::Website::WithGitRepository
 
   # Synchronisation optimale d'objet indirect
   def sync_indirect_object_with_git(indirect_object)
+    all_dependencies = []
     indirect_object.direct_sources.each do |direct_source|
-      add_direct_source_to_sync(direct_source)
+      all_dependencies = add_direct_source_to_sync(direct_source, array: all_dependencies)
     end
+    all_dependencies.each do |dependency|
+      Communication::Website::GitFile.sync self, dependency
+    end
+
     git_repository.sync!
   end
 
@@ -90,15 +95,18 @@ module Communication::Website::WithGitRepository
 
   protected
 
-  def add_direct_source_to_sync(direct_source)
+  def add_direct_source_to_sync(direct_source, array: [])
     # Ne pas traiter les sources d'autres sites
-    return unless direct_source.website.id == self.id
+    return array unless direct_source.website.id == self.id
     # Ne pas traiter les sources non synchronisables
-    return unless direct_source.syncable?
-    Communication::Website::GitFile.sync self, direct_source
-    direct_source.recursive_dependencies(syncable_only: true).each do |object|
-      Communication::Website::GitFile.sync self, object
-    end
+    return array unless direct_source.syncable?
+    # Ne pas traiter si la source directe est déjà dans le tableau de dépendances
+    return array if array.include?(direct_source)
+    array << direct_source
+    # On passe le tableau de dépendances à la méthode recursive_dependencies
+    # pour qu'il soit capable d'early return en cas de doublon
+    array += direct_source.recursive_dependencies(array: array, syncable_only: true)
     # On ne synchronise pas les références de l'objet direct, car on ne le modifie pas lui.
+    array
   end
 end
