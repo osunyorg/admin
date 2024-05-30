9500ad493dfff18fa9884ee82f2f40fd5e77bd97
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-05-30 12:33:37 +0200

    lock

--- app/jobs/communication/website/base_job.rb
+++ app/jobs/communication/website/base_job.rb
@@ -9,6 +9,8 @@ class Communication::Website::BaseJob < ApplicationJob
 
   # Retry the job after 1 minute if it is interrupted, to prevent queue from being blocked
   retry_on GoodJob::InterruptError, wait: 1.minute, attempts: Float::INFINITY
+  # Retry the job after 5 seconds when the website was locked.
+  retry_on Communication::Website::LockError, wait: 5.seconds, attempts: Float::INFINITY
 
   attr_accessor :website_id, :options
 
@@ -17,8 +19,20 @@ class Communication::Website::BaseJob < ApplicationJob
     @options = options
     # Website might be deleted in between
     return unless website.present?
-    # TODO manage lock / unlock
-    execute
+    # Skip website lock if specified
+    skip_website_lock = options.fetch(:skip_website_lock, false)
+    if website.locked_for_background_jobs? && !skip_website_lock
+      raise Communication::Website::LockError.new("Interrupted because of website lock.")
+    end
+    # We lock the website to prevent race conditions
+    website.lock_for_background_jobs!
+    begin
+      # We execute the job
+      execute
+    ensure
+      # We make sure to unlock the website to allow the other jobs to run
+      website.unlock_for_background_jobs!
+    end
   end
 
   protected
--- app/jobs/communication/website/clean_and_rebuild_job.rb
+++ app/jobs/communication/website/clean_and_rebuild_job.rb
@@ -1,6 +1,11 @@
 class Communication::Website::CleanAndRebuildJob < Communication::Website::BaseJob
   queue_as :whale
 
+  def perform(website_id, options = {})
+    # Clean and Rebuild Job is a special job that can be run even if the website is locked
+    super(website_id, options.merge(skip_website_lock: true))
+  end
+
   def execute
     website.clean_and_rebuild
   end
