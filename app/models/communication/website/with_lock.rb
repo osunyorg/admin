a207ecde1b3c893fda92bee9a853500e499e2466
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-05-31 10:46:23 +0200

    V3 du lock du website : Locked by (#1962)
    * locked_by_job_id
    
    * fix

--- app/models/communication/website/with_lock.rb
+++ app/models/communication/website/with_lock.rb
@@ -6,16 +6,24 @@ module Communication::Website::WithLock
     LOCK_MAX_DURATION = 2.hours
   end
 
-  def locked_for_background_jobs?
+  def locked_for_background_jobs?(executing_job_id)
     reload
-    locked_at.present?
+    locked_at.present? && # Not locked if locked_at is nil
+    locked_by_job_id != executing_job_id && # If the website was locked by the same job, we can continue
+    GoodJob::Job.running.find_by(id: locked_by_job_id).present? # Check if the locking job is still running (to handle SIGKILL and hard shutdowns)
   end
 
-  def lock_for_background_jobs!
-    update_column :locked_at, Time.zone.now
+  def lock_for_background_jobs!(job_id)
+    update_columns(
+      locked_at: Time.zone.now,
+      locked_by_job_id: job_id
+    )
   end
 
   def unlock_for_background_jobs!
-    update_column :locked_at, nil
+    update_columns(
+      locked_at: nil,
+      locked_by_job_id: nil
+    )
   end
 end
