e4c8b8ec1a8da228e937a4ab6187adfd4d9c13fe
Arnaud Levy <contact@arnaudlevy.com>
2024-05-31 09:05:39 +0200

    reversible migration

--- db/migrate/20240531065216_remove_admin_theme_from_users.rb
+++ db/migrate/20240531065216_remove_admin_theme_from_users.rb
@@ -1,5 +1,5 @@
 class RemoveAdminThemeFromUsers < ActiveRecord::Migration[7.1]
   def change
-    remove_column :users, :admin_theme
+    remove_column :users, :admin_theme, :integer, default: 1
   end
 end
