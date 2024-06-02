ed1b1853c90400cb7fe875a45bc692392695a4b0
Arnaud Levy <contact@arnaudlevy.com>
2024-06-02 21:50:57 +0200

    done

---
+++ app/helpers/admin/osuny_helper.rb
@@ -0,0 +1,39 @@
+module Admin::OsunyHelper
+
+  def osuny_panel(title = nil, subtitle: nil, action: nil, image: nil, small: false, classes: '', &block)
+    render  layout: "admin/application/components/panel",
+            locals: {
+              title: title,
+              subtitle: subtitle,
+              action: action,
+              image: image,
+              small: small,
+              classes: classes
+            } do
+      capture(&block)
+    end
+  end
+
+  def osuny_label(title, classes: '')
+    raw "<label class=\"form-label #{classes}\">#{title}</label>"
+  end
+
+  def osuny_separator
+    raw '<hr class="my-5">'
+  end
+
+  def osuny_thumbnail(image, initials: nil, large: false, cropped: true)
+    render  partial: "admin/application/components/thumbnail",
+            locals: {
+              image: image,
+              initials: initials,
+              large: large,
+              cropped: cropped
+            }
+  end
+
+  def osuny_published(object)
+    raw "<span class=\"osuny__published osuny__published--#{ object.published? }\"></span>"
+  end
+
+end
