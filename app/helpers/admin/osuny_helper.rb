8d7782a10a9c8add43ed463488efd47d1de64084
Arnaud Levy <contact@arnaudlevy.com>
2024-06-02 22:28:50 +0200

    fix

--- app/helpers/admin/osuny_helper.rb
+++ app/helpers/admin/osuny_helper.rb
@@ -23,7 +23,7 @@ module Admin::OsunyHelper
   end
 
   def osuny_thumbnail(object, large: false, cropped: true)
-    image = object.respond_to?(:featured_image) ? image.featured_image
+    image = object.respond_to?(:featured_image) ? object.featured_image
                                                 : nil
     render  partial: "admin/application/components/thumbnail",
             locals: {
