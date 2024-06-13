f350fef26850cb2a961019bdc1505392b485f567
pabois <pierreandre.boissinot@noesya.coop>
2024-06-13 12:29:37 +0200

    change migration

--- db/migrate/20240613084620_connect_universities_to_languages.rb
+++ db/migrate/20240613084620_connect_universities_to_languages.rb
@@ -4,7 +4,9 @@ class ConnectUniversitiesToLanguages < ActiveRecord::Migration[7.1]
       persons_languages_ids = university.university_people.pluck(:language_id).uniq
       orgas_languages_ids = university.university_organizations.pluck(:language_id).uniq
       websites_languages_ids = university.communication_websites.map(&:language_ids).flatten.uniq
-      languages = Language.where(id: persons_languages_ids + orgas_languages_ids + websites_languages_ids + [university.default_language_id])
+      journals_languages_ids =  university.research_journals.pluck(:language_id).uniq
+      programs_languages_ids =  university.education_programs.pluck(:language_id).uniq
+      languages = Language.where(id: persons_languages_ids + orgas_languages_ids + websites_languages_ids + journals_languages_ids + programs_languages_ids + [university.default_language_id])
       university.languages = languages
     end
   end
