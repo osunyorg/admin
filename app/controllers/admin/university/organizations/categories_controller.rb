1fb95188b21a8c011e4ef01deec3a718d975edfe
pabois <pierreandre.boissinot@noesya.coop>
2024-06-14 16:00:33 +0200

    orga multilingues

--- app/controllers/admin/university/organizations/categories_controller.rb
+++ app/controllers/admin/university/organizations/categories_controller.rb
@@ -4,9 +4,12 @@ class Admin::University::Organizations::CategoriesController < Admin::University
                               through_association: :organization_categories
 
   include Admin::Categorizable
+  include Admin::Translatable
 
   def index
     @root_categories = categories.root
+                                 .in_closest_language_id(current_language.id)
+                                 .ordered
     @categories_class = categories_class
     @feature_nav = 'navigation/admin/university/organizations'
     breadcrumb
@@ -15,18 +18,6 @@ class Admin::University::Organizations::CategoriesController < Admin::University
   def show
     @organizations = @category.organizations.ordered.page(params[:page])
     breadcrumb
-  end
-
-  def in_language
-    language = Language.find_by!(iso_code: params[:lang])
-    translation = @category.find_or_translate!(language)
-    if translation.newly_translated
-      # There's an attribute accessor named "newly_translated" that we set to true
-      # when we just created the translation. We use it to redirect to the form instead of the show.
-      redirect_to [:edit, :admin, translation.becomes(translation.class.base_class)]
-    else
-      redirect_to [:admin, translation.becomes(translation.class.base_class)]
-    end
   end
 
   def static
@@ -45,7 +36,7 @@ class Admin::University::Organizations::CategoriesController < Admin::University
   end
 
   def create
-    @category.language_id = current_university.default_language_id
+    @category.language_id = current_language.id
     if @category.save
       redirect_to admin_university_organization_category_path(@category),
                   notice: t('admin.successfully_created_html', model: @category.to_s)
