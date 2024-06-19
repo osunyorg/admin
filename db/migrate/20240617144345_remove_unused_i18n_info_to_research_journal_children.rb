826422884d244e497a6be1ea220cebe80e59096b
Arnaud Levy <contact@arnaudlevy.com>
2024-06-19 07:08:04 +0200

    reversible

--- db/migrate/20240617144345_remove_unused_i18n_info_to_research_journal_children.rb
+++ db/migrate/20240617144345_remove_unused_i18n_info_to_research_journal_children.rb
@@ -1,8 +1,8 @@
 class RemoveUnusedI18nInfoToResearchJournalChildren < ActiveRecord::Migration[7.1]
   def change
-    remove_column :research_journal_volumes, :language_id
-    remove_column :research_journal_papers, :language_id
-    remove_column :research_journal_paper_kinds, :language_id
-    remove_column :research_journal_paper_kinds, :original_id
+    remove_column :research_journal_volumes, :language_id, :uuid
+    remove_column :research_journal_papers, :language_id, :uuid
+    remove_column :research_journal_paper_kinds, :language_id, :uuid
+    remove_column :research_journal_paper_kinds, :original_id, :uuid
   end
 end
