28ad56192164f3441a24d618ecaedc905bd5f843
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-06-28 17:04:58 +0200

    profile

--- app/controllers/admin/profile_controller.rb
+++ app/controllers/admin/profile_controller.rb
@@ -1,10 +1,13 @@
 class Admin::ProfileController < Admin::ApplicationController
+  include UserManagement
+
   def edit
     breadcrumb
   end
 
   def update
-    if current_user.update(permitted_params)
+    if update_user(user_params)
+      bypass_sign_in current_user, scope: :user if sign_in_after_change_password?
       redirect_to admin_profile_path, notice: t('admin.successfully_updated_html', model: current_user.to_s)
     else
       breadcrumb
@@ -14,6 +17,7 @@ class Admin::ProfileController < Admin::ApplicationController
 
   def destroy
     current_user.destroy
+    sign_out
     redirect_to root_path
   end
 
@@ -22,18 +26,5 @@ class Admin::ProfileController < Admin::ApplicationController
   def breadcrumb
     super
     add_breadcrumb t('menu.edit_profile')
-  end
-
-  def permitted_params
-    params.require(:user)
-          .permit(
-            :email,
-            :first_name,
-            :last_name,
-            :language_id,
-            :mobile_phone,
-            :picture, :picture_infos, :picture_delete,
-            :current_password, :password, :password_confirmation
-          )
   end
 end
