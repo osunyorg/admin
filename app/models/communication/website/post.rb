0788469a5d20a2723087097c0493714ef8931691
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-07-05 15:56:05 +0200

    fix

--- app/models/communication/website/post.rb
+++ app/models/communication/website/post.rb
@@ -55,6 +55,9 @@ class Communication::Website::Post < ApplicationRecord
 
   has_summernote :text # TODO: Remove text attribute
 
+  belongs_to :website,
+               class_name: 'Communication::Website',
+               foreign_key: :communication_website_id
   belongs_to :author,
              class_name: 'University::Person',
              optional: true
