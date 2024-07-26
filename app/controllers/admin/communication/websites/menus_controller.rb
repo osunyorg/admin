c6afeedbf3c323ff88656267ad862945613ee404
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-07-26 13:13:11 +0200

    todo

--- app/controllers/admin/communication/websites/menus_controller.rb
+++ app/controllers/admin/communication/websites/menus_controller.rb
@@ -56,6 +56,7 @@ class Admin::Communication::Websites::MenusController < Admin::Communication::We
 
   protected
 
+  # TODO L10N : Handle website missing language
   def redirect_to_correct_language
     if @menu.language != current_language
       correct_menu = @website.menus.find_by(language_id: current_language.id, identifier: @menu.identifier)
