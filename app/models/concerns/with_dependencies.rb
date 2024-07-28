d909dfdc3729df0008ace1915b60a32bea5afdfb
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-07-28 16:17:13 +0200

    adding dependent destroy + try(:sync_with_git) on destroy

--- app/models/concerns/with_dependencies.rb
+++ app/models/concerns/with_dependencies.rb
@@ -29,7 +29,8 @@ module WithDependencies
       website_ids_before_destroy = websites_to_clean_ids
       super
       snapshot_direct_sources.each do |direct_source|
-        direct_source.sync_with_git
+        # TODO L10N : Replaced by a try method, but it's not the best solution
+        direct_source.try(:sync_with_git)
       end
       clean_websites(website_ids_before_destroy)
       # TODO: Actuellement, on ne nettoie pas les références
