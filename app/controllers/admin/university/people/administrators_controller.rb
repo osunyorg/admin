ebbf913205ecc41fd3a87a5cbea6d02de2d7f182
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-07-29 15:39:31 +0200

    fix admin static person facets

--- app/controllers/admin/university/people/administrators_controller.rb
+++ app/controllers/admin/university/people/administrators_controller.rb
@@ -1,9 +1,21 @@
 class Admin::University::People::AdministratorsController < Admin::University::ApplicationController
-  load_and_authorize_resource :administrator,
-                              class: University::Person::Administrator,
+  load_and_authorize_resource :person,
+                              class: University::Person,
                               through: :current_university,
-                              through_association: :people
+                              through_association: :people,
+                              parent: false
 
   include Admin::HasStaticAction
   include Admin::Localizable
+
+  protected
+
+  # On every other action, we need to load the localization
+  def load_localization
+    @l10n = super.administrator
+  end
+
+  def resource_name
+    "person"
+  end
 end
--- app/controllers/admin/university/people/teachers_controller.rb
+++ app/controllers/admin/university/people/teachers_controller.rb
@@ -1,9 +1,21 @@
 class Admin::University::People::TeachersController < Admin::University::ApplicationController
-  load_and_authorize_resource :teacher,
-                              class: University::Person::Researcher,
+  load_and_authorize_resource :person,
+                              class: University::Person,
                               through: :current_university,
-                              through_association: :people
+                              through_association: :people,
+                              parent: false
 
   include Admin::HasStaticAction
   include Admin::Localizable
+
+  protected
+
+  # On every other action, we need to load the localization
+  def load_localization
+    @l10n = super.teacher
+  end
+
+  def resource_name
+    "person"
+  end
 end
