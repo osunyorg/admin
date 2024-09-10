74f3fd3282ee694528e6c523622a6777c9c3764f
Arnaud Levy <arnaud.levy@noesya.coop>
2024-09-10 10:37:15 +0200

    Documentation de l'API avec Swagger et rswag (#2157)
    * wip
    
    * temporary fix for rack 3
    
    * wip
    
    * doc
    
    * clean
    
    * auth
    
    * api
    
    * wip
    
    * Update app/controllers/api/osuny/communication/websites/posts_controller.rb
    
    * Update app/controllers/api/osuny/communication/websites/posts_controller.rb
    
    * Update config/initializers/rswag_ui.rb
    
    ---------
    
    Co-authored-by: Sébastien Gaya <sebastien.gaya@gmail.com>

--- app/controllers/api/osuny/server/websites_controller.rb
+++ app/controllers/api/osuny/server/websites_controller.rb
@@ -1,11 +1,13 @@
 class Api::Osuny::Server::WebsitesController < Api::Osuny::ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:theme_released]
+  skip_before_action :verify_app_token
+  before_action :verify_autoupdate_theme_key
 
   def theme_released
-    if params[:secret_key].present? && params[:secret_key] == ENV['OSUNY_API_AUTOUPDATE_THEME_KEY']
-      Communication::Website.autoupdate_websites
-    else
-      render_forbidden 
-    end
+    Communication::Website.autoupdate_websites
+  end
+
+  def verify_autoupdate_theme_key
+    render_forbidden if params[:secret_key].blank?
+    render_forbidden if params[:secret_key] != ENV['OSUNY_API_AUTOUPDATE_THEME_KEY']
   end
 end
