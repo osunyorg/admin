5c92d1d5c328aa0f372416170361fa60a11da9c0
Arnaud Levy <arnaud.levy@noesya.coop>
2024-09-10 15:18:10 +0200

    Update app/services/migrations/l10n/research/laboratory/axis.rb
    Co-authored-by: Sébastien Gaya <sebastien.gaya@gmail.com>

--- app/services/migrations/l10n/research/laboratory/axis.rb
+++ app/services/migrations/l10n/research/laboratory/axis.rb
@@ -7,6 +7,8 @@ class Migrations::L10n::Research::Laboratory::Axis < Migrations::L10n::Base
       l10n = Research::Laboratory::Axis::Localization.create(
         name: object.name,
         short_name: object.short_name,
+        meta_description: object.meta_description,
+        summary: object.text, # summary was a "text" attribute on axis.rb
         about_id: about_id,
         language_id: object.university.default_language_id,
         university_id: object.university_id,
