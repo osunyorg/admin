74620a2c336e31ed9734285e3562ff6dfb517d32
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-09-16 10:33:46 +0200

    server fix

--- app/controllers/application_controller/with_features.rb
+++ app/controllers/application_controller/with_features.rb
@@ -4,34 +4,34 @@ module ApplicationController::WithFeatures
   included do
 
     def feature_education?
-      current_university.is_really_a_university &&
+      current_university.is_really_a_university? &&
       can?(:read, Education::Program)
     end
     helper_method :feature_education?
-    
+
     def feature_research?
-      current_university.is_really_a_university && (
+      current_university.is_really_a_university? && (
         can?(:read, Research::Journal) ||
         can?(:read, Research::Publication) ||
         can?(:read, Research::Laboratory)
       )
     end
     helper_method :feature_research?
-    
+
     def feature_communication?
       can?(:read, Communication::Website)
     end
     helper_method :feature_communication?
 
     def feature_administration?
-      current_university.is_really_a_university &&
+      current_university.is_really_a_university? &&
         can?(:read, Administration::Qualiopi::Criterion)
     end
     helper_method :feature_administration?
-    
+
     def feature_settings?
-      can?(:read, University::Person) || 
-      can?(:read, University::Organization) || 
+      can?(:read, University::Person) ||
+      can?(:read, University::Organization) ||
       can?(:read, User)
     end
     helper_method :feature_settings?
--- app/controllers/server/universities_controller.rb
+++ app/controllers/server/universities_controller.rb
@@ -14,6 +14,7 @@ class Server::UniversitiesController < Server::ApplicationController
   end
 
   def show
+    @websites = @university.websites.ordered(current_language)
     breadcrumb
   end
 
