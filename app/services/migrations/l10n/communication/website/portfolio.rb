1c3c38b863ce6ab11f1c7e63d83773582db8902d
Arnaud Levy <contact@arnaudlevy.com>
2024-09-19 14:49:52 +0200

    remove content migrations

--- app/services/migrations/l10n/communication/website/portfolio.rb
+++
@@ -1,7 +0,0 @@
-class Migrations::L10n::Communication::Website::Portfolio < Migrations::L10n::Base
-  def self.execute
-    Project.execute
-    Category.execute
-    reconnect_objects_to_categories Communication::Website::Portfolio::Project
-  end
-end
