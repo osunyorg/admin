1e608993bf2c92b8ff6379dd249b0f1f629c34a1
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-09-19 17:28:53 +0200

    import experience

--- app/services/importers/people_experiences.rb
+++ app/services/importers/people_experiences.rb
@@ -7,7 +7,7 @@ module Importers
       hash_to_person = HashToPerson.new(@university, @language, hash)
       if hash_to_person.valid?
         person = hash_to_person.person
-        hash_to_experience = HashToExperience.new(person, hash)
+        hash_to_experience = HashToExperience.new(person, @language, hash)
         add_error(hash_to_experience.error, index + 1) unless hash_to_experience.valid?
       else
         add_error(hash_to_person.error, index + 1)
