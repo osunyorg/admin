9cb518157f9f5a94315be7e0c4adcec6e5dfa902
pabois <pierreandre.boissinot@noesya.coop>
2024-09-20 13:43:58 +0200

    remove for apps

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,6 @@
 class Admin::UsersController < Admin::ApplicationController
   load_and_authorize_resource through: :current_university
 
-  has_scope :for_language
-  has_scope :for_role
-  has_scope :for_search_term
-
   def index
     @users = @users.filter_by(params[:filters], current_language)
                    .ordered
