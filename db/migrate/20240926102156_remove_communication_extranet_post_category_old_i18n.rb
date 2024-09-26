b7f9faa42b95c44761c4982faca6882e08a14570
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-09-26 16:40:03 +0200

    fix typo

--- db/migrate/20240926102156_remove_communication_extranet_post_category_old_i18n.rb
+++ db/migrate/20240926102156_remove_communication_extranet_post_category_old_i18n.rb
@@ -1,6 +1,6 @@
 class RemoveCommunicationExtranetPostCategoryOldI18n < ActiveRecord::Migration[7.1]
   def change
     remove_column :communication_extranet_post_categories, :slug
-    remove_column :communication_extranet_post_categories, :nam
+    remove_column :communication_extranet_post_categories, :name
   end
 end
