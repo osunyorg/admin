efd5d708f1cb791b8cce8bb54b32a83101ca3583
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-09-27 12:52:24 +0200

    remove todos

--- config/navigation/admin/communication/website/settings_navigation.rb
+++ config/navigation/admin/communication/website/settings_navigation.rb
@@ -7,7 +7,5 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.item  :subnav_settings,
                   t('admin.subnav.settings'),
                   edit_admin_communication_website_path(id: @website.id, website_id: nil) if can?(:edit, @website)
-    # TODO L10N : @Arnaud on peut supprimer si qu'un item?
-    # Non il faut que je code d'autres menus pour séparer l'édition en plusieurs formulaires plus simples
   end
 end
