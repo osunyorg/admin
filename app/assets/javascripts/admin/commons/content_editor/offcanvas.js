40cf29ddc21a4000309f5f446a879b0a21f31a1f
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-09-30 17:36:21 +0200

    Update app/assets/javascripts/admin/commons/content_editor/offcanvas.js

--- app/assets/javascripts/admin/commons/content_editor/offcanvas.js
+++ app/assets/javascripts/admin/commons/content_editor/offcanvas.js
@@ -30,7 +30,7 @@ window.osuny.contentEditor.offcanvas = {
     onAddBlockButtonClick: function (event) {
         'use strict';
         event.preventDefault();
-        open(event.target.href, 'editor');
+        window.open(event.target.href, 'editor');
         this.editorBootstrap.show();
     },
 
