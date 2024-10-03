9939e9c9485a01d2d7ea2e3845b3b186f6ee57e6
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-10-03 15:12:23 +0200

    fix mailers

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -25,7 +25,7 @@ class NotificationMailer < ApplicationMailer
   def website_invalid_access_token(website, user)
     @website = website
     merge_with_university_infos(@website.university, {})
-    @url = edit_admin_communication_website_url(@website)
+    @url = edit_admin_communication_website_url(@website, lang: @website.default_language.iso_code)
     I18n.with_locale(user.language.iso_code) do
       subject = t('mailers.notifications.website_invalid_access_token.subject', website: website)
       mail(from: user.university.mail_from[:full], to: user.email, subject: subject)
