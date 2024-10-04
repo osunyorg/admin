dcccb231d093d8effd18e6c17044c0143a72aeab
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-10-04 10:30:47 +0200

    set not null default_language in extranets

--- db/migrate/20241003134739_add_default_language_to_extranets.rb
+++ db/migrate/20241003134739_add_default_language_to_extranets.rb
@@ -3,9 +3,9 @@ class AddDefaultLanguageToExtranets < ActiveRecord::Migration[7.1]
     add_reference :communication_extranets, :default_language, null: true, foreign_key: { to_table: :languages }, type: :uuid
     Communication::Extranet.reset_column_information
     Communication::Extranet.find_each do |extranet|
-      next if extranet.languages.none?
       extranet.update_column :default_language_id, extranet.languages.first.id
     end
+    change_column_null :communication_extranets, :default_language_id, false
   end
 
   def down
