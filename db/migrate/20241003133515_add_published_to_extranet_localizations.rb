25bffe03a166c37ce29e4a3e1f2f7540ea8a7269
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-10-04 10:27:16 +0200

    Update db/migrate/20241003133515_add_published_to_extranet_localizations.rb

--- db/migrate/20241003133515_add_published_to_extranet_localizations.rb
+++ db/migrate/20241003133515_add_published_to_extranet_localizations.rb
@@ -2,7 +2,8 @@ class AddPublishedToExtranetLocalizations < ActiveRecord::Migration[7.1]
   def up
     add_column :communication_extranet_localizations, :published, :boolean, default: false
     add_column :communication_extranet_localizations, :published_at, :datetime
-    Communication::Extranet::Localization.all.each do |localization|
+    Communication::Extranet::Localization.reset_column_information
+    Communication::Extranet::Localization.update_all("published = true, published_at = created_at")
       localization.update(published: true, published_at: localization.created_at)
     end
   end
