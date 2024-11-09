ffd05c3276f2ddee4cfe4bb854fe47c8ead39f41
Arnaud Levy <contact@arnaudlevy.com>
2024-11-09 15:22:42 +0100

    climate

--- app/services/migrations/authors.rb
+++ app/services/migrations/authors.rb
@@ -1,15 +1,21 @@
 module Migrations
   class Authors
+    # Mono to multi-authors
     def self.migrate
-      # Mono to multi-authors
       Communication::Website::Post.find_each do |post|
-        next if post.author_id.blank?
-        author = University::Person.find(post.author_id)
-        next if author.in?(post.authors)
-        puts post.original_localization.to_s
-        puts "  Adding author #{author.original_localization}"
-        post.authors << author
+        migrate_post(post)
       end
     end
+
+    protected
+
+    def self.migrate_post(post)
+      return if post.author_id.blank?
+      author = University::Person.find(post.author_id)
+      return if author.in?(post.authors)
+      puts post.original_localization.to_s
+      puts " author #{author.original_localization}"
+      post.authors << author
+  end
   end
 end
