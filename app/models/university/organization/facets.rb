5d7f65c3286d3ce731d6a23527895c76a9b7eeff
Arnaud Levy <arnaud.levy@noesya.coop>
2024-11-18 16:47:27 +0100

    Facettes dans l'extranet (#2415)
    * list
    
    * shorter
    
    * simpler
    
    * better
    
    * tweaks
    
    * clean add_scope taxonomy
    
    * fix search extranet document
    
    * fix scope taxonomy
    
    * fix when cohort.program.diploma is nil
    
    ---------
    
    Co-authored-by: Sébastien Gaya <sebastien.gaya@gmail.com>

--- app/models/university/organization/facets.rb
+++ app/models/university/organization/facets.rb
@@ -1,13 +1,31 @@
 class University::Organization::Facets < FacetedSearch::Facets
+  attr_reader :language, :categories
+
   def initialize(params, options)
-    super params
+    super       params
+    @model      = options[:model]
+    @about      = options[:about]
+    @language   = options[:language]
+    @categories = options[:categories]
+    add_name
+    add_taxonomies
+  end
 
-    @model = options[:model]
-    @about = options[:about]
+  protected
 
+  def add_name
     filter_with_text :name, {
       title: University::Organization::Localization.human_attribute_name('name')
     }
-
+  end
+  
+  def add_taxonomies
+    categories.taxonomies.each do |taxonomy|
+      taxonomy_l10n = taxonomy.localization_for(language)
+      next if taxonomy_l10n.nil?
+      add_facet FacetedSearch::Facets::Taxonomy, taxonomy_l10n.slug, {
+        l10n: taxonomy_l10n
+      }
+    end
   end
 end
