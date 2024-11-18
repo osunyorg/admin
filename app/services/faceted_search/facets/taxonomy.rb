22af69ba691a5c629222a877d500a1eb02138bd9
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-11-18 14:40:09 +0100

    fix scope taxonomy

--- app/services/faceted_search/facets/taxonomy.rb
+++ app/services/faceted_search/facets/taxonomy.rb
@@ -21,10 +21,21 @@ module FacetedSearch
       descendants.ordered(language)
     end
 
+    # INNER JOIN "university_organizations_categories" "university_organizations_categories_01234567-89ab-cdef-0123-4567890abcde"
+    #   ON "university_organizations_categories_01234567-89ab-cdef-0123-4567890abcde"."organization_id" = "university_organizations"."id"
+    # WHERE "university_organizations_categories_01234567-89ab-cdef-0123-4567890abcde"."category_id" IN (?)
     def add_scope(scope)
       return scope if params_array.blank?
 
-      scope.joins(association_name).where(association_table_name => { id: params_array })
+      scope
+        .joins("
+          INNER JOIN \"#{association_join_table_name}\" \"#{association_join_table_name_alias}\"
+            ON \"#{association_join_table_name_alias}\".\"#{join_table_foreign_key}\" = \"#{table_name}\".\"id\"
+        ")
+        .where(
+          "\"#{association_join_table_name_alias}\".\"#{join_table_association_foreign_key}\" IN (?)",
+          params_array
+        )
     end
 
     protected
@@ -33,12 +44,33 @@ module FacetedSearch
       taxonomy.class
     end
 
-    def association_name
-      @association_name ||= :categories
+    # university_organizations
+    def table_name
+      @facets.model.klass.table_name
     end
 
-    def association_table_name
-      @association_table_name ||= categories_class.table_name
+    def habtm_reflection
+      @association_reflection ||= @facets.model.klass._reflections[:categories].parent_reflection
+    end
+
+    # organization_id
+    def join_table_foreign_key
+      @join_table_foreign_key ||= habtm_reflection.foreign_key
+    end
+
+    # category_id
+    def join_table_association_foreign_key
+      @join_table_association_foreign_key ||= habtm_reflection.association_foreign_key
+    end
+
+    # university_organizations_categories
+    def association_join_table_name
+      @association_join_table_name ||= habtm_reflection.join_table
+    end
+
+    # university_organizations_categories_01234567-89ab-cdef-0123-4567890abcde
+    def association_join_table_name_alias
+      @association_join_table_name_alias ||= "#{association_join_table_name}_#{taxonomy.id}"
     end
 
     def descendants
