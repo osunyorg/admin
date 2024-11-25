39eea0d4830cd5a92945635571362d755cdef19e
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-11-25 10:07:20 +0100

    Fix "hugo_ancestors_for_special_page" for page categories

--- app/models/concerns/staticable.rb
+++ app/models/concerns/staticable.rb
@@ -1,6 +1,11 @@
 module Staticable
   extend ActiveSupport::Concern
 
+  TYPES_WITHOUT_SPECIAL_PAGE = [
+    Communication::Website::Page::Localization,
+    Communication::Website::Page::Category::Localization
+  ]
+
   def hugo(website)
     @hugo ||= OpenStruct.new(
       permalink: hugo_permalink_in_website(website),
@@ -24,17 +29,18 @@ module Staticable
 
   protected
 
-  # Si on est sur une page, pas d'ancêtres à chercher, le breadcrumb va se construire avec les parents.
-  # Sinon, les objets ont une "page spéciale", (agenda, actualités, offre de formation...)
-  # Cette page a aussi des ancêtres, qu'il faut récupérer avec ancestors_and_self
   def hugo_ancestors_for_special_page(website)
-    return [] if is_a?(Communication::Website::Page::Localization)
+    # Si on est sur une page ou une catégorie de page, pas d'ancêtres à chercher, le breadcrumb va se construire avec les parents.
+    does_not_have_special_page = TYPES_WITHOUT_SPECIAL_PAGE.any? { |type| is_a?(type) }
+    return [] if does_not_have_special_page
+    # Sinon, les objets ont une "page spéciale", (agenda, actualités, offre de formation...)
     permalink = Communication::Website::Permalink.for_object(self, website)
     return [] unless permalink
     special_page = permalink.special_page(website)
     return [] unless special_page
     special_page_l10n = special_page.localization_for(language)
     return [] unless special_page_l10n
+    # Cette page a aussi des ancêtres, qu'il faut récupérer avec ancestors_and_self
     special_page_l10n.ancestors_and_self
   end
 
