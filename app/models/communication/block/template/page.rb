f00d9aac92f8828ddf1e401b6712157c6896d4bc
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-11-25 12:40:03 +0100

    add category mode to block template pages

--- app/models/communication/block/template/page.rb
+++ app/models/communication/block/template/page.rb
@@ -8,9 +8,15 @@ class Communication::Block::Template::Page < Communication::Block::Template::Bas
     :alternate,
     :large
   ]
-  has_component :mode, :option, options: [:selection, :children]
+  has_component :mode, :option, options: [
+    :selection,
+    :children,
+    :category
+  ]
   has_component :text, :rich_text
   has_component :page_id, :page
+  has_component :category_id, :page_category
+  has_component :pages_quantity, :number, options: 3
 
   has_component :option_image,        :boolean, default: true
   has_component :option_main_summary, :boolean, default: true
@@ -18,6 +24,10 @@ class Communication::Block::Template::Page < Communication::Block::Template::Bas
 
   def page
     page_id_component.page
+  end
+
+  def category
+    category_id_component.category
   end
 
   def dependencies
@@ -47,6 +57,18 @@ class Communication::Block::Template::Page < Communication::Block::Template::Bas
     page.children
         .published_now_in(block.language)
         .ordered
+  end
+
+  def selected_pages_category
+    return [] if category.nil?
+    category_ids = [category.id, category.descendants.map(&:id)].flatten
+
+    block.about&.website
+                .pages
+                .published_now_in(block.language)
+                .for_category(category_ids)
+                .ordered(block.language)
+                .limit(pages_quantity)
   end
 
 end
