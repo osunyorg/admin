8991cb28b560d790c73aaf82cf928f51330b89cd
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-11-25 10:00:40 +0100

    fix subnav + tablist

--- config/navigation/admin/communication/website/pages_navigation.rb
+++ config/navigation/admin/communication/website/pages_navigation.rb
@@ -6,7 +6,10 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.item  :feature_nav_pages,
                   t('admin.communication.website.subnav.structure'),
-                  admin_communication_website_pages_path(website_id: @website.id)
+                  admin_communication_website_pages_path(website_id: @website.id),
+                  highlights_on: lambda {
+                    controller_name == "pages" && action_name.in?(["index", "index_list"])
+                  }
     primary.item  :feature_nav_categories,
                   Communication::Website::Page::Category.model_name.human(count: 2),
                   admin_communication_website_page_categories_path(website_id: @website.id)
