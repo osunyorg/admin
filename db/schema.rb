5b068bb6f3c92442c8b5cf27d15b9b5ae6b442fa
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-11-28 18:03:28 +0100

    good_job update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_25_104348) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_28_170222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "pgcrypto"
@@ -1083,6 +1083,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_25_104348) do
     t.datetime "enqueued_at"
     t.datetime "discarded_at"
     t.datetime "finished_at"
+    t.datetime "jobs_finished_at"
   end
 
   create_table "good_job_executions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
