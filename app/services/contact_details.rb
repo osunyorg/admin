18816e9bf39367040f1f3b8844bcbd37b0c413db
Arnaud Levy <contact@arnaudlevy.com>
2024-11-28 12:47:03 +0100

    DRY

--- app/services/contact_details.rb
+++ app/services/contact_details.rb
@@ -1,4 +1,19 @@
 class ContactDetails
+  
+  SOCIAL_NETWORKS = [
+    :email,
+    :facebook,
+    :github,
+    :instagram,
+    :linkedin,
+    :mastodon,
+    :peertube,
+    :tiktok,
+    :vimeo,
+    :x,
+    :youtube
+  ]
+  
   def self.with_kind(kind)
     "ContactDetails::#{kind.to_s.camelize}".constantize
   end
