c0203a2ee99b65f53229d90227c622a3fd7beee2
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-11-29 16:10:52 +0100

    handle json responses on errors

--- app/controllers/application_controller/with_errors.rb
+++ app/controllers/application_controller/with_errors.rb
@@ -31,11 +31,17 @@ module ApplicationController::WithErrors
     end
 
     def render_not_found
-      render file: Rails.root.join('public/404.html'), formats: [:html], status: 404, layout: false
+      respond_to do |format|
+        format.html { render file: Rails.root.join('public/404.html'), formats: [:html], status: 404, layout: false }
+        format.json { render json: { message: "The resource you were looking for doesn't exist." }, status: 404 }
+      end
     end
 
     def render_forbidden
-      render file: Rails.root.join('public/403.html'), formats: [:html], status: 403, layout: false
+      respond_to do |format|
+        format.html { render file: Rails.root.join('public/403.html'), formats: [:html], status: 403, layout: false }
+        format.json { render json: { message: "You do not have access to this resource." }, status: 403 }
+      end
     end
   end
 end
