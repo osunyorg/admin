15c89001d7c2202f8ae4178617a700946bfbb5c4
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-11-29 11:39:27 +0100

    websites api

--- app/views/api/osuny/communication/websites/show.json.jbuilder
+++ app/views/api/osuny/communication/websites/show.json.jbuilder
@@ -1,3 +1,4 @@
+json.id @website.id
 json.name @website.to_s_in(current_language)
 json.url @website.url
 json.deuxfleurs do
@@ -18,5 +19,7 @@ end
 json.showcase do
   json.present @website.in_showcase
   json.highlighted @website.highlighted_in_showcase
-  json.tags @website.showcase_tags
+  json.tags do
+    json.array! @website.showcase_tags, :id, :name, :slug
+  end
 end
--- spec/requests/osuny/v1/communication/websites_spec.rb
+++ spec/requests/osuny/v1/communication/websites_spec.rb
@@ -6,20 +6,17 @@ RSpec.describe 'Osuny API' do
       tags 'Communication::Website'
       response '200', 'successful operation' do
         schema type: :object, properties: {
-          id: { 
-            type: :string, 
-            example: '6d8fb0bb-0445-46f0-8954-0e25143e7a58', 
-            title: 'Website identifier' 
-          },
-          name: { 
+          id: {
             type: :string,
-            example: 'Site de démo',
-            title: 'Nom du site'
+            example: '6d8fb0bb-0445-46f0-8954-0e25143e7a58'
           },
-          url: { 
+          name: {
             type: :string,
-            example: 'https://example.osuny.org',
-            title: 'URL du site'
+            example: 'Site de démo'
+          },
+          url: {
+            type: :string,
+            example: 'https://example.osuny.org'
           },
         }
         run_test!
@@ -29,11 +26,121 @@ RSpec.describe 'Osuny API' do
   path '/communication/websites/{id}' do
     get 'Shows a website' do
       tags 'Communication::Website'
-      parameter name: :id, in: :path, type: :string, 
+      parameter name: :id, in: :path, type: :string,
                 description: 'Identifier', example: '6d8fb0bb-0445-46f0-8954-0e25143e7a58'
 
       let(:id) { '6d8fb0bb-0445-46f0-8954-0e25143e7a58' }
       response '200', 'successful operation' do
+        schema type: :object, properties: {
+          id: {
+            type: :string,
+            example: '6d8fb0bb-0445-46f0-8954-0e25143e7a58'
+          },
+          name: {
+            type: :string,
+            example: 'Site de démo'
+          },
+          url: {
+            type: :string,
+            example: 'https://example.osuny.org'
+          },
+          deuxfleurs: {
+            type: :object,
+            title: "Information about Deuxfleurs hosting",
+            properties: {
+              hosting: {
+                type: :boolean,
+                example: true
+              },
+              identifier: {
+                type: :string,
+                example: 'demo-example'
+              }
+            }
+          },
+          features: {
+            type: :object,
+            title: 'Enabled features',
+            properties: {
+              agenda: {
+                type: :boolean,
+                title: 'Agenda (events)',
+                example: true
+              },
+              portfolio: {
+                type: :boolean,
+                title: 'Portfolio (projects)',
+                example: false
+              },
+              posts: {
+                type: :boolean,
+                title: 'Posts',
+                example: true
+              }
+            }
+          },
+          git: {
+            type: :object,
+            title: 'Information about Git repository',
+            properties: {
+              branch: {
+                type: :string,
+                example: 'main'
+              },
+              endpoint: {
+                type: :string,
+                title: "Present if different from default provider endpoint",
+                example: ''
+              },
+              provider: {
+                type: :string,
+                example: 'github'
+              },
+              repository: {
+                type: :string,
+                title: 'Repository name with owner',
+                example: 'osunyorg/example'
+              }
+            }
+          },
+          showcase: {
+            type: :object,
+            title: 'Information about Osuny showcase',
+            properties: {
+              present: {
+                type: :boolean,
+                example: true
+              },
+              highlighted: {
+                type: :boolean,
+                example: false
+              },
+              tags: {
+                type: :array,
+                items: {
+                  type: :array,
+                  items: {
+                    type: :object,
+                    properties: {
+                      id: {
+                        type: :string,
+                        example: '00b85e18-3d4b-4422-811d-a33e99ab6ae3'
+                      },
+                      name: {
+                        type: :string,
+                        example: 'Éducation'
+                      },
+                      slug: {
+                        type: :string,
+                        example: 'education'
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
         run_test!
       end
     end
