3e2b7df2d626095f1612c7f126e6f2428170f908
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-12-02 14:18:24 +0100

    updates post

--- app/controllers/api/osuny/communication/websites/posts_controller.rb
+++ app/controllers/api/osuny/communication/websites/posts_controller.rb
@@ -1,14 +1,15 @@
 class Api::Osuny::Communication::Websites::PostsController < Api::Osuny::Communication::Websites::ApplicationController
-  before_action :load_migration_identifier,
-                :load_post,
-                only: [:create, :update]
+  before_action :build_post, only: :create
+  before_action :load_post, only: [:show, :update]
+
+  before_action :load_migration_identifier, only: [:create, :update]
+  before_action :ensure_same_migration_identifier, only: :update
 
   def index
     @posts = website.posts.includes(:localizations)
   end
 
   def show
-    @post = website.posts.find params[:id]
   end
 
   def create
@@ -16,7 +17,7 @@ class Api::Osuny::Communication::Websites::PostsController < Api::Osuny::Communi
     if @post.save
       render :show, status: :created
     else
-      render json: @post.errors, status: :unprocessable_entity
+      render json: { errors: @post.errors }, status: :unprocessable_entity
     end
   end
 
@@ -24,21 +25,29 @@ class Api::Osuny::Communication::Websites::PostsController < Api::Osuny::Communi
     if @post.update post_params
       render :show
     else
-      render json: @post.errors, status: :unprocessable_entity
+      render json: { errors: @post.errors }, status: :unprocessable_entity
     end
   end
 
   protected
+
+  def build_post
+    @post = website.posts.build
+  end
+
+  def load_post
+    @post = website.posts.find params[:id]
+  end
 
   def load_migration_identifier
     @migration_identifier = post_params[:migration_identifier]
     render_on_missing_migration_identifier unless @migration_identifier.present?
   end
 
-  def load_post
-    @post = website.posts.where(
-      migration_identifier: @migration_identifier
-    ).first_or_initialize
+  def ensure_same_migration_identifier
+    if @post.migration_identifier != @migration_identifier
+      render json: { error: 'Migration identifier does not match' }, status: :unprocessable_entity
+    end
   end
 
   def post_params
@@ -53,9 +62,21 @@ class Api::Osuny::Communication::Websites::PostsController < Api::Osuny::Communi
                           communication_website_id: website.id
                         )
     permitted_params[:localizations_attributes].each do |localization_attributes|
-      language_iso_code = localization_attributes.delete(:language)
-      localization_attributes[:language_id] = Language.find_by(iso_code: language_iso_code)&.id
+      set_id_and_language_id_to_l10n_attributes(localization_attributes)
     end
     permitted_params
+  end
+
+  def set_id_and_language_id_to_l10n_attributes(l10n_attributes)
+    l10n_migration_identifier = l10n_attributes[:migration_identifier]
+    language_iso_code = l10n_attributes.delete(:language)
+    l10n_attributes[:language_id] = Language.find_by(iso_code: language_iso_code)&.id
+    if @post.persisted?
+      post_l10n = @post.localizations.find_by(
+        migration_identifier: l10n_migration_identifier,
+        language_id: l10n_attributes[:language_id]
+      )
+      l10n_attributes[:id] = post_l10n.id if post_l10n.present?
+    end
   end
 end
--- spec/requests/osuny/v1/communication/websites/posts_spec.rb
+++ spec/requests/osuny/v1/communication/websites/posts_spec.rb
@@ -70,24 +70,28 @@ RSpec.describe 'Communication::Website::Post' do
         },
         required: [:post]
       }
-      let(:communication_website_post) { { post: {
-        migration_identifier: 'post-from-api-1',
-        full_width: false,
-        localizations_attributes: [
-          {
-            migration_identifier: 'post-from-api-1-fr',
-            language: 'fr',
-            title: 'Ma nouvelle actualité',
-            meta_description: 'Une nouvelle actualité depuis l\'API',
-            pinned: false,
-            published: true,
-            published_at: '2024-11-29T16:49:00Z',
-            slug: 'ma-nouvelle-actualite',
-            subtitle: 'Une nouvelle actualité',
-            summary: 'Ceci est une nouvelle actualité créée depuis l\'API.'
+      let(:communication_website_post) {
+        {
+          post: {
+            migration_identifier: 'post-from-api-1',
+            full_width: false,
+            localizations_attributes: [
+              {
+                migration_identifier: 'post-from-api-1-fr',
+                language: 'fr',
+                title: 'Ma nouvelle actualité',
+                meta_description: 'Une nouvelle actualité depuis l\'API',
+                pinned: false,
+                published: true,
+                published_at: '2024-11-29T16:49:00Z',
+                slug: 'ma-nouvelle-actualite',
+                subtitle: 'Une nouvelle actualité',
+                summary: 'Ceci est une nouvelle actualité créée depuis l\'API.'
+              }
+            ]
           }
-        ]
-      } } }
+        }
+      }
 
       response '201', 'Successful creation' do
         it 'creates a post and its localization', rswag: true do |example|
@@ -99,23 +103,27 @@ RSpec.describe 'Communication::Website::Post' do
       end
 
       response '400', 'Missing migration identifier.' do
-        let(:communication_website_post) { { post: {
-          full_width: false,
-          localizations_attributes: [
-            {
-              migration_identifier: 'post-from-api-1-fr',
-              language: 'fr',
-              title: 'Ma nouvelle actualité',
-              meta_description: 'Une nouvelle actualité depuis l\'API',
-              pinned: false,
-              published: true,
-              published_at: '2024-11-29T16:49:00Z',
-              slug: 'ma-nouvelle-actualite',
-              subtitle: 'Une nouvelle actualité',
-              summary: 'Ceci est une nouvelle actualité créée depuis l\'API.'
+        let(:communication_website_post) {
+          {
+            post: {
+              full_width: false,
+              localizations_attributes: [
+                {
+                  migration_identifier: 'post-from-api-1-fr',
+                  language: 'fr',
+                  title: 'Ma nouvelle actualité',
+                  meta_description: 'Une nouvelle actualité depuis l\'API',
+                  pinned: false,
+                  published: true,
+                  published_at: '2024-11-29T16:49:00Z',
+                  slug: 'ma-nouvelle-actualite',
+                  subtitle: 'Une nouvelle actualité',
+                  summary: 'Ceci est une nouvelle actualité créée depuis l\'API.'
+                }
+              ]
             }
-          ]
-        } } }
+          }
+        }
         run_test!
       end
 
@@ -130,19 +138,21 @@ RSpec.describe 'Communication::Website::Post' do
       end
 
       response '422', 'Invalid parameters' do
-        let(:communication_website_post) do
+        let(:communication_website_post) {
           {
-            migration_identifier: 'post-from-api-1',
-            full_width: false,
-            localizations_attributes: [
-              {
-                migration_identifier: 'post-from-api-1-fr',
-                language: 'fr',
-                title: nil
-              }
-            ]
+            post: {
+              migration_identifier: 'post-from-api-1',
+              full_width: false,
+              localizations_attributes: [
+                {
+                  migration_identifier: 'post-from-api-1-fr',
+                  language: 'fr',
+                  title: nil
+                }
+              ]
+            }
           }
-        end
+        }
         run_test!
       end
     end
@@ -175,6 +185,148 @@ RSpec.describe 'Communication::Website::Post' do
 
       response '404', 'Post not found' do
         let(:id) { 'fake-id' }
+        run_test!
+      end
+    end
+
+    patch 'Updates a post' do
+      tags 'Communication::Website::Post'
+      security [{ api_key: [] }]
+      let("X-Osuny-Token") { university_apps(:default_app).token }
+
+      parameter name: :website_id, in: :path, type: :string, description: 'Website identifier'
+      let(:website_id) { communication_websites(:website_with_github).id }
+      parameter name: :id, in: :path, type: :string, description: 'Post identifier'
+      let(:id) { communication_website_posts(:test_post).id }
+
+      parameter name: :communication_website_post, in: :body, type: :object, schema: {
+        type: :object,
+        properties: {
+          post: {
+            type: :object,
+            properties: {
+              migration_identifier: { type: :string, description: 'Unique migration identifier of the post' },
+              full_width: { type: :boolean },
+              localizations_attributes: {
+                type: :array,
+                items: {
+                  type: :object,
+                  properties: {
+                    migration_identifier: { type: :string, description: 'Unique migration identifier of the localization' },
+                    language: { type: :string, description: 'ISO 639-1 code of the language', example: 'fr' },
+                    title: { type: :string },
+                    # TODO Featured image & blocks
+                    meta_description: { type: :string },
+                    pinned: { type: :boolean },
+                    published: { type: :boolean },
+                    published_at: { type: :string, format: 'date-time' },
+                    slug: { type: :string },
+                    subtitle: { type: :string },
+                    summary: { type: :string },
+                    text: { type: :string }
+                  },
+                  required: [:migration_identifier, :language, :title]
+                }
+              }
+            },
+            required: [:migration_identifier, :localizations_attributes]
+          }
+        },
+        required: [:post]
+      }
+      let(:communication_website_post) {
+        test_post = communication_website_posts(:test_post)
+        test_post_l10n = communication_website_post_localizations(:test_post_fr)
+        {
+          post: {
+            migration_identifier: test_post.migration_identifier,
+            full_width: test_post.full_width,
+            localizations_attributes: [
+              {
+                migration_identifier: test_post_l10n.migration_identifier,
+                language: test_post_l10n.language.iso_code,
+                title: "Mon nouveau titre",
+                meta_description: test_post_l10n.meta_description,
+                pinned: test_post_l10n.pinned,
+                published: test_post_l10n.published,
+                published_at: test_post_l10n.published_at,
+                slug: test_post_l10n.slug,
+                subtitle: test_post_l10n.subtitle,
+                summary: test_post_l10n.summary
+              }
+            ]
+          }
+        }
+      }
+
+      response '200', 'Successful update' do
+        run_test! do |response|
+          assert_equal("Mon nouveau titre", communication_website_post_localizations(:test_post_fr).reload.title)
+        end
+      end
+
+      response '400', 'Missing migration identifier.' do
+        let(:communication_website_post) {
+          test_post = communication_website_posts(:test_post)
+          test_post_l10n = communication_website_post_localizations(:test_post_fr)
+          {
+            post: {
+              full_width: test_post.full_width,
+              localizations_attributes: [
+                {
+                  migration_identifier: test_post_l10n.migration_identifier,
+                  language: test_post_l10n.language.iso_code,
+                  title: test_post_l10n.title,
+                  meta_description: test_post_l10n.meta_description,
+                  pinned: test_post_l10n.pinned,
+                  published: test_post_l10n.published,
+                  published_at: test_post_l10n.published_at,
+                  slug: test_post_l10n.slug,
+                  subtitle: test_post_l10n.subtitle,
+                  summary: test_post_l10n.summary
+                }
+              ]
+            }
+          }
+        }
+        run_test!
+      end
+
+      response '401', 'Unauthorized. Please make sure you provide a valid API key.' do
+        let("X-Osuny-Token") { 'fake-token' }
+        run_test!
+      end
+
+      response '404', 'Website not found' do
+        let(:website_id) { 'fake-id' }
+        run_test!
+      end
+
+      response '422', 'Invalid parameters' do
+        let(:communication_website_post) {
+          test_post = communication_website_posts(:test_post)
+          test_post_l10n = communication_website_post_localizations(:test_post_fr)
+          {
+            post: {
+              migration_identifier: test_post.migration_identifier,
+              full_width: test_post.full_width,
+              localizations_attributes: [
+                {
+                  migration_identifier: test_post_l10n.migration_identifier,
+                  language: test_post_l10n.language.iso_code,
+                  title: nil,
+                  meta_description: test_post_l10n.meta_description,
+                  pinned: test_post_l10n.pinned,
+                  published: test_post_l10n.published,
+                  published_at: test_post_l10n.published_at,
+                  slug: test_post_l10n.slug,
+                  subtitle: test_post_l10n.subtitle,
+                  summary: test_post_l10n.summary
+                }
+              ]
+            }
+          }
+        }
         run_test!
       end
     end
