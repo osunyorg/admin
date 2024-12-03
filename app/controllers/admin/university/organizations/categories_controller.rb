212236095d9712300ae1c0b6c564a96f6d91f668
Arnaud Levy <contact@arnaudlevy.com>
2024-12-03 11:43:54 +0100

    controllers

--- app/controllers/admin/university/organizations/categories_controller.rb
+++ app/controllers/admin/university/organizations/categories_controller.rb
@@ -84,7 +84,8 @@ class Admin::University::Organizations::CategoriesController < Admin::University
     params.require(:university_organization_category).permit(
       :is_taxonomy, :parent_id,
       localizations_attributes: [
-        :id, :name, :slug, :language_id
+        :id, :name, :slug, :summary, :language_id,
+        :featured_image, :featured_image_delete, :featured_image_infos, :featured_image_alt, :featured_image_credit
       ]).merge(university_id: current_university.id)
   end
 end
