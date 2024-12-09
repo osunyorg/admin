7a9aa068056e1ab9512bb2af7c5d06c3a16372c1
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-12-09 10:31:12 +0100

    fix file component schema

--- app/models/communication/block/component/file.rb
+++ app/models/communication/block/component/file.rb
@@ -1,4 +1,18 @@
-class Communication::Block::Component::File < Communication::Block::Component::BaseReference
+class Communication::Block::Component::File < Communication::Block::Component::Base
+
+  def self.openapi_property_type
+    :object
+  end
+
+  def self.openapi_property_additional_properties
+    {
+      properties: {
+        id: { type: :string, format: :uuid, nullable: true },
+        filename: { type: :string, nullable: true },
+        signed_id: { type: :string, nullable: true }
+      }
+    }
+  end
 
   def blob
     return if data.nil? || data['id'].blank?
