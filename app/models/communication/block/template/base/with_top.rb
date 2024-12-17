0e57af99514096298102b00061770c99747b323e
Arnaud Levy <arnaud.levy@noesya.coop>
2024-12-17 16:57:25 +0100

    Ajout de la variable screen_reader_only au block top (#2509)
    * Screen reader only
    
    * active

--- app/models/communication/block/template/base/with_top.rb
+++ app/models/communication/block/template/base/with_top.rb
@@ -1,19 +1,27 @@
 module Communication::Block::Template::Base::WithTop
   extend ActiveSupport::Concern
 
+  def top_active
+    top_title.present? || top_description.present?
+  end
+
   def top_title
     block.try(:title)
   end
 
-  def top_description
-    try(:description)
+  def top_heading
+    block.heading_rank_base
   end
 
   def top_link
     nil
   end
 
-  def top_heading
-    block.heading_rank_base
+  def top_screen_reader_only
+    false
+  end
+
+  def top_description
+    try(:description)
   end
 end
