bee45eca114722a04384f7f1fd6200ce8c113b49
Arnaud Levy <contact@arnaudlevy.com>
2024-12-28 09:39:28 +0100

    wip

--- app/models/communication/website/page/legal_term.rb
+++ app/models/communication/website/page/legal_term.rb
@@ -1,5 +1,18 @@
 class Communication::Website::Page::LegalTerm < Communication::Website::Page
 
+  DEFAULT_CONTENT = [
+    {
+      title: 'Direction de publication',
+      template_kind: :title
+    },
+    {
+      template_kind: :chapter,
+      data: {
+        text: '<p>[Prénom Nom]<br>[Adresse postale]<br>[Adresse email]</p>'
+      }
+    }
+  ]
+
   def draftable?
     false
   end
