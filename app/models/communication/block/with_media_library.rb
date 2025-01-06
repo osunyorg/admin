d0c2fbcb5bbbd451f1a9f30e9fe939a7b79cf0a3
Arnaud Levy <contact@arnaudlevy.com>
2025-01-06 12:24:50 +0100

    wip
    Co-authored-by: Sébastien Gaya <sebastien.gaya@gmail.com>
    Co-authored-by: Pierre-André Boissinot <pierreandre.boissinot@noesya.coop>

--- app/models/communication/block/with_media_library.rb
+++ app/models/communication/block/with_media_library.rb
@@ -9,6 +9,7 @@ module Communication::Block::WithMediaLibrary
   
   def synchronize_media_library
     media_blob_ids = []
+    # Add
     template.media_blobs.each do |media_blob|
       blob = media_blob[:blob]
       alt = media_blob[:alt]
@@ -23,6 +24,7 @@ module Communication::Block::WithMediaLibrary
         credit: credit
       )
     end
+    # Clean
     Communication::Media::Context
       .where(about: self)
       .where.not(active_storage_blob_id: media_blob_ids)
