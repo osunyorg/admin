0e85fae5d9673d0acf9afee5943586028848d70e
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-01-21 15:42:08 +0100

    fix

--- app/models/education/program/category.rb
+++ app/models/education/program/category.rb
@@ -31,4 +31,12 @@ class Education::Program::Category < ApplicationRecord
                           join_table: :education_program_categories_programs,
                           foreign_key: :education_program_category_id,
                           association_foreign_key: :education_program_id
+
+  def dependencies
+    localizations
+  end
+
+  def references
+    programs
+  end
 end
