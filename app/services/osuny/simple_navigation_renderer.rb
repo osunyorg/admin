9c875be79122dca11c75c99b8ac4715af6a3071c
Arnaud Levy <contact@arnaudlevy.com>
2025-01-28 17:33:35 +0100

    new menu

--- app/services/osuny/simple_navigation_renderer.rb
+++ app/services/osuny/simple_navigation_renderer.rb
@@ -1,52 +1,58 @@
 class Osuny::SimpleNavigationRenderer < SimpleNavigation::Renderer::Base
-  OPEN = "<div class=\"col-sm-6 col-lg-3 mb-5\">"
-  CLOSE = "</div>"
-
-  attr_accessor :content, :index, :item
-
   def render(item_container)
-    @content = ''
-    @index = 0
+    content = '<ul class="navbar-nav">'
     item_container.items.each do |item|
-      @item = item
-      build
-      @index += 1
+      content << make(item)
     end
-    @content << CLOSE
-    @content.html_safe
+    content << '</ul>'
+    content.html_safe
   end
 
   protected
 
-  def build
-    if @index.zero?
-      @content << part
-    elsif item_is_header?
-      @content << "</ul>#{CLOSE}#{part}"
-    else
-      @content << "<li>#{item_name_and_link}</li>"
+  def make(item)
+    has_sub_navigation = consider_sub_navigation?(item)
+    li = "<li class=\"nav-item p-0"
+    li += " active" if item.selected?
+    li += " dropend" if has_sub_navigation
+    li += "\">"
+    li += make_a(item)
+    li += make_subnavigation(item) if has_sub_navigation
+    li += '</li>'
+    li
+  end
+
+  def make_a(item)
+    has_sub_navigation = consider_sub_navigation?(item)
+    a = "<a href=\"#{ item.url }\" class=\"nav-link"
+    a += " dropdown-toggle" if has_sub_navigation
+    a += "\""
+    a += " data-bs-toggle=\"dropdown\" aria-expanded=\"false\"" if has_sub_navigation
+    a += ">"
+    a += item.name
+    a += "</a>"
+    a
+  end
+
+  def make_subnavigation(item)
+    subnavigation = "<div class=\"dropdown-menu submenu bg-dark border-0\">"
+    image = item.options[:image]
+    if image.present?
+      subnavigation += "<a href=\"#{item.url}\" class=\"submenu__image\">";
+      subnavigation += ActionController::Base.helpers.image_tag image, class: 'card-img-top rounded-top', loading: :lazy
+      subnavigation += "</a>";
     end
-  end
-
-  def part
-    part = OPEN
-    if item.options.has_key? :image
-      image = item.options[:image]
-      part += "<a href=\"#{item.url}\">"
-      part += ActionController::Base.helpers.image_tag image, class: 'image', loading: :lazy
-      part += "</a>"
+    subnavigation += "<ul class=\"pt-2\">"
+    item.sub_navigation.items.each do |item|
+      subnavigation += "<li>"
+      if item.url.present?
+        subnavigation += "<a href=\"#{ item.url }\" class=\"dropdown-item bg-dark\">#{item.name}</a>"
+      else
+        subnavigation += "<span class=\"dropdown-item bg-dark text-light\">#{item.name}</span>"
+      end
+      subnavigation += "</li>"
     end
-    part += "<h2>#{item_name_and_link}</h2>"
-    part += "<ul>"
-    part
-  end
-
-  def item_is_header?
-    item.send(:options)[:kind] == :header
-  end
-
-  def item_name_and_link
-    item.url.present? ? "<a href=\"#{item.url}\">#{item.name}</a>"
-                      : "<span>#{item.name}</span>"
+    subnavigation += "</ul></div>"
+    subnavigation
   end
 end
