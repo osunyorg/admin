9a907a428b63399a4c7a3e08d7756f314c820b5e
Arnaud Levy <contact@arnaudlevy.com>
2025-02-06 17:14:16 +0100

    black png

--- app/services/osuny/media/picker.rb
+++ app/services/osuny/media/picker.rb
@@ -65,7 +65,7 @@ class Osuny::Media::Picker
   end
 
   def url
-    @url ||= image.attached? ? "/media/#{image.signed_id}/preview.jpg" : ''
+    @url ||= image.attached? ? "/media/#{image.signed_id}/preview.png" : ''
   end
 
   def about_type
