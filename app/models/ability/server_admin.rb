856c62c11b5e2b3252c17de604884f7e63cff934
Arnaud Levy <contact@arnaudlevy.com>
2025-02-07 18:01:48 +0100

    fix

--- app/models/ability/server_admin.rb
+++ app/models/ability/server_admin.rb
@@ -3,6 +3,6 @@ class Ability::ServerAdmin < Ability
   def initialize(user)
     super
     can :manage, :all
-    cannot :read, User, role: :server_admin
+    cannot :read, User, role: [:server_admin], university_id: @user.university_id
   end
 end
