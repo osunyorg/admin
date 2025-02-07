43245f1fe4b09c2149c1902162dd48557c11385a
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-02-07 15:52:34 +0100

    redirect if feature disabled

--- app/controllers/extranet/documents/application_controller.rb
+++ app/controllers/extranet/documents/application_controller.rb
@@ -1,9 +1,14 @@
 class Extranet::Documents::ApplicationController < Extranet::ApplicationController
+  before_action :redirect_if_feature_disabled
 
   protected
 
   def breadcrumb
     super
     add_breadcrumb Communication::Extranet.human_attribute_name(:feature_documents), documents_root_path
+  end
+
+  def redirect_if_feature_disabled
+    redirect_to extranet_root_path unless current_extranet.feature_documents?
   end
 end
