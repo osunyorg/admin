898fdb6978acfe630e0d531e378ba8762ffae94e
Pierre-André Boissinot <pierreandre.boissinot@noesya.coop>
2025-02-10 15:03:00 +0100

    allow preview of emails (#2698)
    * allow preview of emails
    
    * remove tab

--- test/mailers/previews/notification_mailer_preview.rb
+++ test/mailers/previews/notification_mailer_preview.rb
@@ -19,13 +19,15 @@ class NotificationMailerPreview < BaseMailerPreview
 
   # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/low_sms_credits
   def low_sms_credits
+    user # we will need the user to be created
     credits = 22.0
     NotificationMailer.low_sms_credits(university, credits)
   end
-
+  
   # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/new_registration
   def new_registration
-    NotificationMailer.new_registration(university, user)
+    user # we will need the user to be created
+    NotificationMailer.new_registration(university, visitor_user)
   end
 
 
