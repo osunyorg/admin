313c3c19fee0336fca52096feb7afdb626b5460c
Arnaud Levy <contact@arnaudlevy.com>
2025-03-11 17:28:30 +0100

    better with sebou

--- app/services/migrations/agenda.rb
+++ app/services/migrations/agenda.rb
@@ -2,8 +2,7 @@ class Migrations::Agenda
   def self.migrate
     Communication::Website::Page.where(type: 'Communication::Website::Page::CommunicationAgendaArchive').find_each do |page|
       page.localizations.each do |l10n|
-        l10n.published = false
-        l10n.save
+        l10n.unpublish!
       end
     end
   end
