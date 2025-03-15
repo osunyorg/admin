f0cca0e5101e1cc53bf32a7b54ea822762f2dcee
Arnaud Levy <contact@arnaudlevy.com>
2025-03-15 16:20:16 +0100

    wip

---
+++ app/helpers/admin/buttons_helper.rb
@@ -0,0 +1,31 @@
+module Admin::ButtonsHelper
+
+  def button_classes(additional = '', **options)
+    classes = "btn btn-light #{additional}"
+    classes += ' disabled' if options[:disabled]
+    classes
+  end
+
+  def button_classes_major(additional = '', **options)
+    classes = "btn btn-primary #{additional}"
+    classes += ' disabled' if options[:disabled]
+    classes
+  end
+
+  def button_classes_danger(**options)
+    classes = 'btn btn-danger'
+    classes += ' disabled' if options[:disabled]
+    classes
+  end
+
+  def button_advanced(&block)
+    id = SecureRandom.hex(10)
+    html = "<button class=\"btn btn-light\" type=\"button\" data-bs-toggle=\"dropdown\" data-bs-target=\"##{id}\" aria-controls=\"menu\" aria-expanded=\"false\" aria-label=\"Toggle actions\">"
+    html += lucide_icon('cog') 
+    html += "</button>"
+    html += "<div class=\"dropdown-menu border-0 bg-light mt-2 p-3\" id=\"#{id}\">"
+    html += capture(&block)
+    html += "</div>"
+    raw html
+  end
+end
