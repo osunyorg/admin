0220d7140f2ca90c79a8a3fd114bba6ac9432391
Arnaud Levy <contact@arnaudlevy.com>
2025-05-01 10:13:44 +0200

    refactor

--- app/controllers/server/background_jobs_controller.rb
+++ app/controllers/server/background_jobs_controller.rb
@@ -1,37 +1,14 @@
 class Server::BackgroundJobsController < Server::ApplicationController
   
   def index
-    @jobs = GoodJob::Job.where(finished_at: nil)
-    @universities = {}
-    labels = @jobs.pluck(:labels).flatten.compact.uniq
-    labels.each do |label|
-      next unless label.start_with?('gid://osuny/University')
-      university = GlobalID::Locator.locate(label)
-      jobs = @jobs.where('labels @> ?', "{#{label}}")
-      @universities[university.id] = {
-        label: university.to_s,
-        count: jobs.count,
-        websites: []
-      }
-    end
-    labels.each do |label|
-      next unless label.start_with?('gid://osuny/Communication::Website')
-      website = GlobalID::Locator.locate(label)
-      university = website.university
-      jobs = @jobs.where('labels @> ?', "{#{label}}")
-      @universities[university.id][:websites] << {
-        label: website.to_s,
-        count: jobs.count
-      }
-    end
-    @universities = @universities.sort_by { |key, value| value[:count] }.reverse!
-    @universities.each do |id, hash|
-      hash[:websites] = hash[:websites].sort_by { |value| value[:count] }.reverse!
-    end
+    @analyzer = BackgroundJobs::Analyzer.new
+    @jobs = @analyzer.jobs
+    @universities = @analyzer.universities
     breadcrumb
   end
 
   protected
+
   def breadcrumb
     super
     add_breadcrumb 'Jobs'
