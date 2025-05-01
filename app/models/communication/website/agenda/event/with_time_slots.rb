22e5caf5b1017078ee98d6287e8ae82b5b8b4b35
Arnaud Levy <contact@arnaudlevy.com>
2025-05-01 11:51:35 +0200

    climate

--- app/models/communication/website/agenda/event/with_time_slots.rb
+++ app/models/communication/website/agenda/event/with_time_slots.rb
@@ -21,18 +21,9 @@ module Communication::Website::Agenda::Event::WithTimeSlots
     existing_slots_ids = []
     # Create slots
     params[:slots].each do |slot|
-      next if slot[:date].blank?  || slot[:time].blank?
       break if existing_slots_ids.length >= TIME_SLOTS_LIMIT
-      date = slot[:date].to_date
-      time = slot[:time].to_time
-      datetime = Time.new date.year,
-                          date.month,
-                          date.day,
-                          time.hour,
-                          time.min,
-                          time.sec,
-                          ActiveSupport::TimeZone[time_zone]
-      time_slot = save_time_slot(slot, datetime, language)
+      time_slot = time_slot_from_param(slot, language)
+      next if time_slot.nil?
       existing_slots_ids << time_slot.id
     end
     # Delete obsolete slots
@@ -41,6 +32,39 @@ module Communication::Website::Agenda::Event::WithTimeSlots
     end
     # Sync event after time slots save
     sync_with_git
+  end
+
+  def time_slots_to_json(language)
+    {
+      min: from_day.iso8601,
+      max: to_day.iso8601,
+      slots: time_slots.ordered.map { |slot|
+        l10n = slot.localization_for(language)
+        {
+          id: slot.id,
+          date: slot.date.iso8601,
+          time: slot.time,
+          place: l10n&.place,
+          duration: slot.duration
+        }
+      }
+    }
+  end
+
+  protected
+
+  def time_slot_from_param(slot, language)
+    return if slot[:date].blank? || slot[:time].blank?
+    date = slot[:date].to_date
+    time = slot[:time].to_time
+    datetime = Time.new date.year,
+                        date.month,
+                        date.day,
+                        time.hour,
+                        time.min,
+                        time.sec,
+                        ActiveSupport::TimeZone[time_zone]
+    save_time_slot(slot, datetime, language)
   end
 
   # slot is a hash
@@ -70,20 +94,4 @@ module Communication::Website::Agenda::Event::WithTimeSlots
     time_slot
   end
 
-  def time_slots_to_json(language)
-    {
-      min: from_day.iso8601,
-      max: to_day.iso8601,
-      slots: time_slots.ordered.map { |slot|
-        l10n = slot.localization_for(language)
-        {
-          id: slot.id,
-          date: slot.date.iso8601,
-          time: slot.time,
-          place: l10n&.place,
-          duration: slot.duration
-        }
-      }
-    }
-  end
 end
