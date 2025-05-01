e00503482a8a3bd43a71365f5b917c4477d3b148
Arnaud Levy <contact@arnaudlevy.com>
2025-05-01 09:32:12 +0200

    better sort

--- app/controllers/server/background_jobs_controller.rb
+++ app/controllers/server/background_jobs_controller.rb
@@ -2,24 +2,32 @@ class Server::BackgroundJobsController < Server::ApplicationController
   
   def index
     @jobs = GoodJob::Job.where(finished_at: nil)
-    @jobs_by_university = []
-    @jobs_by_website = [] 
-    @labels = @jobs.pluck(:labels)
-                   .flatten
-                   .compact
-                   .uniq.each do |label|
-      if label.start_with?('gid://osuny/University')
-        university = GlobalID::Locator.locate(label)
-        jobs = @jobs.where('labels @> ?', "{#{label}}")
-        @jobs_by_university << [university.to_s, jobs.count]
-      elsif label.start_with?('gid://osuny/Communication::Website')
-        website = GlobalID::Locator.locate(label)
-        jobs = @jobs.where('labels @> ?', "{#{label}}")
-        @jobs_by_website << [website.to_s, jobs.count]
-      end
+    @universities = {}
+    labels = @jobs.pluck(:labels).flatten.compact.uniq
+    labels.each do |label|
+      next unless label.start_with?('gid://osuny/University')
+      university = GlobalID::Locator.locate(label)
+      jobs = @jobs.where('labels @> ?', "{#{label}}")
+      @universities[university.id] = {
+        label: university.to_s,
+        count: jobs.count,
+        websites: []
+      }
     end
-    @jobs_by_university.sort_by!(&:last).reverse!
-    @jobs_by_website.sort_by!(&:last).reverse!
+    labels.each do |label|
+      next unless label.start_with?('gid://osuny/Communication::Website')
+      website = GlobalID::Locator.locate(label)
+      university = website.university
+      jobs = @jobs.where('labels @> ?', "{#{label}}")
+      @universities[university.id][:websites] << {
+        label: website.to_s,
+        count: jobs.count
+      }
+    end
+    @universities = @universities.sort_by { |key, value| value[:count] }.reverse!
+    @universities.each do |id, hash|
+      hash[:websites] = hash[:websites].sort_by { |value| value[:count] }.reverse!
+    end
     breadcrumb
   end
 
