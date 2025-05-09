874979ea42d9943197cc238df70a23a7b21196b8
Arnaud Levy <contact@arnaudlevy.com>
2025-05-09 16:47:33 +0200

    Climate

--- app/models/communication/website/git_file.rb
+++ app/models/communication/website/git_file.rb
@@ -30,11 +30,11 @@
 class Communication::Website::GitFile < ApplicationRecord
   # We don't include Sanitizable as this model is never handled by users directly.
 
+  include WithContent
+
   belongs_to :university
   belongs_to :website, class_name: 'Communication::Website'
   belongs_to :about, polymorphic: true
-
-  has_one_attached :current_content_file
 
   scope :desynchronized, -> { where(desynchronized: true) }
   scope :desynchronized_since, -> (time) { desynchronized.where('desynchronized_at > ?', time) }
@@ -71,7 +71,7 @@ class Communication::Website::GitFile < ApplicationRecord
     if about.try(:syncable?)
       # If it's just initialized, it needs to be saved
       save unless persisted?
-      # Anyway, we need to generate content
+      # Anyway, we need to generate content (from WithContent)
       generate_content
     elsif persisted?
       # There, but not syncable, so bye bye
@@ -87,57 +87,6 @@ class Communication::Website::GitFile < ApplicationRecord
       desynchronized: true,
       desynchronized_at: Time.zone.now
     )
-  end
-
-  def current_content
-    return '' unless current_content_file.attached?
-    @current_content ||= ActiveStorage::Utils.text_from_attachment(current_content_file)
-  end
-
-  def generate_content_safely
-    return if up_to_date?
-    @current_content = nil
-    ActiveStorage::Utils.attach_from_text(current_content_file, computed_content, 'file.html')
-    update(
-      current_path: computed_path,
-      current_sha: computed_sha,
-      desynchronized: true,
-      desynchronized_at: Time.zone.now
-    )
-  end
-
-  def computed_path
-    @computed_path ||= needs_deletion? ? nil : about.git_path(website)&.gsub(/\/+/, '/')
-  end
-
-  def needs_deletion?
-    about.nil? || !about.try(:syncable?)
-  end
-
-  def computed_filename
-    @computed_filename ||= File.basename(computed_path)
-  rescue
-    ''
-  end
-
-  def computed_content
-    @computed_content ||= (about.nil? ? '' : Static.render(template_static, about, website))
-  end
-
-  def computed_sha
-    website.git_repository.computed_sha(computed_content)
-  end
-
-  def up_to_date?
-    path_up_to_date? && content_up_to_date?
-  end
-
-  def path_up_to_date?
-    current_path == previous_path
-  end
-
-  def content_up_to_date?
-    current_content == computed_content
   end
 
   def synchronized?
@@ -158,10 +107,6 @@ class Communication::Website::GitFile < ApplicationRecord
   end
 
   protected
-
-  def generate_content
-    Communication::Website::GitFile::GenerateContentJob.perform_later(self)
-  end
 
   def self.manage_permalink(object, website)
     return unless Communication::Website::Permalink.supported_by?(object)
