ffb05f4c56d56db05015bc84a43344eca6b5d87e
Arnaud Levy <contact@arnaudlevy.com>
2025-05-09 16:00:47 +0200

    all files

--- app/services/migrations/git_files.rb
+++ app/services/migrations/git_files.rb
@@ -1,6 +1,6 @@
 class Migrations::GitFiles
   def self.migrate_all
-    Communication::Website::GitFile.desynchronized.find_each do |git_file|
+    Communication::Website::GitFile.find_each do |git_file|
       Communication::Website::GitFile::MigrateJob.perform_later git_file
     end
   end
