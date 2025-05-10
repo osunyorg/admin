46b377b117553729562b83959b581a89670937da
Arnaud Levy <contact@arnaudlevy.com>
2025-05-10 10:57:08 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_08_171008) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_10_081918) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
