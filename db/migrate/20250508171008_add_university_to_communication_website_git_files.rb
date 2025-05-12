19373e0d0bc2c5d4d2e0e80714ea7a3ea7aef6f5
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-05-12 10:34:26 +0200

    fix migration

--- db/migrate/20250508171008_add_university_to_communication_website_git_files.rb
+++ db/migrate/20250508171008_add_university_to_communication_website_git_files.rb
@@ -1,9 +1,10 @@
 class AddUniversityToCommunicationWebsiteGitFiles < ActiveRecord::Migration[8.0]
   def change
     add_reference :communication_website_git_files, :university, null: true, foreign_key: true, type: :uuid
-    Communication::Website::GitFile.find_each do |git_file|
-      git_file.update_column(:university_id, git_file.website.university_id)
+    Communication::Website::GitFile.reset_column_information
+    Communication::Website.find_each do |website|
+      website.website_git_files.update_all(university_id: website.university_id)
     end
-    change_column_null :communication_website_git_files, :university_id, true
+    change_column_null :communication_website_git_files, :university_id, false
   end
 end
