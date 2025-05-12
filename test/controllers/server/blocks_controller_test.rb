6a200c4ed3d76be877b7cfc679e7e4367995e2b9
Arnaud Levy <contact@arnaudlevy.com>
2025-05-12 08:17:13 +0200

    Fix #2208

--- test/controllers/server/blocks_controller_test.rb
+++ test/controllers/server/blocks_controller_test.rb
@@ -12,9 +12,4 @@ class Server::BlocksControllerTest < ActionDispatch::IntegrationTest
     get(server_block_path("chapter"))
     assert_response(:success)
   end
-
-  def test_resave
-    post(resave_server_block_path("chapter"))
-    assert_redirected_to(server_block_path("chapter"))
-  end
 end
