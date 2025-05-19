97ee315b771b922eb3cb855a571b3654448a53ff
Arnaud Levy <arnaud.levy@noesya.coop>
2025-05-19 14:24:48 +0200

    Position correcte dans les arbres (#2993)
    * Generation
    
    Co-authored-by: Sébastien Gaya <sebastien.gaya@gmail.com>
    
    * remove _and_sync methods
    
    * sync
    
    * modifs
    
    * modifs
    
    * wip
    
    * wip
    
    * clean
    
    * simplify
    
    * simpler
    
    * better
    
    * wip
    
    * split
    
    * remove
    
    * remove from extranets
    
    * handle more than one batch
    
    * Revert "remove from extranets"
    
    This reverts commit 94a000b25e83c584de91142e7fb2af0b411aa14d.
    
    * Revert "split"
    
    This reverts commit 02bab6ab83f5920b9abba645378d8d4bfdf46da2.
    
    * commit ca
    
    * Enhance bin/dev
    
    Based on https://github.com/rails/rails/pull/53035
    
    * revert bin/dev
    
    * wip
    
    * ok
    
    * method order
    
    * wip
    
    * upload
    
    * fix
    
    * memo
    
    * Done with cleans
    
    Co-authored-by: Sébastien Gaya <sebastien.gaya@gmail.com>
    Co-authored-by: Pierre-André Boissinot <pierreandre.boissinot@noesya.coop>
    
    * done
    
    * test adjustment
    
    * Fix connections test
    
    * fix
    
    * connection done
    
    * comment
    
    * migration
    
    Co-authored-by: Sébastien Gaya <sebastien.gaya@gmail.com>
    Co-authored-by: Pierre-André Boissinot <pierreandre.boissinot@noesya.coop>
    
    * explicit nil
    
    * fix
    
    * puts
    
    * Robustness
    
    * fix
    
    * wip
    
    * complete tests
    
    * all files
    
    * fix
    
    * Climate
    
    * Fix #1955
    
    * Fix https://github.com/osunyorg/theme/issues/1040
    
    * Fix https://github.com/osunyorg/theme/issues/1040
    
    * proper order
    
    * schema
    
    * fix
    
    ---------
    
    Co-authored-by: Sébastien Gaya <sebastien.gaya@gmail.com>
    Co-authored-by: pabois <pierreandre.boissinot@noesya.coop>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_15_155151) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_19_120222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -1045,6 +1045,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_15_155151) do
     t.uuid "created_by_id"
     t.boolean "full_width", default: true
     t.string "bodyclass"
+    t.string "migration_identifier"
     t.index ["communication_website_id"], name: "idx_on_communication_website_id_aac12e3adb"
     t.index ["created_by_id"], name: "idx_on_created_by_id_7009ee99c6"
     t.index ["university_id"], name: "idx_on_university_id_ac2f4a0bfc"
