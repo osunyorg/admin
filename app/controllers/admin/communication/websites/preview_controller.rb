67a1008acdddc94b4d4fe8672e6c4a6f053b35fd
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-05-22 16:42:44 +0200

    fix preview controller

--- app/controllers/admin/communication/websites/preview_controller.rb
+++ app/controllers/admin/communication/websites/preview_controller.rb
@@ -1,14 +1,14 @@
 class Admin::Communication::Websites::PreviewController < Admin::Communication::Websites::ApplicationController
   def style
-    render  body: @website.preview_style, 
+    render  body: @website.preview_style,
             content_type: "text/css"
   end
 
   def assets
     path = request.path.partition('/assets').last
     url = "#{@website.url}/assets#{path}"
-    data = URI(url).open.read
-    render  body: data, 
+    data = URI.parse(url).open.read
+    render  body: data,
             content_type: request.format.to_s
   end
 end
