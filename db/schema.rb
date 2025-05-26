c987c7bfbf0addba491d36c2afb563029ca54bb8
pabois <pierreandre.boissinot@noesya.coop>
2025-05-26 17:44:32 +0200

    connection ok

--- db/schema.rb
+++ db/schema.rb
@@ -805,8 +805,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_26_152736) do
     t.string "current_path"
     t.string "current_sha"
     t.boolean "desynchronized", default: true
-    t.datetime "desynchronized_at"
-    t.uuid "university_id"
+    t.datetime "desynchronized_at", default: -> { "CURRENT_TIMESTAMP" }
+    t.uuid "university_id", null: false
     t.index ["about_type", "about_id"], name: "index_communication_website_github_files_on_about"
     t.index ["desynchronized_at"], name: "index_communication_website_git_files_on_desynchronized_at"
     t.index ["university_id"], name: "index_communication_website_git_files_on_university_id"
@@ -1153,7 +1153,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_26_152736) do
     t.uuid "created_by_id"
     t.boolean "full_width", default: true
     t.string "bodyclass"
-    t.string "migration_identifier"
     t.index ["communication_website_id"], name: "idx_on_communication_website_id_aac12e3adb"
     t.index ["created_by_id"], name: "idx_on_created_by_id_7009ee99c6"
     t.index ["university_id"], name: "idx_on_university_id_ac2f4a0bfc"
@@ -1300,8 +1299,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_26_152736) do
     t.string "deuxfleurs_access_key_id"
     t.string "deuxfleurs_secret_access_key"
     t.datetime "last_sync_at"
-    t.boolean "feature_alumni", default: false
     t.boolean "feature_jobboard", default: false
+    t.boolean "feature_alumni", default: false
     t.index ["about_type", "about_id"], name: "index_communication_websites_on_about"
     t.index ["default_language_id"], name: "index_communication_websites_on_default_language_id"
     t.index ["university_id"], name: "index_communication_websites_on_university_id"
