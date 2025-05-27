3750d73d9566f2e198cd004b5e80311c51b85a1a
Arnaud Levy <contact@arnaudlevy.com>
2025-05-27 08:11:01 +0200

    clean

--- app/javascript/apps/time-slots/components/Duration.vue
+++ app/javascript/apps/time-slots/components/Duration.vue
@@ -1,70 +1,69 @@
 <script>
-
 export default {
-    props: [
-      'modelValue',
-      'i18n'
-    ],
-    emits: ['update:modelValue'],
-    data () {
-        return {
-            custom: 42,
-            presetValue: '0',
-            presetValues: [
-              ['1800', '30mn'],
-              ['3600', '1h'],
-              ['5400', '1h30'],
-              ['7200', '2h'],
-              ['9000', '2h30'],
-              ['10800', '3h'],
-              ['12600', '3h30'],
-              ['14400', '4h'],
-              ['18000', '5h'],
-              ['21600', '6h'],
-              ['25200', '7h'],
-              ['28800', '8h'],
-            ]
-        }
-    },
-    computed: {
-      value: {
-        get() {
-          if (this.preset === 'custom') {
-            return this.custom;
-          } else {
-            return this.modelValue;
-          }
-        },
-        set(value) {
-          if (this.isInPresetValues(value)) {
-            this.preset = value;
-          } else {
-            this.preset = 'custom'; 
-          }
-          this.$emit('update:modelValue', value);
-        }
-      },
-      preset: {
-        get() {
-          return this.presetValue;
-        },
-        set(value) {
-          this.presetValue = value;
-          this.$emit('update:modelValue', value);
-        }
+  props: [
+    'modelValue',
+    'i18n'
+  ],
+  emits: ['update:modelValue'],
+  data() {
+    return {
+      preset: '0', // sélection par défaut
+      custom: 42,
+      presetValues: [
+        ['1800', '30mn'],
+        ['3600', '1h'],
+        ['5400', '1h30'],
+        ['7200', '2h'],
+        ['9000', '2h30'],
+        ['10800', '3h'],
+        ['12600', '3h30'],
+        ['14400', '4h'],
+        ['18000', '5h'],
+        ['21600', '6h'],
+        ['25200', '7h'],
+        ['28800', '8h'],
+      ],
+    };
+  },
+  created() {
+    if (this.isInPresetValues(this.modelValue)) {
+      this.preset = this.modelValue;
+    } else {
+      this.preset = 'custom';
+      this.custom = this.modelValue;
+    }
+  },
+  watch: {
+    preset(newVal) {
+      if (newVal === 'custom') {
+        this.$emit('update:modelValue', this.custom);
+      } else {
+        this.$emit('update:modelValue', newVal);
       }
     },
-    methods: {
-      isInPresetValues(value) {
-        for (var i = 0; i < this.presetValues.length; i++) {
-          var preset = this.presetValues[i];
-          if (preset[0] == value) {
-            return true;
-          }
-        }
-        return false;
+    custom(newVal) {
+      if (this.preset === 'custom') {
+        this.$emit('update:modelValue', newVal);
       }
     },
+    modelValue(newVal) {
+      if (this.isInPresetValues(newVal)) {
+        this.preset = String(newVal);
+      } else {
+        this.preset = 'custom';
+        this.custom = newVal;
+      }
+    }
+  },
+  methods: {
+    isInPresetValues(value) {
+      value = String(value);
+      if (value === '0') {
+        return true;
+      }
+      return this.presetValues.some(p => p[0] === value);
+    },
+  },
 };
 </script>
 
@@ -72,17 +71,13 @@ export default {
   <div>
     <select class="form-select" v-model="preset">
       <option value="0">{{ i18n.label }}</option>
-      <option v-for="value in presetValues" :value="value[0]">{{ value[1] }}</option>
+      <option v-for="value in presetValues" :key="value[0]" :value="value[0]">
+        {{ value[1] }}
+      </option>
       <option value="custom">{{ i18n.custom }}</option>
     </select>
     <div>
-      <input  type="number"
-              class="form-control"
-              v-model="custom"
-              v-if="preset === 'custom'"> 
-      {{ value }} •
-      {{ preset }} •
-      {{ custom }}
+      <input type="number" class="form-control" v-model.number="custom" v-if="preset === 'custom'">
     </div>
   </div>
 </template>
