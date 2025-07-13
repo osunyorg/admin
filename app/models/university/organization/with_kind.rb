f7a3c00666ac3383c3ed9ad29b94f085c9b34c2f
sachaandre <andresachaa@gmail.com>
2025-07-13 15:50:16 +0200

    adding two kinds for issue #3232

--- app/models/university/organization/with_kind.rb
+++ app/models/university/organization/with_kind.rb
@@ -2,7 +2,7 @@ module University::Organization::WithKind
   extend ActiveSupport::Concern
 
   included do
-    enum :kind, { company: 10, non_profit: 20, government: 30 }
+    enum :kind, { company: 10, non_profit: 20, government: 30, public_org: 40, collective: 50 }
 
     scope :for_kind, -> (kind, language = nil) { where(kind: kind) }
   end
