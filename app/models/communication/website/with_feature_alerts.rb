2753bf242ef8ec5f2dd52ce694e7a51c78373d18
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-07-18 11:45:41 +0200

    Alertes (#3243)
    * add feature alerts
    
    * admin / todo static
    
    * annotate + alert static
    
    * fix git_path
    
    * add alerts to website dependencies + alerts in planned objects
    
    * published_today on alert_localizations

---
+++ app/models/communication/website/with_feature_alerts.rb
@@ -0,0 +1,20 @@
+module Communication::Website::WithFeatureAlerts
+  extend ActiveSupport::Concern
+
+  included do
+    has_many    :alerts,
+                class_name: "Communication::Website::Alert",
+                foreign_key: :communication_website_id,
+                dependent: :destroy
+    has_many    :alert_localizations,
+                class_name: "Communication::Website::Alert::Localization",
+                foreign_key: :communication_website_id,
+                dependent: :destroy
+
+    scope :with_feature_alerts, -> { where(feature_alerts: true) }
+  end
+
+  def feature_alerts_dependencies
+    alerts
+  end
+end
