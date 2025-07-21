1e4046a50aa690c9a98a7afa0f44c8053f6f3ed0
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-07-21 15:05:35 +0200

    ARCHIVABLE_MODEL_NAMES

--- app/models/communication/website/with_content_archive.rb
+++ app/models/communication/website/with_content_archive.rb
@@ -1,6 +1,14 @@
 module Communication::Website::WithContentArchive
   extend ActiveSupport::Concern
 
+  included do
+    ARCHIVABLE_MODEL_NAMES = [
+      'Communication::Website::Post::Localization',
+      'Communication::Website::Agenda::Event::Localization',
+      'Communication::Website::Agenda::Exhibition::Localization'
+    ].freeze
+  end
+
   def max_archive_datetime
     @max_archive_datetime ||= Time.current - years_before_archive_content.years
   end
@@ -9,8 +17,9 @@ module Communication::Website::WithContentArchive
 
   def unpublish_archivable_content
     return unless archive_content?
-    [post_localizations, event_localizations, exhibition_localizations].each do |localizations|
-      localizations.archivable(max_archive_datetime).find_each do |l10n|
+    ARCHIVABLE_MODEL_NAMES.each do |model_name|
+      model = model_name.safe_constantize
+      model.where(communication_website_id: id).archivable(max_archive_datetime).find_each do |l10n|
         l10n.update(published: false)
       end
     end
