15e672ddf09a0a61ebf348ba6c9d438b9f0d51f2
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-07-21 17:25:48 +0200

    alert l10n : should_sync_to

--- app/models/communication/website/alert/localization.rb
+++ app/models/communication/website/alert/localization.rb
@@ -52,8 +52,11 @@ class Communication::Website::Alert::Localization < ApplicationRecord
   before_validation :set_communication_website_id, on: :create
 
   def git_path(website)
-    return unless website.id == communication_website_id && website.active_language_ids.include?(language_id) && published && published_at
     "data/alerts/#{language.iso_code}/#{slug}.yml"
+  end
+
+  def should_sync_to?(website)
+    website.id == communication_website_id && website.active_language_ids.include?(language_id) && published?
   end
 
   def template_static
