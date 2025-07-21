6f069c79cccb596a73ee9c0c474494f845f91b66
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-07-21 15:14:28 +0200

    clean

--- app/models/communication/website/with_content_archive.rb
+++ app/models/communication/website/with_content_archive.rb
@@ -18,10 +18,15 @@ module Communication::Website::WithContentArchive
   def unpublish_archivable_content
     return unless archive_content?
     ARCHIVABLE_MODEL_NAMES.each do |model_name|
-      model = model_name.safe_constantize
-      model.where(communication_website_id: id).archivable(max_archive_datetime).find_each do |l10n|
-        l10n.update(published: false)
-      end
+      unpublish_archivable_model(model_name)
+    end
+  end
+
+  def unpublish_archivable_model(model_name)
+    model = model_name.safe_constantize
+    return unless model
+    model.where(communication_website_id: id).archivable(max_archive_datetime).find_each do |l10n|
+      l10n.update(published: false)
     end
   end
 end
