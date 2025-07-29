70732088c56f704dc5d42e1f322b302065b6da2e
Arnaud Levy <arnaud.levy@noesya.coop>
2025-07-29 12:37:05 +0200

    git queues (#3283)

--- app/jobs/communication/website/git_file/generate_content_job.rb
+++ app/jobs/communication/website/git_file/generate_content_job.rb
@@ -1,7 +1,9 @@
 class Communication::Website::GitFile::GenerateContentJob < ApplicationJob
   discard_on ActiveJob::DeserializationError
+  queue_as :mice
 
   def perform(git_file)
     git_file.generate_content_safely
   end
+
 end
