14016842ea50533c377e71a1d869f80f53ee156c
Arnaud Levy <contact@arnaudlevy.com>
2025-07-30 08:44:30 +0200

    wip

--- app/jobs/communication/website/git_file/generate_content_job.rb
+++ app/jobs/communication/website/git_file/generate_content_job.rb
@@ -1,9 +1,14 @@
 class Communication::Website::GitFile::GenerateContentJob < ApplicationJob
-  discard_on ActiveJob::DeserializationError
   queue_as :mice
 
   def perform(git_file)
     git_file.generate_content_safely
   end
+
+  good_job_control_concurrency_with(
+    enqueue_limit: 1,
+    perform_limit: 1,
+    key: -> { "#{self.class.name}-#{queue_name}-#{arguments.first.to_global_id}" }
+  )
 
 end
