4dbda4b052b42fc6fca3f0a5db78022890bc8c91
Arnaud Levy <contact@arnaudlevy.com>
2025-09-02 15:05:59 +0200

    fix

--- app/models/communication/website/agenda/event/time_slot.rb
+++ app/models/communication/website/agenda/event/time_slot.rb
@@ -102,7 +102,7 @@ class Communication::Website::Agenda::Event::TimeSlot < ApplicationRecord
 
   def end_datetime
     return if datetime.nil? || duration.to_i.zero?
-    datetime + duration.minutes
+    datetime + duration.seconds
   end
 
   def end_date
