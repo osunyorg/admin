841de0bb3d1f3137c92e68bc52b0dee7d105c779
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-09-11 12:43:47 +0200

    set_in_production_at

--- app/models/communication/website/with_production.rb
+++ app/models/communication/website/with_production.rb
@@ -2,6 +2,8 @@ module Communication::Website::WithProduction
   extend ActiveSupport::Concern
 
   included do
+    before_validation :set_in_production_at
+
     scope :in_production, -> { where(in_production: true) }
     scope :for_production, -> (production, language = nil) { where(in_production: production) }
     scope :ordered_by_production_date, -> {
@@ -9,4 +11,9 @@ module Communication::Website::WithProduction
     }
   end
 
+  protected
+
+  def set_in_production_at
+    self.in_production_at = Time.zone.now if in_production && in_production_at.nil?
+  end
 end
