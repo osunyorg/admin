89546d63d0d79dcfb852de4f1f897c0a7598a534
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-09-11 12:36:27 +0200

    remove orga.active & clean api (#3400)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_10_131327) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_11_100918) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -2142,7 +2142,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_10_131327) do
     t.string "country"
     t.string "phone"
     t.string "email"
-    t.boolean "active", default: true
     t.string "siren"
     t.integer "kind", default: 10
     t.datetime "created_at", null: false
