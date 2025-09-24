5ee4a6c9c488ed550cf88ce230acc0b2fdf93cfe
Arnaud Levy <contact@arnaudlevy.com>
2025-09-24 12:37:04 +0200

    fix

--- app/models/concerns/as_localization.rb
+++ app/models/concerns/as_localization.rb
@@ -64,7 +64,7 @@ module AsLocalization
   # standalone-category
   # parent-category/child-category
   def slug_with_ancestors_slugs
-    slug_path do |ancestor|
+    slug_pathes do |ancestor|
       l10n = ancestor.localization_for(language)
       if l10n.nil? || l10n.try(:draft?)
         # If l10n is nil or draft, no slug
@@ -79,7 +79,7 @@ module AsLocalization
   # Used to put the files in the correct directory
   # If an index does not exist in a language, use the default index
   def slug_with_ancestors_slugs_with_draft
-    slug_path do |ancestor|
+    slug_pathes do |ancestor|
       ancestor.best_localization_for(language)&.slug
     end
   end
