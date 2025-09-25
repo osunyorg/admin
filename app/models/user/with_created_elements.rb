f6059867333e070f3e6871a0fca049bc4de3a419
pabois <pierreandre.boissinot@noesya.coop>
2025-09-25 17:39:13 +0200

    adjust

--- app/models/user/with_created_elements.rb
+++ app/models/user/with_created_elements.rb
@@ -2,8 +2,23 @@ module User::WithCreatedElements
   extend ActiveSupport::Concern
 
   included do
-    has_many :created_university_people,
-             class_name: "University::Person",
+    has_many :created_communication_website_agenda_events,
+             class_name: "Communication::Website::Agenda::Event",
+             foreign_key: "created_by_id",
+             dependent: :nullify
+
+    has_many :created_communication_website_agenda_exhibitions,
+             class_name: "Communication::Website::Agenda::Exhibition",
+             foreign_key: "created_by_id",
+             dependent: :nullify
+
+    has_many :created_communication_website_jobboard_jobs,
+             class_name: "Communication::Website::Jobboard::Job",
+             foreign_key: "created_by_id",
+             dependent: :nullify
+
+    has_many :created_communication_website_portfolio_projects,
+             class_name: "Communication::Website::Portfolio::Project",
              foreign_key: "created_by_id",
              dependent: :nullify
 
@@ -12,24 +27,9 @@ module User::WithCreatedElements
              foreign_key: "created_by_id",
              dependent: :nullify
 
-    has_many :created_agenda_events,
-              class_name: "Communication::Website::Agenda::Event",
-              foreign_key: "created_by_id",
-             dependent: :nullify
-
-    has_many :created_agenda_exhibitions,
-              class_name: "Communication::Website::Agenda::Exhibition",
-              foreign_key: "created_by_id",
-             dependent: :nullify
-
-    has_many :created_jobboard_jobs,
-              class_name: "Communication::Website::Jobboard::Job",
-              foreign_key: "created_by_id",
-             dependent: :nullify
-
-    has_many :created_portfolio_projects,
-              class_name: "Communication::Website::Portfolio::Project",
-              foreign_key: "created_by_id",
+    has_many :created_university_people,
+             class_name: "University::Person",
+             foreign_key: "created_by_id",
              dependent: :nullify
   end
 end
