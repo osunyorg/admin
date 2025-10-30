4c961d687648a1f6ae30977dc0408baf8bea7685
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-10-30 10:06:01 +0100

    Actualités fédérées

--- app/controllers/admin/communication/websites/posts_controller.rb
+++ app/controllers/admin/communication/websites/posts_controller.rb
@@ -104,7 +104,8 @@ class Admin::Communication::Websites::PostsController < Admin::Communication::We
   def post_params
     params.require(:communication_website_post)
     .permit(
-      :full_width, :bodyclass, :is_lasting, author_ids: [], category_ids: [],
+      :full_width, :bodyclass, :is_lasting,
+      author_ids: [], category_ids: [], destination_website_ids: [],
       localizations_attributes: [
         :id, :title, :subtitle, :meta_description, :summary,
         :header_cta, :header_cta_label, :header_cta_url,
