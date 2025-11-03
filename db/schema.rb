c9e4ef0c421d6c52639e55f39c4286f5489a6b3b
pabois <pierreandre.boissinot@noesya.coop>
2025-11-03 17:19:13 +0100

    correct concern

--- db/schema.rb
+++ db/schema.rb
@@ -839,8 +839,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_03_133341) do
     t.string "current_path"
     t.string "current_sha"
     t.boolean "desynchronized", default: true
-    t.datetime "desynchronized_at"
-    t.uuid "university_id"
+    t.datetime "desynchronized_at", default: -> { "CURRENT_TIMESTAMP" }
+    t.uuid "university_id", null: false
     t.index ["about_type", "about_id"], name: "index_communication_website_github_files_on_about"
     t.index ["desynchronized_at"], name: "index_communication_website_git_files_on_desynchronized_at"
     t.index ["university_id"], name: "index_communication_website_git_files_on_university_id"
@@ -1090,7 +1090,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_03_133341) do
     t.string "type"
     t.string "migration_identifier"
     t.integer "position_in_tree"
-    t.jsonb "design_options"
     t.datetime "deleted_at"
     t.index ["communication_website_id"], name: "index_communication_website_pages_on_communication_website_id"
     t.index ["deleted_at"], name: "index_communication_website_pages_on_deleted_at"
@@ -1346,8 +1345,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_03_133341) do
     t.string "deuxfleurs_access_key_id"
     t.string "deuxfleurs_secret_access_key"
     t.datetime "last_sync_at"
-    t.boolean "feature_alumni", default: false
     t.boolean "feature_jobboard", default: false
+    t.boolean "feature_alumni", default: false
     t.boolean "feature_alerts", default: false
     t.boolean "archive_content", default: false
     t.integer "years_before_archive_content", default: 3
