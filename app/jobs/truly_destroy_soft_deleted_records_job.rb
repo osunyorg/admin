ae9b5dd85a9971384315ee79f670ce4e3557d465
pabois <pierreandre.boissinot@noesya.coop>
2025-11-03 16:58:56 +0100

    adjust destroy job

--- app/jobs/truly_destroy_soft_deleted_records_job.rb
+++ app/jobs/truly_destroy_soft_deleted_records_job.rb
@@ -8,9 +8,9 @@ class TrulyDestroySoftDeletedRecordsJob < ApplicationJob
   ].freeze
 
   def perform
-    date = Date.today - Lifecyclable::LIFECYCLE_DAYS_BEFORE_DELETION.days
+    date = Date.current - Lifecyclable::LIFECYCLE_DAYS_BEFORE_DELETION.days
     PARANOID_MODELS.each do |model_class|
-      old_objects = model_class.only_deleted.where("deleted_at < ?", date)
+      old_objects = model_class.only_deleted.where("deleted_at < ?", date.end_of_day)
       old_objects.find_each &:really_destroy!
     end
   end
