e4e27bd95a9f975901d2a5f98b96f758aa227347
Arnaud Levy <arnaud.levy@noesya.coop>
2025-11-18 09:45:25 +0100

    fix search in children (#3596)

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -7,6 +7,7 @@ module Filterable
   end
 
   class_methods do
+    # Keep a registry of scope names 
     def scope(name, body, &block)
       scope_names << name.to_s
       super
@@ -15,20 +16,23 @@ module Filterable
     def filter_by(params, language)
       return all unless params
       params.to_unsafe_hash.reduce(all) do |scope, (name, value)|
-        value = filter_clean_value(value)
-        filter_scope(scope, name, value, language)
+        filter_by_scope(scope, name, value, language)
       end
     end
 
     protected
 
-    def filter_scope(scope, name, value, language)
-      should_add_scope = scope_names.include?(name) && value.present?
-      should_add_scope ? scope.public_send(name, value, language) : scope
+    def filter_by_scope(scope, name, value, language)
+      value = filter_clean_value(value)
+      should_filter_by_scope?(scope, name, value) ? scope.public_send(name, value, language) : scope
+    end
+
+    def should_filter_by_scope?(scope, name, value)
+      scope_names.include?(name) && value.present?
     end
 
     def filter_clean_value(value)
-      value = value.strip if value.is_a?(String)
+      value.strip! if value.is_a?(String)
       # params might contain an array with an empty value [""] if it has been reset by select2
       value.compact_blank! if value.is_a?(Array)
       value
