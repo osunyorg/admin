297e780550b947a41d3e7cfe292dbff0a821667d
Arnaud Levy <contact@arnaudlevy.com>
2025-12-31 10:14:24 +0100

    better

--- test/services/variant_service_test.rb
+++ test/services/variant_service_test.rb
@@ -6,6 +6,75 @@ class VariantServiceTest < ActiveSupport::TestCase
 
   include ActiveRecord::TestFixtures
 
+  EXPECTED_PARAMS_PER_FILENAME = [
+    ['dan-gold.jpeg', {}],
+    ['dan-gold.webp', { format: 'webp' }],
+    ['dan-gold_500x500.jpeg', { size: [500, 500] }],
+    ['dan-gold_500x.jpeg', { size: [500, nil] }],
+    ['dan-gold_x500.jpeg', { size: [nil, 500] }],
+    ['dan-gold_crop_left.jpeg', { gravity: 'West' }],
+    ['dan-gold@2x.jpeg', { scale: 2 }],
+    ['dan-gold_500x500_crop_left.jpeg', { size: [500, 500], gravity: 'West' }],
+    ['dan-gold_500x500@2x.jpeg', { size: [500, 500], scale: 2 }],
+    ['dan-gold_250x250@3x.jpeg', { size: [250, 250], scale: 3 }],
+    ['dan-gold_crop_left@2x.jpeg', { gravity: 'West', scale: 2 }],
+    ['dan-gold_250x250_crop_left@2x.jpeg', { size: [250, 250], gravity: 'West', scale: 2 }],
+    ['dan-gold_200x300_crop_top.jpeg', { size: [200, 300], gravity: 'North' }],
+    ['dan-gold_300x200_crop_right@2x.jpeg', { size: [300, 200], gravity: 'East', scale: 2 }],
+    ['dan-gold_1000x500_crop_left.jpeg', { size: [1000, 500], gravity: 'West' }],
+    ['dan-gold_1500x500_crop_left.jpeg', { size: [1500, 500], gravity: 'West' }],
+    ['dan-gold_800x840_crop_left.jpeg', { size: [800, 840], gravity: 'West' }],
+    ['dan-gold', {}],
+    ['dan-gold_500x500', { size: [500, 500] }],
+  ]
+
+  EXPECTED_TRANSFORMATIONS_PER_FILENAME = [
+    ['dan-gold.jpeg', {}],
+    ['dan-gold.webp', { format: 'webp' }],
+    ['dan-gold_500x500.jpeg', { resize_to_limit: [500, 500] }],
+    ['dan-gold_500x.jpeg', { resize_to_limit: [500, nil] }],
+    ['dan-gold_x500.jpeg', { resize_to_limit: [nil, 500] }],
+    ['dan-gold_crop_left.jpeg', {}],
+    ['dan-gold@2x.jpeg', { resize_to_limit: [2972, 1672] }],
+    ['dan-gold_500x500_crop_left.jpeg', { resize_to_fill: [500, 500, { gravity: 'West' }], crop: '500x500+0+0' }],
+    ['dan-gold_500x500@2x.jpeg', { resize_to_limit: [1000, 1000] }],
+    ['dan-gold_250x250@3x.jpeg', { resize_to_limit: [750, 750] }],
+    ['dan-gold_crop_left@2x.jpeg', { resize_to_limit: [2972, 1672] }],
+    ['dan-gold_250x250_crop_left@2x.jpeg', { resize_to_fill: [500, 500, { gravity: 'West' }], crop: '500x500+0+0' }],
+    ['dan-gold_200x300_crop_top.jpeg', { resize_to_fill: [200, 300, { gravity: 'North' }], crop: '200x300+0+0' }],
+    ['dan-gold_300x200_crop_right@2x.jpeg', { resize_to_fill: [600, 400, { gravity: 'East' }], crop: '600x400+0+0' }],
+    ['dan-gold_1000x500_crop_left.jpeg', { resize_to_fill: [1000, 500, { gravity: 'West' }], crop: '1000x500+0+0' }],
+    ['dan-gold_1500x500_crop_left.jpeg', { resize_to_limit: [1500, 500] }],
+    ['dan-gold_800x840_crop_left.jpeg', { resize_to_limit: [800, 840] }],
+    ['dan-gold', { }],
+    ['dan-gold_500x500', { resize_to_limit: [500, 500] }],
+  ]
+
+  EXPECTED_KEYCDN_PARAMS_PER_FILENAME = [
+    ['dan-gold.jpeg', {}],
+    ['dan-gold.webp', { format: 'webp' }],
+    ['dan-gold_500x500.jpeg', { width: 500, height: 500 }],
+    ['dan-gold_500x.jpeg', { width: 500, enlarge: 0 }],
+    ['dan-gold_x500.jpeg', { height: 500, enlarge: 0 }],
+    ['dan-gold_crop_left.jpeg', { }],
+    ['dan-gold@2x.jpeg', { width: 2972, enlarge: 0 }],
+    ['dan-gold_500x500_crop_left.jpeg', { width: 500, height: 500, position: 'left' }],
+    # FIXME je crois que si on demande une image de 500 par 500 en retina, on ne s'attend absolument pas à recevoir une image pas carrée sous prétexte que ce n'est pas assez grand
+    ['dan-gold_500x500@2x.jpeg', { width: 1000, enlarge: 0 }],
+    # FIXME idem
+    ['dan-gold_250x250@3x.jpeg', { width: 750, height: 750 }],
+    # FIXME Je ne sais pas ce que ce test veut dire (sans taille, pourquoi cropper ?)
+    ['dan-gold_crop_left@2x.jpeg', { width: 2972, enlarge: 0 }],
+    ['dan-gold_250x250_crop_left@2x.jpeg', { width: 500, height: 500, position: 'left'  }],
+    ['dan-gold_200x300_crop_top.jpeg', { width: 200, height: 300, position: 'top' }],
+    ['dan-gold_300x200_crop_right@2x.jpeg', { width: 600, height: 400, position: 'right' }],
+    ['dan-gold_1000x500_crop_left.jpeg', { width: 1000, height: 500, position: 'left' }],
+    ['dan-gold_1500x500_crop_left.jpeg', { width: 1500, enlarge: 0 }],
+    ['dan-gold_800x840_crop_left.jpeg', { width: 800, enlarge: 0 }],
+    ['dan-gold', {}],
+    ['dan-gold_500x500', { width: 500, height: 500 }],
+  ]
+
   setup do
     @was_tracking, ActiveStorage.track_variants = ActiveStorage.track_variants, false
   end
@@ -55,97 +124,24 @@ class VariantServiceTest < ActiveSupport::TestCase
     assert_equal 'webp', variant_service.format
   end
 
-  # Active Storage tests
-
-  ACTIVE_STORAGE_PARAMS = [
-    ['dan-gold.jpeg', {}],
-    ['dan-gold.webp', { format: 'webp' }],
-    ['dan-gold_500x500.jpeg', { size: [500, 500] }],
-    ['dan-gold_500x.jpeg', { size: [500, nil] }],
-    ['dan-gold_x500.jpeg', { size: [nil, 500] }],
-    ['dan-gold_crop_left.jpeg', { gravity: 'West' }],
-    ['dan-gold@2x.jpeg', { scale: 2 }],
-    ['dan-gold_500x500_crop_left.jpeg', { size: [500, 500], gravity: 'West' }],
-    ['dan-gold_500x500@2x.jpeg', { size: [500, 500], scale: 2 }],
-    ['dan-gold_250x250@3x.jpeg', { size: [250, 250], scale: 3 }],
-    ['dan-gold_crop_left@2x.jpeg', { gravity: 'West', scale: 2 }],
-    ['dan-gold_250x250_crop_left@2x.jpeg', { size: [250, 250], gravity: 'West', scale: 2 }],
-    ['dan-gold_200x300_crop_top.jpeg', { size: [200, 300], gravity: 'North' }],
-    ['dan-gold_300x200_crop_right@2x.jpeg', { size: [300, 200], gravity: 'East', scale: 2 }],
-    ['dan-gold_1000x500_crop_left.jpeg', { size: [1000, 500], gravity: 'West' }],
-    ['dan-gold_1500x500_crop_left.jpeg', { size: [1500, 500], gravity: 'West' }],
-    ['dan-gold_800x840_crop_left.jpeg', { size: [800, 840], gravity: 'West' }],
-    ['dan-gold', {}],
-    ['dan-gold_500x500', { size: [500, 500] }],
-  ]
-
   test "Active Storage params matrix" do
-    ACTIVE_STORAGE_PARAMS.each do |filename, output|
+    EXPECTED_PARAMS_PER_FILENAME.each do |filename, output|
       blob = create_file_blob(filename: "dan-gold.jpeg")
       variant_service = VariantService.manage(blob, { filename_with_transformations: filename })
       assert_equal output, variant_service.params
     end
   end
 
-  TRANSFORMATIONS = [
-    [{ filename_with_transformations: 'dan-gold.jpeg' }, {}],
-    [{ filename_with_transformations: 'dan-gold.webp' }, { format: 'webp' }],
-    [{ filename_with_transformations: 'dan-gold_500x500.jpeg' }, { resize_to_limit: [500, 500] }],
-    [{ filename_with_transformations: 'dan-gold_500x.jpeg' }, { resize_to_limit: [500, nil] }],
-    [{ filename_with_transformations: 'dan-gold_x500.jpeg' }, { resize_to_limit: [nil, 500] }],
-    [{ filename_with_transformations: 'dan-gold_crop_left.jpeg' }, {}],
-    [{ filename_with_transformations: 'dan-gold@2x.jpeg' }, { resize_to_limit: [2972, 1672] }],
-    [{ filename_with_transformations: 'dan-gold_500x500_crop_left.jpeg' }, { resize_to_fill: [500, 500, { gravity: 'West' }], crop: '500x500+0+0' }],
-    [{ filename_with_transformations: 'dan-gold_500x500@2x.jpeg' }, { resize_to_limit: [1000, 1000] }],
-    [{ filename_with_transformations: 'dan-gold_250x250@3x.jpeg' }, { resize_to_limit: [750, 750] }],
-    [{ filename_with_transformations: 'dan-gold_crop_left@2x.jpeg' }, { resize_to_limit: [2972, 1672] }],
-    [{ filename_with_transformations: 'dan-gold_250x250_crop_left@2x.jpeg' }, { resize_to_fill: [500, 500, { gravity: 'West' }], crop: '500x500+0+0' }],
-    [{ filename_with_transformations: 'dan-gold_200x300_crop_top.jpeg' }, { resize_to_fill: [200, 300, { gravity: 'North' }], crop: '200x300+0+0' }],
-    [{ filename_with_transformations: 'dan-gold_300x200_crop_right@2x.jpeg' }, { resize_to_fill: [600, 400, { gravity: 'East' }], crop: '600x400+0+0' }],
-    [{ filename_with_transformations: 'dan-gold_1000x500_crop_left.jpeg' }, { resize_to_fill: [1000, 500, { gravity: 'West' }], crop: '1000x500+0+0' }],
-    [{ filename_with_transformations: 'dan-gold_1500x500_crop_left.jpeg' }, { resize_to_limit: [1500, 500] }],
-    [{ filename_with_transformations: 'dan-gold_800x840_crop_left.jpeg' }, { resize_to_limit: [800, 840] }],
-    [{ filename_with_transformations: 'dan-gold' }, { }],
-    [{ filename_with_transformations: 'dan-gold_500x500' }, { resize_to_limit: [500, 500] }],
-  ]
-
   test "transformations matrix" do
-    TRANSFORMATIONS.each do |input, output|
+    EXPECTED_TRANSFORMATIONS_PER_FILENAME.each do |filename, output|
       blob = create_file_blob(filename: "dan-gold.jpeg")
-      variant_service = VariantService.manage(blob, input)
+      variant_service = VariantService.manage(blob, { filename_with_transformations: filename })
       assert_equal output, variant_service.transformations
     end
   end
 
-  # KeyCDN tests
-
-  KEYCDN_PARAMS = [
-    ['dan-gold.jpeg', {}],
-    ['dan-gold.webp', { format: 'webp' }],
-    ['dan-gold_500x500.jpeg', { width: 500, height: 500 }],
-    ['dan-gold_500x.jpeg', { width: 500, enlarge: 0 }],
-    ['dan-gold_x500.jpeg', { height: 500, enlarge: 0 }],
-    ['dan-gold_crop_left.jpeg', { }],
-    ['dan-gold@2x.jpeg', { width: 2972, enlarge: 0 }],
-    ['dan-gold_500x500_crop_left.jpeg', { width: 500, height: 500, position: 'left' }],
-    # FIXME je crois que si on demande une image de 500 par 500 en retina, on ne s'attend absolument pas à recevoir une image pas carrée sous prétexte que ce n'est pas assez grand
-    ['dan-gold_500x500@2x.jpeg', { width: 1000, enlarge: 0 }],
-    # FIXME idem
-    ['dan-gold_250x250@3x.jpeg', { width: 750, height: 750 }],
-    # FIXME Je ne sais pas ce que ce test veut dire (sans taille, pourquoi cropper ?)
-    ['dan-gold_crop_left@2x.jpeg', { width: 2972, enlarge: 0 }],
-    ['dan-gold_250x250_crop_left@2x.jpeg', { width: 500, height: 500, position: 'left'  }],
-    ['dan-gold_200x300_crop_top.jpeg', { width: 200, height: 300, position: 'top' }],
-    ['dan-gold_300x200_crop_right@2x.jpeg', { width: 600, height: 400, position: 'right' }],
-    ['dan-gold_1000x500_crop_left.jpeg', { width: 1000, height: 500, position: 'left' }],
-    ['dan-gold_1500x500_crop_left.jpeg', { width: 1500, enlarge: 0 }],
-    ['dan-gold_800x840_crop_left.jpeg', { width: 800, enlarge: 0 }],
-    ['dan-gold', {}],
-    ['dan-gold_500x500', { width: 500, height: 500 }],
-  ]
-
   test "KeyCDN params matrix" do
-    KEYCDN_PARAMS.each do |filename, output|
+    EXPECTED_KEYCDN_PARAMS_PER_FILENAME.each do |filename, output|
       blob = create_file_blob(filename: "dan-gold.jpeg")
       variant_service = VariantService.manage(blob, { filename_with_transformations: filename, keycdn: 'true' })
       assert_equal output, variant_service.params
