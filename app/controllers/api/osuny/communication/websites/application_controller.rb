b6ecfe3d24307680ebd746c323e2f4feade6d893
Arnaud Levy <contact@arnaudlevy.com>
2026-01-05 12:52:01 +0100

    refactor

--- app/controllers/api/osuny/communication/websites/application_controller.rb
+++ app/controllers/api/osuny/communication/websites/application_controller.rb
@@ -14,4 +14,17 @@ class Api::Osuny::Communication::Websites::ApplicationController < Api::Osuny::A
   def website
     @website
   end
+
+  def load_migration_identifier
+    raise NoMethodError, "You must implement the `load_migration_identifier` method in #{self.class.name}"
+  end
+
+  def ensure_migration_identifier_is_available
+    raise NoMethodError, "You must implement the `ensure_migration_identifier_is_available` method in #{self.class.name}"
+  end
+
+  def ensure_same_migration_identifier
+    raise NoMethodError, "You must implement the `ensure_same_migration_identifier` method in #{self.class.name}"
+  end
+
 end
