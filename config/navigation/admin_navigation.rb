67a5aacb5af0ee7744e80ce87deb2493f8c3463e
Arnaud Levy <arnaud.levy@noesya.coop>
2026-01-16 10:45:19 +0100

    Update config/navigation/admin_navigation.rb
    Co-authored-by: Sébastien Gaya <sebastien.gaya@gmail.com>

--- config/navigation/admin_navigation.rb
+++ config/navigation/admin_navigation.rb
@@ -9,7 +9,7 @@ SimpleNavigation::Configuration.run do |navigation|
       secondary.item :communication_extranets, Communication::Extranet.model_name.human(count: 2), admin_communication_extranets_path
       secondary.item :communication_media, t('communication.description.parts.media.title'), admin_communication_medias_path
       secondary.item :communication_newsletters, 'Lettres d\'information', nil
-    end if can?(:show, :websites)
+    end if can?(:read, Communication::Website)
     primary.item :directory, t('university.description.title'), admin_university_root_path, { image: 'admin/university-thumb.jpg' } do |secondary|
       secondary.item :directory_persons, University::Person.model_name.human(count: 2), admin_university_people_path
       secondary.item :directory_organizations, University::Organization.model_name.human(count: 2), admin_university_organizations_path
