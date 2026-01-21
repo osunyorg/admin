2284a71181e5dd29c2ffdec878b09c050f1d859a
Arnaud Levy <contact@arnaudlevy.com>
2026-01-21 13:15:54 +0100

    details

--- app/jobs/unicorns_job.rb
+++ app/jobs/unicorns_job.rb
@@ -3,8 +3,5 @@ class UnicornsJob < ApplicationJob
   queue_as :unicorns
 
   def perform
-    Communication::Website::Agenda::Period::Day::Localization.find_each &:save
-    Communication::Website::Agenda::Period::Month::Localization.find_each &:save
-    Communication::Website::Agenda::Period::Year::Localization.find_each &:save
   end
 end
