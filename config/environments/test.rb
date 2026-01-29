cd3fe8c1b0cdfb5ad9f9fb6eabf6f6564162e113
Sébastien Gaya <sebastien.gaya@gmail.com>
2026-01-29 14:39:01 +0100

    Ruby 4.0.1 + Rails 8.1.2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-  config.cache_store = :memory_store
+  config.cache_store = :null_store
 
   # Render exception templates for rescuable exceptions and raise for other exceptions.
   config.action_dispatch.show_exceptions = :rescuable
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "www.example.com" }
+  config.action_mailer.default_url_options = { host: "example.com" }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
