e6bb7bfa45ae4fda6ae04fedc2de0fe38c2848d7
Sébastien Gaya <sebastien.gaya@gmail.com>
2026-02-19 11:29:08 +0100

    assert difference Permalink

--- spec/requests/osuny/v1/communication/websites/pages_spec.rb
+++ spec/requests/osuny/v1/communication/websites/pages_spec.rb
@@ -100,7 +100,9 @@ RSpec.describe 'Communication::Website::Page' do
 
       response '201', 'Successful creation' do
         it 'creates a page and its localization', rswag: true do |example|
-          assert_difference ->{ Communication::Website::Page.count } => 1, ->{ Communication::Website::Page::Localization.count } => 1 do
+          assert_difference ->{ Communication::Website::Page.count } => 1,
+                            ->{ Communication::Website::Page::Localization.count } => 1,
+                            ->{ Communication::Website::Permalink.count } => 1 do
             assert_enqueued_jobs 1, only: Api::AttachFeaturedImageFromUrlJob do
               submit_request(example.metadata)
               assert_response_matches_metadata(example.metadata)
