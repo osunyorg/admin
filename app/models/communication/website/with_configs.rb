9b4db9f6df4c842614e02f3c8912a32378f17b16
Sébastien Gaya <sebastien.gaya@gmail.com>
2026-03-16 16:43:55 +0100

    nginx & wip apache

--- app/models/communication/website/with_configs.rb
+++ app/models/communication/website/with_configs.rb
@@ -4,12 +4,14 @@ module Communication::Website::WithConfigs
   included do
     def self.config_files
       [
+        :apache_config,
         :default_content_security_policy,
         :default_languages,
-        :default_permalinks,
+        :deuxfleurs_config,
         :deuxfleurs_workflow,
-        :deuxfleurs_redirects,
         :development_config,
+        :netlify_config,
+        :nginx_config,
         :production_config,
         :robots_txt
       ]
@@ -18,6 +20,7 @@ module Communication::Website::WithConfigs
 
   def configs
     [
+      config_apache_config,
       config_default_content_security_policy,
       config_default_languages,
       config_development_config,
@@ -25,8 +28,14 @@ module Communication::Website::WithConfigs
       config_deuxfleurs_workflow,
       config_deuxfleurs_config,
       config_netlify_config,
+      config_nginx_config,
       config_robots_txt,
     ].compact
+  end
+
+  def config_apache_config
+    return unless hosted_with_apache?
+    @config_apache_config ||= Communication::Website::Configs::ApacheConfig.find(id)
   end
 
   def config_default_content_security_policy
@@ -58,6 +67,11 @@ module Communication::Website::WithConfigs
   def config_netlify_config
     return unless hosted_with_netlify?
     @config_netlify_config ||= Communication::Website::Configs::NetlifyConfig.find(id)
+  end
+
+  def config_nginx_config
+    return unless hosted_with_nginx?
+    @config_nginx_config ||= Communication::Website::Configs::NginxConfig.find(id)
   end
 
   def config_robots_txt
