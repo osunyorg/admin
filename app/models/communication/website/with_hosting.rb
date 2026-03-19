0f3c4685f2c2b88d8a6cc53d84997b7f6eb4957b
Sébastien Gaya <sebastien.gaya@gmail.com>
2026-03-19 17:57:19 +0100

    Update app/models/communication/website/with_hosting.rb
    Co-authored-by: Arnaud Levy <arnaud.levy@noesya.coop>

--- app/models/communication/website/with_hosting.rb
+++ app/models/communication/website/with_hosting.rb
@@ -2,7 +2,14 @@ module Communication::Website::WithHosting
   extend ActiveSupport::Concern
 
   included do
-    enum :hosting, { undefined: 0, deuxfleurs: 1, apache: 2, nginx: 3, netlify: 4 }, prefix: :hosted_with
+    enum :hosting, {
+      undefined: 0,
+      deuxfleurs: 1,
+      apache: 2,
+      nginx: 3,
+      netlify: 4
+    },
+    prefix: :hosted_with
 
     scope :for_hosting, ->(hosting, language = nil) { where(hosting: hosting) }
   end
