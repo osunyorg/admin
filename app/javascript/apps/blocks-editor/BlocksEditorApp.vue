e8628d5dc285d9c23c643d30513b6b121be732c9
Arnaud Levy <contact@arnaudlevy.com>
2026-04-20 06:33:54 +0200

    wip

---
+++ app/javascript/apps/blocks-editor/BlocksEditorApp.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data () {
+      return {
+      }
+    },
+    methods: {
+    },
+    mounted() {
+    }
+};
+</script>
+
+<template>
+  <section class="vue__blocks-editor">
+    Blocks editor
+  </section>
+</template>
