e489362f2a7489945c373c6e858240840ad61d51
Arnaud Levy <contact@arnaudlevy.com>
2026-04-20 07:05:54 +0200

    load

--- app/javascript/apps/blocks-editor/BlocksEditorApp.vue
+++ app/javascript/apps/blocks-editor/BlocksEditorApp.vue
@@ -7,9 +7,24 @@ export default {
     },
     data () {
       return {
+        i18n: {}
       }
     },
+    beforeMount() {
+      const dataset = document.getElementById('blocks-editor-app').dataset;
+      this.loadJson(dataset.i18n, "i18n");
+    },
     methods: {
+      loadJson(url, target) {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            this[target] = JSON.parse(xhr.responseText);
+          }
+        }.bind(this);
+        xhr.open("GET", url, false);
+        xhr.send();
+      },
     },
     mounted() {
     }
@@ -19,6 +34,7 @@ export default {
 <template>
   <section class="vue__blocks-editor">
     Blocks editor
+    {{ i18n }}
     <AddBlockButton />
   </section>
 </template>
