e12916fa34a5bf08bc20df165b7e9a10cd413bc2
Sébastien Gaya <sebastien.gaya@gmail.com>
2026-04-27 14:54:23 +0200

    Validate GitFile uniqueness & clean git files in unicorns job (#3954)
    * clean git files in unicorns job
    
    * validates
    
    * generate if valid?

--- app/models/communication/website/git_file.rb
+++ app/models/communication/website/git_file.rb
@@ -37,6 +37,9 @@ class Communication::Website::GitFile < ApplicationRecord
   belongs_to :website, class_name: 'Communication::Website'
   belongs_to :about, polymorphic: true, optional: true
 
+  # One Git File per about and website, unless about is nil (destroy orphans)
+  validates :about_id, uniqueness: { scope: [:about_type, :website_id] }, allow_nil: true
+
   scope :desynchronized, -> { where(desynchronized: true) }
   scope :desynchronized_since, -> (time) { desynchronized.where('desynchronized_at > ?', time) }
   scope :desynchronized_until, -> (time) { desynchronized.where('desynchronized_at <= ?', time) }
@@ -63,7 +66,7 @@ class Communication::Website::GitFile < ApplicationRecord
       # If it's just initialized, it needs to be saved
       save unless persisted?
       # Anyway, we need to generate content (from WithContent)
-      generate_content
+      generate_content if valid?
     elsif persisted?
       # There, but not syncable, so bye bye
       mark_for_destruction!
