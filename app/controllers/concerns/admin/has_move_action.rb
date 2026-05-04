647bb64f9f1a4f4173a28017805dcec3c5bcf658
Sébastien Gaya <sebastien.gaya@gmail.com>
2026-05-04 12:55:43 +0200

    clean

--- app/controllers/concerns/admin/has_move_action.rb
+++ app/controllers/concerns/admin/has_move_action.rb
@@ -3,6 +3,7 @@ module Admin::HasMoveAction
 
   included do
     before_action :load_target_websites, only: [:move, :do_move, :move_batch, :do_move_batch]
+    before_action :load_and_authorize_target_website, only: [:do_move, :do_move_batch]
     before_action :load_object, :ensure_access, only: [:move, :do_move]
   end
 
@@ -13,41 +14,40 @@ module Admin::HasMoveAction
   end
 
   def do_move
-    new_website = @target_websites.find(params[:new_website_id])
-    redirect_to [:admin, @object], alert: t('admin.move.permission_denied') and return unless new_website
-    redirect_to [:admin, @object], alert: t('admin.move.permission_denied') and return if new_website.university_id != @object.university_id
-    @object.move_to!(new_website)
-    redirect_to [:admin, @object, website_id: new_website.id], notice: t('admin.move.successfully_moved', model: @object.to_s_in(current_language))
+    raise CanCan::AccessDenied unless can?(:move, @object)
+    @object.move_to!(@target_website)
+    redirect_to [:admin, @object, website_id: @target_website.id],
+                notice: t('admin.move.successfully_moved', model: @object.to_s_in(current_language))
   end
 
   def move_batch
-    @filtered = @website.public_send(resources)
+    @filtered = @website.public_send(resource_plural_name)
                         .filter_by(params[:filters], current_language)
     @objects = @filtered.at_lifecycle(params[:lifecycle], current_language)
                         .ordered(current_language)
                         .page(params[:page])
-    @resource_name = resources
+    @resource_plural_name = resource_plural_name
     breadcrumb
     add_breadcrumb t('admin.move.cta')
     render 'admin/application/move/move_batch'
   end
 
   def do_move_batch
-    target_website = @target_websites.find(params[:target])
-    ids = params[:ids].compact_blank || []
+    ids = params[:ids]&.compact_blank || []
     ids.each do |id|
-      object = @website.public_send(resources).find_by(id: id)
-      object.move_to!(target_website)
+      object = @website.public_send(resource_plural_name).find_by(id: id)
+      object.move_to!(@target_website) if can?(:move, object)
     end
-    redirect_to public_send("admin_communication_website_#{resources}_path", website_id: @website.id),
+    redirect_to public_send("admin_communication_website_#{resource_plural_name}_path", website_id: @target_website.id),
                 notice: t('batch.movable.successfully_moved')
   end
 
   protected
 
   def load_object
-    # resource is inherited from localizable concern, only working because "movable" object are also localizable. 
-    # If we want to use this concern for non-localizable objects, we would need to define a method to load the object instead of relying on resource.
+    # resource is inherited from localizable concern, only working because "movable" object are also localizable.
+    # If we want to use this concern for non-localizable objects, we would need to define a method to load the object
+    # instead of relying on resource.
     @object = resource
   end
 
@@ -55,16 +55,28 @@ module Admin::HasMoveAction
     @target_websites = current_user.managed_websites
                                    .where(university_id: @website.university_id)
                                    .where.not(id: @website.id)
+    redirect_back(fallback_location: [:admin, @website], alert: t('admin.move.permission_denied')) if @target_websites.empty?
   end
-  
-  def ensure_access
-    unless can?(:move, @object) && @target_websites.count > 0
-      redirect_to [:admin, @object], alert: t('admin.move.permission_denied')
+
+  def load_and_authorize_target_website
+    @target_website = @target_websites.find_by(id: params[:new_website_id])
+    if @target_website.nil? || @target_website.university_id != @website.university_id
+      redirect_back(fallback_location: [:admin, @website], alert: t('admin.move.permission_denied'))
     end
   end
 
-  def resources
-    self.class.to_s.remove("Controller").demodulize.underscore
+  def ensure_access
+    redirect_back(fallback_location: [:admin, @website], alert: t('admin.move.permission_denied')) unless can?(:move, @object)
+  end
+
+  def resource_plural_name
+    # Admin::Communication::Websites::PostsController => "posts"
+    # Admin::Communication::Websites::Agenda::EventsController => "agenda_events"
+    self.class.to_s
+      .delete_suffix("Controller")
+      .delete_prefix("Admin::Communication::Websites::")
+      .gsub('::', '')
+      .underscore
   end
 
 end
