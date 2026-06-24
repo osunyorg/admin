2b7f74bff2abb4b8e0987137ade8e08e81ddb3dd
Arnaud Levy <contact@arnaudlevy.com>
2026-06-24 08:13:11 +0200

    fix

--- app/models/university/person/with_facets.rb
+++ app/models/university/person/with_facets.rb
@@ -19,4 +19,27 @@ module University::Person::WithFacets
     # Filters
     scope :for_facet, -> (facet, language = nil) { where("is_#{facet}": true) }  
   end
+
+  def facets
+    LIST_OF_FACETS.reject do |facet|
+      !public_send("is_#{facet}")
+    end
+  end
+
+  def administrator_facets
+    @administrator_facets ||= University::Person::Localization::Administrator.where(id: localization_ids)
+  end
+
+  def author_facets
+    @author_facets ||= University::Person::Localization::Author.where(id: localization_ids)
+  end
+
+  def researcher_facets
+    @researcher_facets ||= University::Person::Localization::Researcher.where(id: localization_ids)
+  end
+
+  def teacher_facets
+    @teacher_facets ||= University::Person::Localization::Teacher.where(id: localization_ids)
+  end
+
 end
