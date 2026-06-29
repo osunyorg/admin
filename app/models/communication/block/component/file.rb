fc2ff9a75348ed739a9e23185f5e692e12912c50
Arnaud Levy <contact@arnaudlevy.com>
2026-06-29 11:34:20 +0200

    interface component file

--- app/models/communication/block/component/file.rb
+++ app/models/communication/block/component/file.rb
@@ -16,20 +16,39 @@ class Communication::Block::Component::File < Communication::Block::Component::B
 
   def blob
     return if data.nil? || data['id'].blank?
-    @blob ||= template.block
-                      .university
-                      .active_storage_blobs
-                      .find_by id: data['id']
+    @blob ||= university.active_storage_blobs.find_by(id: data['id'])
+  end
+
+  def communication_file
+    return if data.nil? || data['communication_file_id'].blank?
+    @communication_file ||= university.communication_file.find_by(id: data['communication_file_id'])
+  end
+
+  def communication_file_localisation
+    @communication_file_localisation ||= communication_file.localization_for(language)
   end
 
   def default_data
     {
-      'id' => ''
+      'id' => '',
+      'communication_file_id' => ''
     }
   end
 
   def dependencies
-    [blob]
+    [
+      blob,
+      communication_file
+    ]
   end
 
+  protected
+
+  def university
+    @university ||= template.block.university
+  end
+
+  def language
+    @lanuage ||= block.language
+  end
 end
