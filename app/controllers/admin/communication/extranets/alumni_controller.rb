ab3ad08f29c4c2484ad5036808d4b510fdc653c3
pabois <pierreandre.boissinot@noesya.coop>
2026-07-02 15:58:02 +0200

    Permet d'envoyer des mails d'invitations aux alumni d'un extranet depuis l'admin

--- app/controllers/admin/communication/extranets/alumni_controller.rb
+++ app/controllers/admin/communication/extranets/alumni_controller.rb
@@ -2,10 +2,43 @@ class Admin::Communication::Extranets::AlumniController < Admin::Communication::
   def index
     @about = @extranet.about
     @alumni = @extranet.alumni
-    @cohorts = @extranet.cohorts
-    @years = @extranet.years
-    @organizations = @extranet.organizations
+                       .filter_by(params[:filters], current_language)
+    @alumni = filter_by_account(@alumni)
+    @alumni = @alumni.alumni
+                     .ordered(current_language)
+                     .page(params[:page])
+    @cohorts_count = @extranet.cohorts.count
+    @years_count = @extranet.years.count
+    @organizations_count = @extranet.organizations.count
     breadcrumb
     add_breadcrumb Communication::Extranet.human_attribute_name(:feature_alumni)
+  end
+
+  def send_invitation
+    person = @extranet.alumni.find(params[:id])
+    if person && !person.user_id
+      ExtranetMailer.invitation_message(@extranet, person).deliver_later
+      person.update_column(:invitation_sent_at, Time.current)
+      redirect_to admin_communication_extranet_alumni_path(@extranet), 
+                  notice: t('admin.communication.extranet.alumni.send_invitation.just_sent', name: person.to_s_in(current_language))
+    else
+      redirect_to admin_communication_extranet_alumni_path(@extranet), 
+                  alert: t('admin.communication.extranet.alumni.already_active')
+    end
+  end
+
+  private
+
+  # This filter can't live in the Filterable scopes because it needs the extranet context.
+  def filter_by_account(alumni)
+    with_account = params.dig(:filters, :for_alumni_account)
+    return alumni if with_account.blank?
+
+    people_with_account = University::Person.where(user_id: @extranet.users.select(:id)).select(:id)
+    if ActiveModel::Type::Boolean.new.cast(with_account)
+      alumni.where(id: people_with_account)
+    else
+      alumni.where.not(id: people_with_account)
+    end
   end
 end
