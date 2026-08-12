2d5d617b19965f4e39efdff2f3d958c9998f8134
Arnaud Levy <contact@arnaudlevy.com>
2026-08-12 07:33:50 +0200

    page

--- app/javascript/apps/components/inputs/primitives/SelectedPage.vue
+++ app/javascript/apps/components/inputs/primitives/SelectedPage.vue
@@ -42,18 +42,49 @@ export default {
 <template>
   <div>
     <div v-if="loading" class="placeholder-glow">
-      <span class="placeholder d-block" style="width: 120px; height: 1rem;"></span>
+      <div class="card card--osuny card--horizontal">
+        <div class="osuny__thumbnail osuny__thumbnail--small osuny__thumbnail--cropped">
+          <picture class="placeholder" style="width: 140px; height: 140px;"></picture>
+        </div>
+        <div class="card-body">
+          <span class="osuny__published osuny__published--false"></span>
+          <span class="placeholder" style="width: 120px; height: 1rem;"></span>
+        </div>
+      </div>
     </div>
     <template v-else>
-      <p class="mb-0">
-        {{ page?.title }}
-        <a
-          class="btn btn-sm text-danger p-0 small"
-          @click="$emit('remove')">
-          <i class="fas fa-times"></i>
-          {{ $t('components.inputs.pageSelector.remove') }}
-        </a>
-      </p>
+      <div class="card card--osuny card--horizontal">
+        <div class="osuny__thumbnail osuny__thumbnail--small osuny__thumbnail--cropped">
+          <picture v-if="page?.featured_image">
+            <img  :src="page?.featured_image.thumb"
+                  loading="lazy"
+                  decoding="async"
+                  width="140"
+                  height="140"
+                  class="img-fluid">
+          </picture>
+          <span v-else class="osuny__thumbnail__initials">
+            {{ page?.initials }}
+          </span>
+        </div>
+        <div class="card-body">
+          <span
+            class="osuny__published"
+            :class="{ 
+              'osuny__published--true': page?.published, 
+              'osuny__published--false': !page?.published
+            }"></span>
+          {{ page?.title }}
+        </div>
+        <div class="card-footer">
+          <a
+            class="btn btn-sm text-danger p-0 small"
+            @click="$emit('remove')">
+            <i class="fas fa-times"></i>
+            {{ $t('components.inputs.pageSelector.remove') }}
+          </a>
+        </div>
+      </div>
     </template>
   </div>
 </template>
--- app/views/admin/communication/websites/pages/show.json.jbuilder
+++ app/views/admin/communication/websites/pages/show.json.jbuilder
@@ -1,25 +1,5 @@
-json.extract! @page, :id
-json.extract! @l10n,
-              :title, :slug, :breadcrumb_title,
-              :header_text, :meta_description, :published
-json.extract! @page,
-              :bodyclass, :full_width, :type, :position
-json.path admin_communication_website_page_path(@page, format: :json)
+json.extract! @l10n, :title, :initials, :published
 
 json.featured_image do
-  json.alt @l10n.featured_image_alt
-  json.credit @l10n.featured_image_credit
-  json.blob_id @l10n.featured_image.blob_id
+  json.thumb url_for(@l10n.featured_image.variant(resize_to_fill: [140, 140]))
 end if @l10n.featured_image.attached?
-
-language = @l10n.language
-json.language do
-  json.extract! language, :id, :name, :iso_code, :summernote_locale
-  json.label language_name(language.iso_code)
-end
-
-parent = @page.parent
-json.parent do
-  json.extract! parent, :id
-  json.path admin_communication_website_page_path(parent, format: :json)
-end if parent
