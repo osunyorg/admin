2f383ed945985162bd404ae2b99ac3cf301f7e44
Arnaud Levy <contact@arnaudlevy.com>
2026-08-13 11:03:18 +0200

    Rangement du FileInput

--- app/javascript/apps/blocks-editor/components/Editor.vue
+++ app/javascript/apps/blocks-editor/components/Editor.vue
@@ -5,11 +5,10 @@ import { getI18n } from '../../i18n';
 import Picker from '../../picker/Picker.vue';
 // Inputs
 import CodeInput              from '../../components/inputs/CodeInput.vue';
+import FileInput              from '../../components/inputs/FileInput.vue';
 import MultiImageInput        from '../../components/inputs/MultiImageInput.vue';
 import RichTextInput          from '../../components/inputs/RichTextInput.vue';
 import UploadInput            from '../../components/inputs/UploadInput.vue';
-// Selectors
-import FileSelector           from '../../components/selectors/FileSelector.vue';
 // Selected objects
 import SelectedFile           from '../../components/selected-objects/SelectedFile.vue';
 import SelectedMedia          from '../../components/selected-objects/SelectedMedia.vue';
@@ -160,11 +159,10 @@ export default {
       this.innerApp.component('Picker', Picker);
       // Inputs
       this.innerApp.component('CodeInput', CodeInput);
+      this.innerApp.component('FileInput', FileInput);
       this.innerApp.component('MultiImageInput', MultiImageInput);
       this.innerApp.component('RichTextInput', RichTextInput);
       this.innerApp.component('UploadInput', UploadInput);
-      // Selectors
-      this.innerApp.component('FileSelector', FileSelector);
       // Selected objects
       this.innerApp.component('SelectedFile', SelectedFile);
       this.innerApp.component('SelectedMedia', SelectedMedia);
