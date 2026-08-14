74c281d1792fa945d895b6a9b384dc550df97481
Sébastien Gaya <sebastien.gaya@gmail.com>
2026-08-14 15:31:33 +0200

    TODO tests

--- test/models/concerns/sluggable_test.rb
+++ test/models/concerns/sluggable_test.rb
@@ -19,8 +19,23 @@ class SluggableTest < ActiveSupport::TestCase
     orga_l10n.set_slug
     assert_equal "un-nom-dentreprise-avec-caracteres-speciaux-123", orga_l10n.slug, "Special chars are removed"
 
+    # TODO: Handle Vietnamese
+    # orga_l10n.assign_attributes(slug: nil, name: "L'art de cuisiner les phở đặc biệt")
+    # orga_l10n.set_slug
+    # assert_equal "lart-de-cuisiner-les-pho-bac-biet", orga_l10n.slug, "Transliterate Vietnamese characters"
+
     orga_l10n.assign_attributes(slug: nil, name: "L'art de cuisiner les スシ")
     orga_l10n.set_slug
     assert_equal "lart-de-cuisiner-les-sushi", orga_l10n.slug, "Transliterate Kana characters"
+
+    # TODO: Handle Russian
+    # orga_l10n.assign_attributes(slug: nil, name: "L'art de cuisiner les суши")
+    # orga_l10n.set_slug
+    # assert_equal "lart-de-cuisiner-les-sushi", orga_l10n.slug, "Transliterate Cyrillic characters"
+
+    # TODO: Handle Hindi
+    # orga_l10n.assign_attributes(slug: nil, name: "L'art de cuisiner les सूशी")
+    # orga_l10n.set_slug
+    # assert_equal "lart-de-cuisiner-les-sushi", orga_l10n.slug, "Transliterate Hindi characters"
   end
 end
