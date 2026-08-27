6c5e4c8752dd024b86d186fa213ce8a75af1118e
Sébastien Gaya <sebastien.gaya@gmail.com>
2026-08-27 11:34:46 +0200

    line break

--- db/migrate/20260821054601_add_publication_to_communication_media_localizations.rb
+++ db/migrate/20260821054601_add_publication_to_communication_media_localizations.rb
@@ -2,6 +2,7 @@ class AddPublicationToCommunicationMediaLocalizations < ActiveRecord::Migration[
   def change
     add_column :communication_media_localizations, :published, :boolean, default: false
     add_column :communication_media_localizations, :published_at, :datetime
+
     Communication::Media::Localization.reset_column_information
     Communication::Media::Localization.update_all(published: true, published_at: Time.current)
   end
