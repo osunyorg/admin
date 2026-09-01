b0918d793a554b76c628e99c5687f29cff3149b6
Arnaud Levy <contact@arnaudlevy.com>
2026-09-01 09:19:07 +0200

    remove

--- app/jobs/unicorns_job.rb
+++ app/jobs/unicorns_job.rb
@@ -3,12 +3,6 @@ class UnicornsJob < ApplicationJob
   queue_as :unicorns
 
   def perform
-    Migrations::DenormalizeFileExtensions.migrate
-    Migrations::DenormalizeFileContexts.migrate
-    Migrations::DenormalizeMediaExtensions.migrate
-    Migrations::DenormalizeMediaContexts.migrate
-    Migrations::FeaturedImagesToMedias.migrate
-    Migrations::DownloadableSummaryToFiles.migrate
     Migrations::BlocksWithMedias.migrate
     Migrations::BlockTitlesToFiles.migrate
     Migrations::CleanOrphanAttachments.migrate
