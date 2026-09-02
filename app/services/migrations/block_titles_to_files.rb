5fe035683ea3229891f1de755732e9b2645d0694
Sébastien Gaya <sebastien.gaya@gmail.com>
2026-09-02 15:46:55 +0200

    Fix Files report title

--- app/services/migrations/block_titles_to_files.rb
+++ app/services/migrations/block_titles_to_files.rb
@@ -17,11 +17,6 @@ class Migrations::BlockTitlesToFiles
             element.title,
             block.language
           )
-          report_image(
-            element.communication_file,
-            element.image,
-            block.language
-          )
         end
       end
     end
@@ -29,33 +24,14 @@ class Migrations::BlockTitlesToFiles
 
   protected
 
-  # Reporte le title saisi dans le bloc sur le name de la Communication::File::Localization associée, 
+  # Reporte le title saisi dans le bloc sur le name de la Communication::File::Localization associée,
   # uniquement si ce name n'a jamais été changé (= il vaut encore l'original_guessed_name).
   def self.report_title(communication_file, title, language)
     return if communication_file.nil? || title.blank?
     file_l10n = communication_file.localization_for(language)
     return if file_l10n.nil?
-    name_was_changed = file_l10n.name != file_l10n.original_guessed_name
-    return if name_was_changed
-    file_l10n.update(name: title)
-    puts "File localization #{file_l10n.id} renamed to « #{title} »"
+    old_name = file_l10n.name
+    file_l10n.update!(name: title)
+    puts "File localization #{file_l10n.id} renamed from « #{old_name} » to « #{title} »"
   end
-
-  def self.report_image(communication_file, image, language)
-    return if communication_file.nil?
-    return if image['id'].blank?
-    blob = ActiveStorage::Blob.find_by(id: image['id'])
-    return if blob.nil?
-    media = Communication::Media.find_or_create_media_from_blob(blob)
-    media_l10n = media.find_or_create_localization(language)
-    media_l10n.published = true
-    media_l10n.save
-    file_l10n = communication_file.localization_for(language)
-    file_l10n.set_featured_media!(
-      id: media.id,
-      language: language
-    )
-    puts "File localization #{file_l10n.id} added media #{media.id}"
-  end
-
 end
