37bc4f1b462726677e2f48e11dc1530bc7930f60
Arnaud Levy <contact@arnaudlevy.com>
2026-09-11 12:04:21 +0200

    Clean

--- app/services/osuny/evolution/song.rb
+++ app/services/osuny/evolution/song.rb
@@ -2,6 +2,10 @@ class Osuny::Evolution::Song
   PATH = 'app/services/osuny/evolution/songs.yml'
 
   def self.random
-    YAML.load_file(PATH).sample
+    data.sample
+  end
+
+  def self.data
+    @@data ||= YAML.load_file(PATH)
   end
 end
