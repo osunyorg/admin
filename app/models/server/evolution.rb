0fc1133eb3a0337d3aeb63fec38f55f1e86a0a90
Arnaud Levy <arnaud.levy@noesya.coop>
2026-09-11 12:03:22 +0200

    Update app/models/server/evolution.rb
    Co-authored-by: Sébastien Gaya <sebastien.gaya@gmail.com>

--- app/models/server/evolution.rb
+++ app/models/server/evolution.rb
@@ -10,14 +10,13 @@
 class Server::Evolution < ApplicationRecord
 
   has_many :localizations, dependent: :destroy
-  
-  scope :ordered, -> { order(released_at: :desc) }
-  scope :planned, -> { where('released_at > ?', Date.current) }
-
   accepts_nested_attributes_for :localizations
 
   validates_presence_of :released_at
   validates_associated :localizations
+  
+  scope :ordered, -> { order(released_at: :desc) }
+  scope :planned, -> { where('released_at > ?', Date.current) }
 
   def original_localization
     french = Language.find_by(iso_code: :fr)
