11a9783013ae36d6bf3f2743dbf1c35ad79b64d1
pabois <pierreandre.boissinot@noesya.coop>
2026-09-17 16:23:47 +0200

    adjust

--- app/models/university/person/localization.rb
+++ app/models/university/person/localization.rb
@@ -113,22 +113,23 @@ class University::Person::Localization < ApplicationRecord
     "admin/university/people/static"
   end
 
+  # "Jean Dupont (Martin)" si nom d'usage, sinon "Jean Dupont"
   def to_s
-    with_chosen_name("#{first_name} #{last_name}".strip)
+    chosen_name.present? ? "#{first_name} #{last_name} (#{chosen_name})".strip : "#{first_name} #{last_name}".strip
   end
-
+  
   def to_s_with_mail
     about.email.present? ? "#{to_s} (#{about.email})" : to_s
   end
-
+  
+  # "Dupont (Martin) Jean" si nom d'usage, sinon "Dupont Jean"
   def to_s_alphabetical
-    with_chosen_name("#{last_name} #{first_name}".strip)
+    chosen_name.present? ? "#{last_name} #{first_name} (#{chosen_name})".strip : "#{last_name} #{first_name}".strip
   end
 
   def initials
     "#{first_name.to_s.first}#{last_name.to_s.first}"
   end
-
 
   # user in statics where we don't need the cateogries not localized
   def categories
@@ -150,11 +151,6 @@ class University::Person::Localization < ApplicationRecord
 
   def prepare_name
     self.name = to_s
-  end
-
-  # "Jean Dupont (Martin)" si nom d'usage, sinon "Jean Dupont"
-  def with_chosen_name(base)
-    chosen_name.present? ? "#{base} (#{chosen_name})" : base
   end
 
 end
